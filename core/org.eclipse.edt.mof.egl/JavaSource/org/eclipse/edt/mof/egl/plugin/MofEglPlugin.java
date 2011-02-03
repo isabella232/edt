01063d3c408b00611f11e3485e99624c979ba359
twilson <twilson>
2011-02-03 20:45:23 +0000

    Updates to make the plugins work within an Eclipse IDE environment

--- core/org.eclipse.edt.mof.egl/JavaSource/org/eclipse/edt/mof/egl/plugin/MofEglPlugin.java
+++ core/org.eclipse.edt.mof.egl/JavaSource/org/eclipse/edt/mof/egl/plugin/MofEglPlugin.java
@@ -3,20 +3,20 @@ package org.eclipse.edt.mof.egl.plugin;
 import java.io.File;
 import java.io.IOException;
 
+import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Plugin;
 import org.eclipse.edt.compiler.SystemEnvironment;
 import org.eclipse.edt.compiler.internal.core.builder.NullBuildNotifier;
 import org.eclipse.edt.compiler.internal.core.lookup.ICompilerOptions;
 import org.eclipse.edt.compiler.internal.sdk.compile.SourcePathEntry;
-import org.eclipse.edt.compiler.internal.sdk.compile.SourcePathInfo;
 import org.eclipse.edt.mof.egl.compiler.EGL2IREnvironment;
 import org.eclipse.edt.mof.egl.compiler.Processor;
 import org.eclipse.edt.mof.egl.compiler.SystemPackageBuildPathEntryFactory;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
-public class MofEglPlugin extends AbstractUIPlugin{
+public class MofEglPlugin extends Plugin{
 
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
@@ -29,7 +29,7 @@ public class MofEglPlugin extends AbstractUIPlugin{
 	public static void initializeSystemPackages(){
 		Bundle bundle = Platform.getBundle("org.eclipse.edt.compiler"); //$NON-NLS-1$
 		try {
-			String file = Platform.resolve( bundle.getEntry( "/" ) ).getFile(); //$NON-NLS-1$
+			String file = FileLocator.resolve( bundle.getEntry( "/" ) ).getFile(); //$NON-NLS-1$
 			// Replace Eclipse's slashes with the system's file separator.
 			file = file.replace( '/', File.separatorChar );
 			file = file + SystemEnvironment.EDT_LIB_DIRECTORY;
