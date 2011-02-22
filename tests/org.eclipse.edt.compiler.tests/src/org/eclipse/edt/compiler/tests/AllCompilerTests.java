fa118685d25d9af1dac286be954d739d20689a8f
mheitz <mheitz>
2011-02-22 20:29:15 +0000

    Initial version.

---
+++ tests/org.eclipse.edt.compiler.tests/src/org/eclipse/edt/compiler/tests/AllCompilerTests.java
@@ -0,0 +1,18 @@
+package org.eclipse.edt.compiler.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllCompilerTests
+{
+	public static Test suite() throws Exception
+	{
+		TestSuite suite = new TestSuite( "Tests for the EDT Compiler" );
+		
+		// $JUnit-BEGIN$
+		suite.addTestSuite( DummyTest.class );
+		// $JUnit-END$
+		
+		return suite;
+	}
+}
