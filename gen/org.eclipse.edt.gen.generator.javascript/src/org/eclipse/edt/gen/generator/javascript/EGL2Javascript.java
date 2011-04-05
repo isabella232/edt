ebc9ecb7bf7bcd509db2253cedf8903d28327bac
jdouglas <jdouglas>
2011-04-05 19:09:42 +0000

    Extensive changes made to make all classes fully extendible, not just the leaf classes. In addition, all changes made for javascript.

--- gen/org.eclipse.edt.gen.generator.javascript/src/org/eclipse/edt/gen/generator/javascript/EGL2Javascript.java
+++ gen/org.eclipse.edt.gen.generator.javascript/src/org/eclipse/edt/gen/generator/javascript/EGL2Javascript.java
@@ -11,13 +11,17 @@
  *******************************************************************************/
 package org.eclipse.edt.gen.generator.javascript;
 
+import org.eclipse.edt.compiler.core.IEGLConstants;
+import org.eclipse.edt.compiler.internal.core.utils.Aliaser;
 import org.eclipse.edt.gen.AbstractGeneratorCommand;
 import org.eclipse.edt.gen.javascript.Constants;
 import org.eclipse.edt.gen.javascript.JavascriptGenerator;
+import org.eclipse.edt.mof.egl.Annotation;
+import org.eclipse.edt.mof.egl.Part;
 
-public class EGL2JavaScript extends AbstractGeneratorCommand {
+public class EGL2Javascript extends AbstractGeneratorCommand {
 
-	public EGL2JavaScript() {
+	public EGL2Javascript() {
 		super();
 		// define our local command parameters
 		this.installParameter(false, Constants.parameter_checkOverflow, new String[] { "checkOverflow", "overflow", "co" }, new Boolean[] { false, true },
@@ -26,7 +30,7 @@ public class EGL2JavaScript extends AbstractGeneratorCommand {
 
 	public static void main(String[] args) {
 		// set up the command processor and pass the overriding command line options
-		EGL2JavaScript genPart = new EGL2JavaScript();
+		EGL2Javascript genPart = new EGL2Javascript();
 		genPart.generate(args, new JavascriptGenerator(genPart), null);
 	}
 
@@ -52,5 +56,23 @@ public class EGL2JavaScript extends AbstractGeneratorCommand {
 	public String[] getTemplatePath() {
 		return new String[] { "org.eclipse.edt.gen.javascript.templates.egl.lang.templates", "org.eclipse.edt.gen.javascript.templates.egl.core.templates",
 			"org.eclipse.edt.gen.javascript.templates.templates" };
+	}
+
+	protected String getRelativeFileName(Part part) {
+		StringBuilder buf = new StringBuilder(50);
+		String pkg = part.getPackageName();
+		if (pkg.length() > 0) {
+			buf.append(Aliaser.packageNameAlias(pkg.split("[.]"), '/'));
+			buf.append('/');
+		}
+		String nameOrAlias;
+		Annotation annot = part.getAnnotation(IEGLConstants.PROPERTY_ALIAS);
+		if (annot != null)
+			nameOrAlias = (String) annot.getValue();
+		else
+			nameOrAlias = part.getId();
+		buf.append(Aliaser.getAlias(nameOrAlias));
+		buf.append(getFileExtention());
+		return buf.toString();
 	}
 }
