0c3344c2293ca607d75164b0b287f529550c2d68
jdouglas <jdouglas>
2011-04-14 12:10:54 +0000

    Added logic for allowing multiple parts being generated, from the command line, at one time, based on a wildcard character (*).

--- gen/org.eclipse.edt.gen/src/org/eclipse/edt/gen/AbstractGeneratorCommand.java
+++ gen/org.eclipse.edt.gen/src/org/eclipse/edt/gen/AbstractGeneratorCommand.java
@@ -13,6 +13,9 @@ package org.eclipse.edt.gen;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import org.eclipse.edt.mof.egl.Part;
 import org.eclipse.edt.mof.egl.utils.IRLoader;
@@ -33,7 +36,7 @@ public abstract class AbstractGeneratorCommand extends CommandProcessor {
 		this.installParameter(true, Constants.parameter_output, new String[] { "output", "out", "o" }, new String[] { null },
 			"Output must identify the location to write the output");
 		this.installParameter(true, Constants.parameter_part, new String[] { "part", "p" }, new String[] { null },
-			"Part must identify the part to be generated");
+			"Part must identify the part to be generated, which can contain an * for all matching parts");
 		this.installParameter(true, Constants.parameter_root, new String[] { "root", "r" }, new String[] { null },
 			"Root must identify the root location to be used in generation");
 		this.installParameter(false, Constants.parameter_trace, new String[] { "trace" }, new Boolean[] { false, true },
@@ -95,16 +98,18 @@ public abstract class AbstractGeneratorCommand extends CommandProcessor {
 			this.installOverrides(args);
 			// start up the generator, passing the command processor
 			try {
-				Part part = loadEGLPart();
-				generator.generate(part);
-				// now try to write out the file, based on the output location and the part's type signature
-				try {
-					// only write the data, if there was some
-					if (generator.getResult().length() > 0)
-						writeFile(part, generator);
-				}
-				catch (Throwable e) {
-					e.printStackTrace();
+				List<Part> parts = loadEGLParts();
+				for (Part part : parts) {
+					generator.generate(part);
+					// now try to write out the file, based on the output location and the part's type signature
+					try {
+						// only write the data, if there was some
+						if (generator.getResult().length() > 0)
+							writeFile(part, generator);
+					}
+					catch (Throwable e) {
+						e.printStackTrace();
+					}
 				}
 			}
 			catch (Exception e) {
@@ -133,9 +138,62 @@ public abstract class AbstractGeneratorCommand extends CommandProcessor {
 		}
 	}
 
-	protected Part loadEGLPart() throws LoadPartException {
-		return IRLoader.loadEGLPart((String) parameterMapping.get(Constants.parameter_root).getValue(), (String) parameterMapping.get(Constants.parameter_part)
-			.getValue());
+	private void listAllFiles(File parent, List<File> files, String pattern) {
+		if (parent.isDirectory()) {
+			File[] children = parent.listFiles();
+			for (File child : children) {
+				if (child.isFile()
+					&& (child.getName().toLowerCase(Locale.ENGLISH).endsWith(".eglxml") || child.getName().toLowerCase(Locale.ENGLISH).endsWith(".ir"))) {
+					// get the full name of the file
+					String name = child.getPath();
+					// get the prefix length to check, which is the amount up to the asterisk (only 1 asterisk allowed)
+					int prefixLength = pattern.indexOf("*");
+					// does the file we are trying match up until the asterisk
+					if (name.length() >= prefixLength && name.substring(0, prefixLength).equalsIgnoreCase(pattern.substring(0, prefixLength))) {
+						// the prefix matches, so now check to see if there is any suffix and check it as well
+						if (pattern.length() <= (prefixLength + 1)
+							|| name.toLowerCase(Locale.ENGLISH).endsWith(pattern.substring(prefixLength + 1) + ".eglxml")
+							|| name.toLowerCase(Locale.ENGLISH).endsWith(pattern.substring(prefixLength + 1) + ".ir"))
+							files.add(child);
+					}
+				}
+				listAllFiles(child, files, pattern);
+			}
+		}
+	}
+
+	protected List<Part> loadEGLParts() throws LoadPartException {
+		List<Part> parts = new ArrayList<Part>();
+		// check to see if the part has an asterisk, indicating that all matching files are desired
+		String rootName = (String) parameterMapping.get(Constants.parameter_root).getValue();
+		String partName = (String) parameterMapping.get(Constants.parameter_part).getValue();
+		if (partName.indexOf("*") >= 0) {
+			// we need to locate the root ir location and can it's directory for parts matching the pattern defined
+			String totalName;
+			if (rootName.endsWith(File.separator))
+				totalName = rootName;
+			else
+				totalName = rootName + File.separator;
+			if (partName.startsWith(File.separator))
+				totalName += partName.substring(File.separator.length());
+			else
+				totalName += partName;
+			totalName = totalName.replace('\\', File.separatorChar);
+			totalName = totalName.replace('/', File.separatorChar);
+			totalName = totalName.replace('.', File.separatorChar);
+			File irRootDirFile = new File(rootName);
+			List<File> irfiles = new ArrayList<File>();
+			listAllFiles(irRootDirFile, irfiles, totalName);
+			for (File irfile : irfiles) {
+				String irFileRelativePath = irfile.getAbsolutePath().substring(irRootDirFile.getAbsolutePath().length());
+				if (irFileRelativePath.startsWith(File.separator))
+					irFileRelativePath = irFileRelativePath.substring(File.separator.length());
+				parts
+					.add(IRLoader.loadEGLPart(rootName, irFileRelativePath.substring(0, irFileRelativePath.lastIndexOf(".")).replace(File.separatorChar, '.')));
+			}
+		} else
+			parts.add(IRLoader.loadEGLPart(rootName, partName));
+		return parts;
 	}
 
 	protected void writeFile(Part part, Generator generator) throws Exception {
