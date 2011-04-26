af66d6616c686a61e74b5387b669e9ba612f30d1
jdouglas <jdouglas>
2011-04-26 15:15:57 +0000

    *** empty log message ***

---
+++ gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/ThisExpressionTemplate.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright © 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.edt.gen.java.templates;
+
+import org.eclipse.edt.gen.java.Context;
+import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.ThisExpression;
+
+public class ThisExpressionTemplate extends JavaTemplate {
+
+	public void genExpression(ThisExpression expr, Context ctx, TabbedWriter out, Object... args) {
+		// write out "this."
+		out.print("this.");
+	}
+}
