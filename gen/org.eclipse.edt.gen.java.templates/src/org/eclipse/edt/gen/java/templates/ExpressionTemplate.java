fea4e4668e7f1f6130459d720145c186b51f0020
jdouglas <jdouglas>
2011-04-27 15:56:28 +0000

    The expression template was causing an innfinite loop if the expression was never implemented. Now a generation error is created.

--- gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/ExpressionTemplate.java
+++ gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/ExpressionTemplate.java
@@ -11,13 +11,31 @@
  *******************************************************************************/
 package org.eclipse.edt.gen.java.templates;
 
+import org.eclipse.edt.compiler.core.IEGLConstants;
+import org.eclipse.edt.gen.EGLMessages.EGLMessage;
+import org.eclipse.edt.gen.java.Constants;
 import org.eclipse.edt.gen.java.Context;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.Annotation;
 import org.eclipse.edt.mof.egl.Expression;
 
 public class ExpressionTemplate extends JavaTemplate {
 
 	public void genExpression(Expression expr, Context ctx, TabbedWriter out, Object... args) {
-		ctx.gen(genExpression, expr, ctx, out, args);
+		int startLine = 0;
+		int startOffset = 0;
+		int endLine = 0;
+		int endOffset = 0;
+		String[] details = new String[] { expr.getEClass().getETypeSignature() };
+		Annotation annotation = expr.getAnnotation(IEGLConstants.EGL_LOCATION);
+		if (annotation != null) {
+			if (annotation.getValue(IEGLConstants.EGL_PARTLINE) != null)
+				startLine = ((Integer) annotation.getValue(IEGLConstants.EGL_PARTLINE)).intValue();
+			if (annotation.getValue(IEGLConstants.EGL_PARTOFFSET) != null)
+				startOffset = ((Integer) annotation.getValue(IEGLConstants.EGL_PARTOFFSET)).intValue();
+		}
+		EGLMessage message = EGLMessage.createEGLMessage(ctx.getMessageMapping(), EGLMessage.EGL_ERROR_MESSAGE,
+			Constants.EGLMESSAGE_MISSING_TEMPLATE_FOR_OBJECT, expr, details, startLine, startOffset, endLine, endOffset);
+		ctx.getMessageRequestor().addMessage(message);
 	}
 }
