9fc139d62261952bc9358113d24b6dce75488660
sgreer <sgreer>
2011-05-05 17:16:01 +0000

    Cleaned up genQualifier and added genContainerBasedAccessor

--- gen/org.eclipse.edt.gen.javascript/src/org/eclipse/edt/gen/javascript/templates/JavaScriptTemplate.java
+++ gen/org.eclipse.edt.gen.javascript/src/org/eclipse/edt/gen/javascript/templates/JavaScriptTemplate.java
@@ -83,6 +83,7 @@ public abstract class JavaScriptTemplate extends AbstractTemplate {
 	public static final String genUnaryExpression = "genUnaryExpression";
 	// constants that allow container based processing. each of these will attempt to find the user provided container based
 	// method, and if not found, will then end up back in type template and invoked the method name without "containerbased"
+	public static final String genContainerBasedAccessor = "genContainerBasedAccessor";
 	public static final String genContainerBasedArrayAccess = "genContainerBasedArrayAccess";
 	public static final String genContainerBasedAssignment = "genContainerBasedAssignment";
 	public static final String genContainerBasedMemberAccess = "genContainerBasedMemberAccess";
