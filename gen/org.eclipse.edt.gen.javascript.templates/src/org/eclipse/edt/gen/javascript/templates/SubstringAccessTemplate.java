7e523495c05d7ace5220531fc001001a92c3e4c9
jdouglas <jdouglas>
2011-05-13 17:20:36 +0000

    Updated substring logic and pushed it out to the type templates

--- gen/org.eclipse.edt.gen.javascript.templates/src/org/eclipse/edt/gen/javascript/templates/SubstringAccessTemplate.java
+++ gen/org.eclipse.edt.gen.javascript.templates/src/org/eclipse/edt/gen/javascript/templates/SubstringAccessTemplate.java
@@ -12,30 +12,17 @@
 package org.eclipse.edt.gen.javascript.templates;
 
 import org.eclipse.edt.gen.javascript.Context;
-
-import org.eclipse.edt.mof.egl.SubstringAccess;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.SubstringAccess;
+import org.eclipse.edt.mof.egl.Type;
 
 public class SubstringAccessTemplate extends JavaScriptTemplate {
 
 	public void genExpression(SubstringAccess expr, Context ctx, TabbedWriter out, Object... args) {
-		out.print(ctx.getNativeImplementationMapping(expr.getType()) + ".substring(");
-		ctx.gen(genExpression, expr.getStringExpression(), ctx, out, args);
-		out.print(", ");
-		ctx.gen(genExpression, expr.getStart(), ctx, out, args);
-		out.print(", ");
-		ctx.gen(genExpression, expr.getEnd(), ctx, out, args);
-		out.print(")");
+		ctx.gen(genSubstringAccess, (Type) expr.getType(), ctx, out, expr);
 	}
 
 	public void genAssignment(SubstringAccess expr, Context ctx, TabbedWriter out, Object... args) {
-		// TODO this needs work
-		out.print(ctx.getNativeImplementationMapping(expr.getType()) + ".substring(");
-		ctx.gen(genExpression, expr.getStringExpression(), ctx, out, args);
-		out.print(", ");
-		ctx.gen(genExpression, expr.getStart(), ctx, out, args);
-		out.print(", ");
-		ctx.gen(genExpression, expr.getEnd(), ctx, out, args);
-		out.print(")");
+		ctx.gen(genSubstringAssignment, (Type) expr.getType(), ctx, out, expr, args[0]);
 	}
 }
