84ebe1cd470217df9cfd6ea9f0779161d9470ba7
jdouglas <jdouglas>
2011-05-18 14:59:14 +0000

    Added program parameter template

---
+++ gen/org.eclipse.edt.gen.javascript.templates/src/org/eclipse/edt/gen/javascript/templates/ProgramParameterTemplate.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright © 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.edt.gen.javascript.templates;
+
+import org.eclipse.edt.gen.javascript.Context;
+import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.ProgramParameter;
+import org.eclipse.edt.mof.egl.utils.TypeUtils;
+
+public class ProgramParameterTemplate extends JavaScriptTemplate {
+
+	public void genDeclaration(ProgramParameter field, Context ctx, TabbedWriter out, Object... args) {}
+
+	public void genRuntimeTypeName(ProgramParameter mbr, Context ctx, TabbedWriter out, Object... args) {
+		if (mbr.getType() == null)
+			return;
+		else if (ctx.mapsToPrimitiveType(mbr.getType().getClassifier()) && !mbr.isNullable() && TypeUtils.isValueType(mbr.getType()))
+			ctx.gen(genRuntimeTypeName, mbr.getType(), ctx, out, TypeNameKind.JavascriptPrimitive);
+		else if (ctx.mapsToPrimitiveType(mbr.getType().getClassifier()))
+			ctx.gen(genRuntimeTypeName, mbr.getType(), ctx, out, TypeNameKind.JavascriptObject);
+		else if (ctx.mapsToNativeType(mbr.getType().getClassifier()))
+			ctx.gen(genRuntimeTypeName, mbr.getType(), ctx, out, TypeNameKind.EGLInterface);
+		else
+			ctx.gen(genRuntimeTypeName, mbr.getType(), ctx, out, TypeNameKind.JavascriptObject);
+	}
+}
