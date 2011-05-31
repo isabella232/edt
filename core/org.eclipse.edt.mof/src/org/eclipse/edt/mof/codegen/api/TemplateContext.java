662cd44cd0321de9d2d85993fc461d7e46fe5c59
twilson <twilson>
2011-05-31 13:26:59 +0000

    Addition of TemplateContext.invoke() and invokeSuper()

--- core/org.eclipse.edt.mof/src/org/eclipse/edt/mof/codegen/api/TemplateContext.java
+++ core/org.eclipse.edt.mof/src/org/eclipse/edt/mof/codegen/api/TemplateContext.java
@@ -11,10 +11,13 @@
  *******************************************************************************/
 package org.eclipse.edt.mof.codegen.api;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.edt.mof.EClass;
 import org.eclipse.edt.mof.EClassifier;
 import org.eclipse.edt.mof.EObject;
 
@@ -22,6 +25,16 @@ import org.eclipse.edt.mof.EObject;
 public class TemplateContext extends HashMap<Object, Object> {
 	TemplateFactory tFactory;
 	Map<String, Template> templates = new HashMap<String, Template>();
+	
+	private class TemplateMethod {
+		Method method;
+		Template template;
+		
+		TemplateMethod(Template template, Method method) {
+			this.template = template;
+			this.method = method;
+		}
+	}
 	
 	public void setTemplateFactory(TemplateFactory factory) {
 		this.tFactory = factory;
@@ -74,5 +87,163 @@ public class TemplateContext extends HashMap<Object, Object> {
 		Template template = getTemplateFor(object.getEClass());
 		template.validate(genMethod, object, ctx, args);
 	}
+	
+//	private TemplateMethod getTemplateMethod(String methodName, EClassifier classifier, Class<?>...classes) {
+//		TemplateMethod tm = null;
+//		Template template = null;
+//		Method method = null;
+//		try {
+//			template = getTemplateFor(classifier);
+//		}
+//		catch (TemplateException tex) {}
+//		if (template != null) {
+//			method = primGetMethod(methodName, template.getClass(), classes);
+//			if (method != null) {
+//				tm = new TemplateMethod(template, method);
+//			}
+//		}
+//		if (tm == null && classifier instanceof EClass) {
+//			for (Class<?> clazz : classifier.getClass().getInterfaces()) {
+//				classes[0] = clazz;
+//			}
+//			for (EClass eclazz : ((EClass)classifier).getSuperTypes()) {
+////				classes[0] = clazz.getClass().getInterfaces()[0];
+//				tm = getTemplateMethod(methodName, clazz, classes);
+//				if (tm != null) break;
+//			}
+//		}
+//		return tm;
+//	}
+	
+	private TemplateMethod getTemplateMethod(String methodName, Class<?> objectClass, Class<?>...classes) throws TemplateException {
+		TemplateMethod tm = null;
+		Method method = null;
+		Template template = null;
+		template = getTemplateForClass(objectClass);
+		if (template == null) {
+			// Try the interfaces if any
+			for (Class<?> iface : objectClass.getInterfaces()) {
+				template = getTemplateForClass(iface);
+				if (template != null) {
+					classes[0] = iface;
+					objectClass = iface;
+					break;
+				}
+			}
+		}
+		if (template != null) {
+			method = primGetMethod(methodName, template.getClass(), classes);
+			if (method != null) {
+				return new TemplateMethod(template, method);
+			}
+		}
+		if (objectClass.getSuperclass() != null) {
+			Class<?> superClass = objectClass.getSuperclass();
+			tm = getTemplateMethod(methodName, superClass, classes);
+		}
+		if (tm == null) {
+			for (Class<?> iface : objectClass.getInterfaces()) {
+				classes[0] = iface;
+				tm = getTemplateMethod(methodName, iface, classes);
+				if (tm != null) break;
+			}
+		}
+		if (tm == null) {
+			StringBuilder builder = new StringBuilder();
+			builder.append("No such method ");
+			builder.append(methodName);
+			builder.append("(");
+			builder.append(objectClass.getName());
+			builder.append(", ");
+			for (Class<?> arg : classes) {
+				builder.append(arg.getName());
+				builder.append(", ");
+			}
+			builder.append(") for any template for Class " );
+			builder.append(objectClass.getName());
+			throw new TemplateException(builder.toString());
 
+		}
+		return tm;
+	}
+	
+	private Template getTemplateForClass(Class<?> clazz) {
+		Template template = null;
+		try {
+			template = getTemplate(clazz.getName());
+		}
+		catch (TemplateException tex) {}
+		return template;
+	}
+
+	
+	private Method primGetMethod(String methodName, Class<?> templateClass, Class<?>...classes) {
+		Method method = null;
+		try {
+			method = templateClass.getMethod(methodName, classes);
+		} catch (SecurityException e) {
+			throw new RuntimeException(e);
+		} catch (NoSuchMethodException e) {
+			// Do nothing to allow search to continue
+		}
+		return method;
+	}	
+	
+	public Object invoke(String methodName, Object object, Object...args) {
+		return primInvoke(methodName, object.getClass(), object, args);
+	}
+	
+	public Object invokeSuper(String methodName, Object object, Object...args) {
+		Class<?> clazz = object.getClass().getSuperclass();
+		if (clazz == null) {
+			throw new IllegalArgumentException("Class " + object.getClass().getName() + " has no super class");
+		}
+		return primInvoke(methodName, clazz, object, args);
+
+	}
+
+	private Object primInvoke(String methodName, Class<?> objectClass, Object object, Object...args) {
+		Class<?>[] argClasses = new Class<?>[args.length+1];
+		argClasses[0] = objectClass;
+		for (int i=1; i<argClasses.length; i++) {
+			argClasses[i] = args[i-1].getClass();
+		}
+		TemplateMethod tm = getTemplateMethod(methodName, argClasses[0], argClasses);
+		if (tm != null) {
+			try {
+				return doInvoke(tm.method, tm.template, object, args);
+			}
+			catch (TemplateException te) {
+				throw te;
+			}
+			catch (InvocationTargetException itx) {
+				Throwable t = itx.getTargetException();
+				if (t instanceof TemplateException)
+					throw (TemplateException)t;
+				else 
+					throw new TemplateException(t);
+			}
+			catch (Exception e) {
+				throw new TemplateException(e);
+			}
+		}
+		return null;
+
+	}
+
+	private Object doInvoke(Method method, Template template, Object object, Object[] args) throws Exception {
+		switch (args.length) {
+		case 0: return method.invoke(template, object);
+		case 1: return method.invoke(template, object, args[0]);
+		case 2: return method.invoke(template, object, args[0], args[1]);
+		case 3: return method.invoke(template, object, args[0], args[1], args[2]);
+		case 4: return method.invoke(template, object, args[0], args[1], args[2], args[3]);
+		case 5: return method.invoke(template, object, args[0], args[1], args[2], args[3], args[4]);
+		case 6: return method.invoke(template, object, args[0], args[1], args[2], args[3], args[4], args[5]);
+		case 7: return method.invoke(template, object, args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
+		case 8: return method.invoke(template, object, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
+		case 9: return method.invoke(template, object, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
+		default: return method.invoke(template, object, args);
+		}
+	}
 }
