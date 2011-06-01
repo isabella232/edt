00c6dc59cbfc1ab38357ea079e039a1c647ec07f
jqian <jqian>
2011-06-01 17:33:28 +0000

    moved getRealtiveFileName and getExtension to the generator, rather than the AbstractGeneratorCommand, since each generator should know its output file name

--- gen/org.eclipse.edt.gen.generator.javascript/src/org/eclipse/edt/gen/generator/javascript/EGL2JavaScript.java
+++ gen/org.eclipse.edt.gen.generator.javascript/src/org/eclipse/edt/gen/generator/javascript/EGL2JavaScript.java
@@ -34,10 +34,6 @@ public class EGL2JavaScript extends AbstractGeneratorCommand {
 		genPart.generate(args, new JavaScriptGenerator(genPart), null);
 	}
 
-	public String getFileExtention() {
-		return ".js";
-	}
-
 	public String[] getNativeTypePath() {
 		// this defined the locations of the nativeTypes.properties files to be loaded and used
 		return new String[] { "org.eclipse.edt.gen.javascript.nativeTypes" };
@@ -58,21 +54,5 @@ public class EGL2JavaScript extends AbstractGeneratorCommand {
 			"org.eclipse.edt.gen.javascript.templates.templates" };
 	}
 
-	protected String getRelativeFileName(Part part) {
-		StringBuilder buf = new StringBuilder(50);
-		String pkg = part.getPackageName();
-		if (pkg.length() > 0) {
-			buf.append(Aliaser.packageNameAlias(pkg.split("[.]"), '/'));
-			buf.append('/');
-		}
-		String nameOrAlias;
-		Annotation annot = part.getAnnotation(IEGLConstants.PROPERTY_ALIAS);
-		if (annot != null)
-			nameOrAlias = (String) annot.getValue();
-		else
-			nameOrAlias = part.getId();
-		buf.append(Aliaser.getAlias(nameOrAlias));
-		buf.append(getFileExtention());
-		return buf.toString();
-	}
+
 }
