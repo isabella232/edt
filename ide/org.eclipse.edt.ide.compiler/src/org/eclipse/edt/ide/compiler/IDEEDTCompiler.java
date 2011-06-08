a6c01c188f96619bad927710043b79384e66e95e
pharmon <pharmon>
2011-06-08 14:41:44 +0000

    refactoring the compiler

---
+++ ide/org.eclipse.edt.ide.compiler/src/org/eclipse/edt/ide/compiler/IDEEDTCompiler.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright © 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.edt.ide.compiler;
+
+import org.eclipse.edt.ide.core.IDEBaseCompiler;
+
+public class IDEEDTCompiler extends IDEBaseCompiler {
+	
+	public IDEEDTCompiler() {
+		super();
+		baseCompiler = new org.eclipse.edt.compiler.EDTCompiler();
+		parentCompiler = new IDEBaseCompiler();
+	}
+	
+	protected String getSystemEnvironmentPathEntry() {
+		return getPathToPluginDirectory("org.eclipse.edt.compiler", "lib");
+	}
+
+
+}
