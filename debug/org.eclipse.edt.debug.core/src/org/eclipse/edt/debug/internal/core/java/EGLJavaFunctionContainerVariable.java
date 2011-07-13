bf983db0dd9f4fdbdcde749fcac657bf8b0fb3bb
jspadea <jspadea>
2011-07-13 19:55:04 +0000

    bugzilla 351987 - Can't find source when stepping through a program whose name was aliased

--- debug/org.eclipse.edt.debug.core/src/org/eclipse/edt/debug/internal/core/java/EGLJavaFunctionContainerVariable.java
+++ debug/org.eclipse.edt.debug.core/src/org/eclipse/edt/debug/internal/core/java/EGLJavaFunctionContainerVariable.java
@@ -14,6 +14,8 @@ package org.eclipse.edt.debug.internal.core.java;
 import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.model.IDebugTarget;
 import org.eclipse.debug.core.model.IValue;
+import org.eclipse.edt.debug.core.EDTDebugCorePlugin;
+import org.eclipse.jdt.debug.core.IJavaReferenceType;
 import org.eclipse.jdt.debug.core.IJavaValue;
 import org.eclipse.jdt.debug.core.IJavaVariable;
 
@@ -30,8 +32,33 @@ public class EGLJavaFunctionContainerVariable extends EGLJavaVariable
 	 */
 	public EGLJavaFunctionContainerVariable( IDebugTarget target, IJavaVariable javaVariable ) throws DebugException
 	{
-		super( target, javaVariable, new SMAPVariableInfo( javaVariable.getReferenceTypeName(),
-				javaVariable.getReferenceTypeName(), "", -1 ) ); //$NON-NLS-1$
+		super( target, javaVariable, new SMAPVariableInfo( getDisplayName( javaVariable ), javaVariable.getReferenceTypeName(), "", -1 ) ); //$NON-NLS-1$
+	}
+	
+	private static String getDisplayName( IJavaVariable variable ) throws DebugException
+	{
+		// The SMAP will have the full file path 'pkg/foo.egl' - turn that into 'pkg.foo'.
+		try
+		{
+			if ( variable.getJavaType() instanceof IJavaReferenceType )
+			{
+				String[] paths = ((IJavaReferenceType)variable.getJavaType()).getSourcePaths( EDTDebugCorePlugin.EGL_STRATUM );
+				if ( paths.length > 0 )
+				{
+					String path = paths[ 0 ];
+					int lastDot = path.lastIndexOf( '.' );
+					if ( lastDot != -1 )
+					{
+						path = path.substring( 0, lastDot );
+					}
+					return path.replace( '/', '.' );
+				}
+			}
+		}
+		catch ( DebugException e )
+		{
+		}
+		return variable.getReferenceTypeName();
 	}
 	
 	@Override
