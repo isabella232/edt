50a4339cefa30de7be20edffd4cc9f642a859d1d
jspadea <jspadea>
2011-07-29 19:28:17 +0000

    bugzillas 352771 & 352021 - add variable adapter framework, support arrays, and provide some default adapters for basic java types

--- debug/org.eclipse.edt.debug.ui/src/org/eclipse/edt/debug/internal/ui/java/EGLJavaModelPresentation.java
+++ debug/org.eclipse.edt.debug.ui/src/org/eclipse/edt/debug/internal/ui/java/EGLJavaModelPresentation.java
@@ -11,13 +11,13 @@
  *******************************************************************************/
 package org.eclipse.edt.debug.internal.ui.java;
 
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.debug.core.model.IValue;
 import org.eclipse.debug.ui.IValueDetailListener;
-import org.eclipse.edt.debug.internal.core.java.EGLJavaDebugElement;
-import org.eclipse.edt.debug.internal.core.java.EGLJavaFunctionContainerValue;
-import org.eclipse.edt.debug.internal.core.java.EGLJavaFunctionValue;
+import org.eclipse.edt.debug.core.java.IEGLJavaDebugElement;
+import org.eclipse.edt.debug.core.java.IEGLJavaValue;
+import org.eclipse.edt.debug.core.java.IEGLJavaVariable;
 import org.eclipse.edt.debug.internal.core.java.EGLJavaFunctionVariable;
-import org.eclipse.edt.debug.internal.core.java.EGLJavaValue;
 import org.eclipse.jdt.internal.debug.ui.JDIModelPresentation;
 import org.eclipse.jdt.ui.ISharedImages;
 import org.eclipse.jdt.ui.JavaUI;
@@ -32,9 +32,9 @@ public class EGLJavaModelPresentation extends JDIModelPresentation
 	@Override
 	public String getText( Object element )
 	{
-		if ( element instanceof EGLJavaDebugElement )
+		if ( element instanceof IEGLJavaDebugElement )
 		{
-			element = ((EGLJavaDebugElement)element).getJavaElement();
+			element = ((IEGLJavaDebugElement)element).getJavaDebugElement();
 		}
 		
 		return super.getText( element );
@@ -43,15 +43,21 @@ public class EGLJavaModelPresentation extends JDIModelPresentation
 	@Override
 	public void computeDetail( IValue value, IValueDetailListener listener )
 	{
-		if ( value instanceof EGLJavaFunctionValue || value instanceof EGLJavaFunctionContainerValue )
+		if ( value instanceof IEGLJavaValue )
 		{
-			listener.detailComputed( value, "" ); //$NON-NLS-1$
-			return;
-		}
-		
-		if ( value instanceof EGLJavaValue )
-		{
-			value = ((EGLJavaValue)value).getJavaValue();
+			IEGLJavaValue eglValue = (IEGLJavaValue)value;
+			
+			String detail = eglValue.computeDetail();
+			if ( detail != null )
+			{
+				listener.detailComputed( value, detail );
+				return;
+			}
+			
+			if ( eglValue.getJavaValue() != null )
+			{
+				value = eglValue.getJavaValue();
+			}
 		}
 		
 		super.computeDetail( value, listener );
@@ -62,13 +68,25 @@ public class EGLJavaModelPresentation extends JDIModelPresentation
 	{
 		if ( item instanceof EGLJavaFunctionVariable )
 		{
-			return getJavaElementImageRegistry().get(
-					JavaUI.getSharedImages().getImageDescriptor( ISharedImages.IMG_OBJS_LOCAL_VARIABLE ) );
+			return getJavaElementImageRegistry().get( JavaUI.getSharedImages().getImageDescriptor( ISharedImages.IMG_OBJS_LOCAL_VARIABLE ) );
 		}
 		
-		if ( item instanceof EGLJavaDebugElement )
+		if ( item instanceof IAdaptable )
 		{
-			item = ((EGLJavaDebugElement)item).getJavaElement();
+			IEGLJavaVariable var = (IEGLJavaVariable)((IAdaptable)item).getAdapter( IEGLJavaVariable.class );
+			if ( var != null )
+			{
+				if ( var.isLocal() )
+				{
+					return getJavaElementImageRegistry().get( JavaUI.getSharedImages().getImageDescriptor( ISharedImages.IMG_OBJS_LOCAL_VARIABLE ) );
+				}
+				return getJavaElementImageRegistry().get( JavaUI.getSharedImages().getImageDescriptor( ISharedImages.IMG_OBJS_PUBLIC ) );
+			}
+		}
+		
+		if ( item instanceof IEGLJavaDebugElement )
+		{
+			item = ((IEGLJavaDebugElement)item).getJavaDebugElement();
 		}
 		
 		return super.getImage( item );
