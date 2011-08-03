de536c468712014a75096ffda9932cb0fdbaaaf6
mheitz <mheitz>
2011-08-03 21:22:14 +0000

    Added InvalidPatternException.

---
+++ runtimes/org.eclipse.edt.runtime.java/src/org/eclipse/edt/runtime/java/egl/lang/InvalidPatternException.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright © 2006, 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.edt.runtime.java.egl.lang;
+
+import org.eclipse.edt.javart.Constants;
+
+public class InvalidPatternException extends AnyException
+{
+	private static final long serialVersionUID = Constants.SERIAL_VERSION_UID;
+	
+	public InvalidPatternException() 
+	{
+	}
+}
