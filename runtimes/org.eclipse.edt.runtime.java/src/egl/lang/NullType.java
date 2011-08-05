fa2beda5ff9a2ce3486ded8fd5659531327b96e1
jdouglas <jdouglas>
2011-08-05 02:10:33 +0000

    Bug 353790 - NullType logic added

---
+++ runtimes/org.eclipse.edt.runtime.java/src/egl/lang/NullType.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright © 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *
+ *******************************************************************************/
+package egl.lang;
+
+import org.eclipse.edt.javart.AnyBoxedObject;
+
+public interface NullType extends EglAny { 	
+		
+	public <R extends Object> Boolean equals(AnyBoxedObject<R> s1, AnyBoxedObject<R> s2);
+	public <R extends Object> Boolean notEquals(AnyBoxedObject<R> s1, AnyBoxedObject<R> s2);
+	public NullType asNullType(EglAny source);
+	
+}
