5b88e15bd7a4c199d66521f6e4d7f48d094171b1
jdouglas <jdouglas>
2011-08-05 14:49:59 +0000

    Bug 353525 - error in function temporary variables being passed to the next function

--- gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/MemberNameTemplate.java
+++ gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/MemberNameTemplate.java
@@ -29,6 +29,11 @@ public class MemberNameTemplate extends JavaTemplate {
 			ctx.invoke(genAccessor, expr.getMember(), ctx, out);
 			out.print(".ezeCopy(");
 			ctx.invoke(genExpression, arg1, ctx, out);
+			// check to see if we are unboxing RHS temporary variables (inout and out types only)
+			if (arg1 instanceof MemberName
+				&& ctx.getAttribute(((MemberName) arg1).getMember(), org.eclipse.edt.gen.Constants.SubKey_functionArgumentTemporaryVariable) != null
+				&& ctx.getAttribute(((MemberName) arg1).getMember(), org.eclipse.edt.gen.Constants.SubKey_functionArgumentTemporaryVariable) != ParameterKind.PARM_IN)
+				out.print(".ezeUnbox()");
 			out.print(")");
 			// check to see if we are copying LHS boxed temporary variables (inout and out types only)
 		} else if (ctx.getAttribute(expr.getMember(), org.eclipse.edt.gen.Constants.SubKey_functionArgumentTemporaryVariable) != null
@@ -37,6 +42,11 @@ public class MemberNameTemplate extends JavaTemplate {
 			out.print(arg2);
 			out.print("EglAny.ezeWrap(");
 			ctx.invoke(genExpression, arg1, ctx, out);
+			// check to see if we are unboxing RHS temporary variables (inout and out types only)
+			if (arg1 instanceof MemberName
+				&& ctx.getAttribute(((MemberName) arg1).getMember(), org.eclipse.edt.gen.Constants.SubKey_functionArgumentTemporaryVariable) != null
+				&& ctx.getAttribute(((MemberName) arg1).getMember(), org.eclipse.edt.gen.Constants.SubKey_functionArgumentTemporaryVariable) != ParameterKind.PARM_IN)
+				out.print(".ezeUnbox()");
 			out.print(")");
 			// check to see if we are unboxing RHS temporary variables (inout and out types only)
 		} else if (arg1 instanceof MemberName
