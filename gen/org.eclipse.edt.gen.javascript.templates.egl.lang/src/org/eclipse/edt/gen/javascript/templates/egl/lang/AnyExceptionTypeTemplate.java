e1d814c54dca35bddeed766f76af652ae59a4e82
jdouglas <jdouglas>
2011-08-05 14:15:15 +0000

    *** empty log message ***

---
+++ gen/org.eclipse.edt.gen.javascript.templates.egl.lang/src/org/eclipse/edt/gen/javascript/templates/egl/lang/AnyExceptionTypeTemplate.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright © 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.edt.gen.javascript.templates.egl.lang;
+
+import org.eclipse.edt.gen.javascript.Context;
+import org.eclipse.edt.gen.javascript.templates.JavaScriptTemplate;
+import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.Type;
+
+public class AnyExceptionTypeTemplate extends JavaScriptTemplate {
+
+	public void genDefaultValue(Type type, Context ctx, TabbedWriter out) {
+		// this type has no default value
+	}
+
+	public void genRuntimeTypeName(Type type, Context ctx, TabbedWriter out, TypeNameKind arg) {
+		ctx.invoke(genPartName, type, ctx, out);
+	}
+}
