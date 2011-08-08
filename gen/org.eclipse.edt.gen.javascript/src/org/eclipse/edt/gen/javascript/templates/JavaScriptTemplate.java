229a9b4e81af8fb0f002187f7737d9ea62d55f56
jdouglas <jdouglas>
2011-08-08 19:47:14 +0000

    Date/timestamp logic, plus container based invocation logic, and removal of dead library code

--- gen/org.eclipse.edt.gen.javascript/src/org/eclipse/edt/gen/javascript/templates/JavaScriptTemplate.java
+++ gen/org.eclipse.edt.gen.javascript/src/org/eclipse/edt/gen/javascript/templates/JavaScriptTemplate.java
@@ -75,6 +75,7 @@ public abstract class JavaScriptTemplate extends AbstractTemplate {
 	public static final String genMemberName = "genMemberName";
 	public static final String genName = "genName";
 	public static final String genNamespaceMap = "genNamespaceMap";
+	public static final String genNewExpression = "genNewExpression";
 	public static final String genPart = "genPart";
 	public static final String genPartName = "genPartName";
 	public static final String genQualifier = "genQualifier";
@@ -101,6 +102,8 @@ public abstract class JavaScriptTemplate extends AbstractTemplate {
 	public static final String genContainerBasedAssignment = "genContainerBasedAssignment";
 	public static final String genContainerBasedMemberAccess = "genContainerBasedMemberAccess";
 	public static final String genContainerBasedMemberName = "genContainerBasedMemberName";
+	public static final String genContainerBasedNewExpression = "genContainerBasedNewExpression";
+	public static final String genContainerBasedInvocation = "genContainerBasedInvocation";
 
 	//get functions
 	public static final String getCallbackFunction = "getCallbackFunction";
