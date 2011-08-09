f4fcc383d11f1e28c96486ac838cba95e8a849d8
jdouglas <jdouglas>
2011-08-09 16:05:19 +0000

    Bug 353922 - support for date and timestamp conversions

--- gen/org.eclipse.edt.gen.java.templates.egl.lang/src/org/eclipse/edt/gen/java/templates/egl/lang/AnyTypeTemplate.java
+++ gen/org.eclipse.edt.gen.java.templates.egl.lang/src/org/eclipse/edt/gen/java/templates/egl/lang/AnyTypeTemplate.java
@@ -11,36 +11,6 @@
  *******************************************************************************/
 package org.eclipse.edt.gen.java.templates.egl.lang;
 
-import org.eclipse.edt.gen.java.Context;
 import org.eclipse.edt.gen.java.templates.JavaTemplate;
-import org.eclipse.edt.mof.codegen.api.TabbedWriter;
-import org.eclipse.edt.mof.egl.AsExpression;
-import org.eclipse.edt.mof.egl.Classifier;
-import org.eclipse.edt.mof.egl.Type;
 
-public class AnyTypeTemplate extends JavaTemplate {
-
-	public void genConversionOperation(Type type, Context ctx, TabbedWriter out, AsExpression arg) {
-		// check to see if a conversion is required
-		if (arg.getConversionOperation() != null) {
-			out.print(ctx.getNativeImplementationMapping((Classifier) arg.getConversionOperation().getContainer()) + '.');
-			out.print(arg.getConversionOperation().getName());
-			out.print("(");
-			ctx.invoke(genExpression, arg.getObjectExpr(), ctx, out);
-			ctx.invoke(genTypeDependentOptions, arg.getEType(), ctx, out);
-			out.print(")");
-		} else if (ctx.mapsToPrimitiveType(arg.getEType())) {
-			ctx.invoke(genRuntimeTypeName, arg.getEType(), ctx, out, TypeNameKind.EGLImplementation);
-			out.print(".ezeCast(");
-			ctx.invoke(genExpression, arg.getObjectExpr(), ctx, out);
-			ctx.invoke(genTypeDependentOptions, arg.getEType(), ctx, out);
-			out.print(")");
-		} else {
-			out.print("EglAny.ezeCast(");
-			ctx.invoke(genExpression, arg.getObjectExpr(), ctx, out);
-			out.print(", ");
-			ctx.invoke(genRuntimeTypeName, arg.getEType(), ctx, out, TypeNameKind.JavaImplementation);
-			out.print(".class)");
-		}
-	}
-}
+public class AnyTypeTemplate extends JavaTemplate {}
