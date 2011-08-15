08b66ff63caec02c405d6d868f627eff696855bf
mheitz <mheitz>
2011-08-15 16:54:48 +0000

    353663: correct the code that finds ExternalTypes.

--- gen/org.eclipse.edt.gen.java/src/org/eclipse/edt/gen/java/CommonUtilities.java
+++ gen/org.eclipse.edt.gen.java/src/org/eclipse/edt/gen/java/CommonUtilities.java
@@ -13,7 +13,6 @@ package org.eclipse.edt.gen.java;
 
 import java.util.List;
 
-import org.eclipse.edt.compiler.binding.annotationType.EGLIsSystemPartAnnotationTypeBinding;
 import org.eclipse.edt.compiler.core.IEGLConstants;
 import org.eclipse.edt.gen.GenerationException;
 import org.eclipse.edt.mof.EObject;
@@ -78,50 +77,46 @@ public class CommonUtilities {
 	}
 
 	/**
-	 * Returns true if the type is an ExternalType, with subtype JavaObject or NativeType, and does not have {
-	 * eglIsSystemPart = yes }.
+	 * Returns true if the type is an ExternalType with subtype JavaObject or 
+	 * NativeType.
 	 */
-	public static boolean isUserDefinedExternalType(Type type) {
-		return getUserDefinedExternalType(type) != null;
+	public static boolean isJavaExternalType(Type type) {
+		return getJavaExternalType(type) != null;
 	}
 
 	/**
-	 * Returns the Type if it's an ExternalType, with subtype JavaObject or NativeType, and does not have { eglIsSystemPart =
-	 * yes }. If it's something else, returns null.
+	 * Returns the Type if it's an ExternalType, with subtype JavaObject or 
+	 * NativeType. If it's something else, returns null.
 	 */
-	public static ExternalType getUserDefinedExternalType(Type type) {
-		if (type instanceof Part) {
+	public static ExternalType getJavaExternalType(Type type) {
+		if (type instanceof ExternalType) {
+			if ( type.getAnnotation( "eglx.java.JavaObject" ) != null
+					|| type.getAnnotation( "egl.lang.NativeType" ) != null )
+			{
+				return (ExternalType) type;
+			}
+			else
+			{
+				return null;
+			}
+		} else if (type instanceof Part) {
 			Part member = (Part) type;
 			if (member instanceof ExternalType) {
-				if (member.getAnnotation(EGLIsSystemPartAnnotationTypeBinding.name) == null) {
-					return (ExternalType) member;
-				} else {
-					Annotation annot = member.getAnnotation(IEGLConstants.EXTERNALTYPE_SUBTYPE_JAVAOBJECT);
-					if (annot != null) {
-						String value = (String) annot.getValue(IEGLConstants.PROPERTY_PACKAGENAME);
-						if (value != null && (value.startsWith("java.") || value.startsWith("javax."))) {
-							return (ExternalType) member;
-						}
-					}
+				if ( member.getAnnotation( "eglx.java.JavaObject" ) != null
+						|| member.getAnnotation( "egl.lang.NativeType" ) != null )
+				{
+					return (ExternalType)member;
 				}
-			}
-		} else if (type instanceof ExternalType) {
-			if (type.getAnnotation(EGLIsSystemPartAnnotationTypeBinding.name) == null) {
-				return (ExternalType) type;
-			} else {
-				Annotation annot = type.getAnnotation(IEGLConstants.EXTERNALTYPE_SUBTYPE_JAVAOBJECT);
-				if (annot != null) {
-					String value = (String) annot.getValue(IEGLConstants.PROPERTY_PACKAGENAME);
-					if (value != null && (value.startsWith("java.") || value.startsWith("javax."))) {
-						return (ExternalType) type;
-					}
+				else
+				{
+					return null;
 				}
 			}
 		} else if (type instanceof ArrayType) {
 			while (type instanceof ArrayType) {
 				type = ((ArrayType) type).getElementType();
 			}
-			return getUserDefinedExternalType(type);
+			return getJavaExternalType(type);
 		}
 		return null;
 	}
