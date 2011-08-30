7af50f9704b9494be4488941102f75c1dc5a5736
twilson <twilson>
2011-08-30 20:36:47 +0000

    Updates to include SQL templates

--- gen/org.eclipse.edt.gen.generator.java/src/org/eclipse/edt/gen/generator/java/EGL2Java.java
+++ gen/org.eclipse.edt.gen.generator.java/src/org/eclipse/edt/gen/generator/java/EGL2Java.java
@@ -47,7 +47,9 @@ public class EGL2Java extends AbstractGeneratorCommand {
 
 	public String[] getTemplatePath() {
 		// this defined the locations of the templates.properties files to be loaded and used
-		return new String[] { "org.eclipse.edt.gen.java.templates.egl.lang.templates", "org.eclipse.edt.gen.java.templates.templates" };
+		return new String[] { "org.eclipse.edt.gen.java.templates.egl.lang.templates",
+				"org.eclipse.edt.gen.java.templates.eglx.persistence.sql.templates",
+				"org.eclipse.edt.gen.java.templates.templates" };
 	}
 
 }
