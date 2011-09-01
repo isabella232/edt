4b450500a818e91942f6beea92267f5af695323f
sgreer <sgreer>
2011-09-01 19:33:44 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=356165

--- gen/org.eclipse.edt.gen.javascript.templates/src/org/eclipse/edt/gen/javascript/templates/RecordTemplate.java
+++ gen/org.eclipse.edt.gen.javascript.templates/src/org/eclipse/edt/gen/javascript/templates/RecordTemplate.java
@@ -50,6 +50,7 @@ public class RecordTemplate extends JavaScriptTemplate {
 	public void genConstructor(Record part, Context ctx, TabbedWriter out) {
 		out.print(quoted("constructor"));
 		out.println(": function() {");
+		ctx.invoke(genLibraries, part, ctx, out);
 		out.println("this.eze$$setInitial();");
 		out.println("}");
 		out.println(",");
