f50d83933a4001ff44b070f84f6a5b9401c643ec
mheitz <mheitz>
2011-09-01 16:05:02 +0000

    Bug 352362 - Generate settings of ordering and caseSensitive.

--- gen/org.eclipse.edt.gen.java.templates.egl.lang/src/org/eclipse/edt/gen/java/templates/egl/lang/DictionaryTypeTemplate.java
+++ gen/org.eclipse.edt.gen.java.templates.egl.lang/src/org/eclipse/edt/gen/java/templates/egl/lang/DictionaryTypeTemplate.java
@@ -11,8 +11,51 @@
  *******************************************************************************/
 package org.eclipse.edt.gen.java.templates.egl.lang;
 
+import org.eclipse.edt.gen.java.Context;
 import org.eclipse.edt.gen.java.templates.JavaTemplate;
+import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.*;
 
-public class DictionaryTypeTemplate extends JavaTemplate {
-
+public class DictionaryTypeTemplate extends JavaTemplate 
+{
+	private static final String FIELD_KEY = "org.eclipse.edt.gen.java.templates.egl.lang.DictionaryTypeTemplate.FIELD_KEY";
+	
+	public void genConstructorOptions( Type type, Context ctx, TabbedWriter out ) 
+	{
+		Field field = (Field)ctx.remove( FIELD_KEY );
+		if ( field != null )
+		{
+			Annotation caseSense = field.getAnnotation( "eglx.lang.CaseSensitive" ); 
+			Annotation ordering = field.getAnnotation( "eglx.lang.Ordering" );
+			if ( caseSense != null || ordering != null )
+			{
+				boolean caseSenseValue = caseSense != null ? (Boolean)caseSense.getValue() : false;
+				String orderingValue = ordering != null ? ordering.getValue().toString() : "none";
+				out.print( caseSenseValue );
+				out.print( ", eglx.lang.OrderingKind." );
+				out.print( orderingValue );
+			}
+		}
+	}
+	
+	public void genAssignment( Type type, Context ctx, TabbedWriter out, Expression lhs, Expression rhs, String operator )
+	{
+		// If the target is a Field and the source is a NewExpression, save the Field
+		// so we can use its annotations for arguments to the constructor.
+		Field field = null;
+		if ( lhs instanceof ArrayAccess && ((Name)((ArrayAccess)lhs).getArray()).getNamedElement() instanceof Field )
+		{
+			field = (Field)((Name)((ArrayAccess)lhs).getArray()).getNamedElement();
+		}
+		else if ( lhs instanceof Name && ((Name)lhs).getNamedElement() instanceof Field )
+		{
+			field = (Field)((Name)lhs).getNamedElement();
+		}
+		
+		if ( field != null && rhs instanceof NewExpression )
+		{
+			ctx.put( FIELD_KEY, field );
+		}
+		ctx.invokeSuper( this, genAssignment, type, ctx, out, lhs, rhs, operator );
+	}
 }
