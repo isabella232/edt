a39399c8caa57ab8be794de6556596a037b18591
tchen <tchen>
2011-09-05 09:54:12 +0000

    Bug 356704 - Auto open suitable editor

---
+++ ide/org.eclipse.edt.ide.rui.visualeditor/src/org/eclipse/edt/ide/rui/visualeditor/internal/editor/RUIFileDescriber.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright Â© 2008, 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.edt.ide.rui.visualeditor.internal.editor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.core.runtime.content.IContentDescriber;
+import org.eclipse.core.runtime.content.IContentDescription;
+import org.eclipse.core.runtime.content.ITextContentDescriber;
+
+public class RUIFileDescriber implements ITextContentDescriber {	
+	private Pattern handlerPattern; 
+	private Pattern widgetPattern;
+	
+	public RUIFileDescriber() {
+		//to match handler XXX type RUIhandler
+		handlerPattern = Pattern.compile("\\bHANDLER\\b\\s\\s*.*\\s*\\s\\bTYPE\\b\\s\\s*\\bRUIHANDLER\\b", Pattern.CASE_INSENSITIVE);
+		//to match handler XXX type RUIwidget
+		widgetPattern = Pattern.compile("\\bHANDLER\\b\\s\\s*.*\\s*\\s\\bTYPE\\b\\s\\s*\\bRUIWIDGET\\b", Pattern.CASE_INSENSITIVE);
+	}
+
+	@Override
+	public int describe(InputStream contents, IContentDescription description) throws IOException {		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(contents, "UTF-8"));
+		String line;
+		while((line = reader.readLine()) != null){
+			Matcher handlerMatcher = handlerPattern.matcher(line);
+			Matcher widgetMatcher = widgetPattern.matcher(line);
+			if(handlerMatcher.find() || widgetMatcher.find()){
+				return IContentDescriber.VALID;
+			}
+		}
+		return IContentDescriber.INVALID;
+	}
+
+	@Override
+	public QualifiedName[] getSupportedOptions() {
+		return new QualifiedName[0];
+	}
+
+	@Override
+	public int describe(Reader contents, IContentDescription description) throws IOException {
+		return IContentDescriber.INVALID;
+	}
+
+}
