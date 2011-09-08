add2352359d9f5fb7c1fd6a59bb1fbe2369c9c6c
mheitz <mheitz>
2011-09-08 19:21:40 +0000

    Removed an unused constant.

--- gen/org.eclipse.edt.gen/src/org/eclipse/edt/gen/Constants.java
+++ gen/org.eclipse.edt.gen/src/org/eclipse/edt/gen/Constants.java
@@ -44,7 +44,6 @@ public class Constants {
 	public static final String AnnotationBindService  = "eglx.services.BindService";
 	public static final String PartHttp  = "eglx.http.Http";
 	public static final String LibraryService  = "eglx.services.ServiceLib";
-	public static final String Annotation_EGLProperty = "eglx.lang.eglproperty";
 	public static final String AnnotationXMLStructureKind  = "eglx.xml.binding.annotation.XMLStructureKind";
 	public static final String AnnotationXMLValue  = "eglx.xml.binding.annotation.XMLValue";
 }
