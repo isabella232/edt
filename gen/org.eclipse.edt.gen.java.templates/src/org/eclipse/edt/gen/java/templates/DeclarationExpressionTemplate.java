91dd3937d417fa6c3eda5c91346717522d222ff9
jdouglas <jdouglas>
2011-09-08 15:19:02 +0000

    Fixed the combining logic

--- gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/DeclarationExpressionTemplate.java
+++ gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/DeclarationExpressionTemplate.java
@@ -13,9 +13,18 @@ package org.eclipse.edt.gen.java.templates;
 
 import org.eclipse.edt.gen.java.CommonUtilities;
 import org.eclipse.edt.gen.java.Context;
+import org.eclipse.edt.mof.EObject;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.AssignmentStatement;
 import org.eclipse.edt.mof.egl.DeclarationExpression;
+import org.eclipse.edt.mof.egl.DelegateInvocation;
+import org.eclipse.edt.mof.egl.Expression;
 import org.eclipse.edt.mof.egl.Field;
+import org.eclipse.edt.mof.egl.FunctionInvocation;
+import org.eclipse.edt.mof.egl.InvocationExpression;
+import org.eclipse.edt.mof.egl.QualifiedFunctionInvocation;
+import org.eclipse.edt.mof.egl.StatementBlock;
+import org.eclipse.edt.mof.impl.AbstractVisitor;
 
 public class DeclarationExpressionTemplate extends JavaTemplate {
 
@@ -26,16 +35,93 @@ public class DeclarationExpressionTemplate extends JavaTemplate {
 			// process the field
 			ctx.invoke(genRuntimeTypeName, field, ctx, out, TypeNameKind.JavaPrimitive);
 			out.print(" ");
-			ctx.invoke(genName, field, ctx, out);
-			out.print(" = ");
-			ctx.invoke(genInitialization, field, ctx, out);
-			out.println(";");
-			// as this is an expression that also creates a new line with the above println method, it throws off the smap
-			// ending line number by 1. We need to issue a call to correct this
-			ctx.setSmapLastJavaLineNumber(out.getLineNumber() - 1);
-			// now check for any statements to be processed
-			if (field.getInitializerStatements() != null)
-				ctx.invoke(genStatementNoBraces, field.getInitializerStatements(), ctx, out);
+			// check to see if it is safe to combine a declaration with an assignment. to do this, we need to check to see if
+			// there is a set of initializer statements and if the 1st one is an assignment that uses the same field and the
+			// assignment doesn't have side effects, then we are okay to combine
+			if (field.getInitializerStatements() != null
+				&& field.getInitializerStatements() instanceof StatementBlock
+				&& ((StatementBlock) field.getInitializerStatements()).getStatements().size() > 0
+				&& ((StatementBlock) field.getInitializerStatements()).getStatements().get(0) instanceof AssignmentStatement
+				&& !hasSideEffects(((AssignmentStatement) ((StatementBlock) field.getInitializerStatements()).getStatements().get(0)).getAssignment().getRHS(),
+					ctx)) {
+				// this logic will combine
+				ctx.invoke(genInitializeStatement, field.getType(), ctx, out, field);
+				// as this is an expression that also creates a new line with the above println method, it throws off the
+				// smap ending line number by 1. We need to issue a call to correct this
+				ctx.setSmapLastJavaLineNumber(out.getLineNumber() - 1);
+			} else {
+				ctx.invoke(genName, field, ctx, out);
+				out.print(" = ");
+				// this logic will not combine, because it isn't safe to
+				ctx.invoke(genInitialization, field, ctx, out);
+				out.println(";");
+				// as this is an expression that also creates a new line with the above println method, it throws off the
+				// smap ending line number by 1. We need to issue a call to correct this
+				ctx.setSmapLastJavaLineNumber(out.getLineNumber() - 1);
+				// now check for any statements to be processed
+				if (field.getInitializerStatements() != null)
+					ctx.invoke(genStatementNoBraces, field.getInitializerStatements(), ctx, out);
+			}
+		}
+	}
+
+	public static boolean hasSideEffects(Expression expr, Context ctx) {
+		return (new CheckSideEffects()).checkSideEffect(expr, ctx);
+	}
+
+	public static class CheckSideEffects extends AbstractVisitor {
+		boolean has = false;
+		Context ctx;
+
+		public boolean checkSideEffect(Expression expr, Context ctx) {
+			this.ctx = ctx;
+			disallowRevisit();
+			setReturnData(false);
+			expr.accept(this);
+			return (Boolean) getReturnData();
+		}
+
+		public boolean visit(EObject obj) {
+			return false;
+		}
+
+		public boolean visit(Expression expr) {
+			if (has)
+				return false;
+			return true;
+		}
+
+		public boolean visit(FunctionInvocation expr) {
+			processInvocation(expr);
+			has = true;
+			setReturnData(has);
+			return false;
+		}
+
+		public boolean visit(DelegateInvocation expr) {
+			processInvocation(expr);
+			return false;
+		}
+
+		public boolean visit(QualifiedFunctionInvocation expr) {
+			processInvocation(expr);
+			return false;
+		}
+
+		private void processInvocation(InvocationExpression object) {
+			// we need to scan the function arguments for any conditions that require temporary variables to be set
+			// up. Things like IN args, INOUT args with java primitives, OUT arg initialization, etc. We also need to
+			// remember when this statement has already been processed for function invocations, and ignore on
+			// subsequent attempts
+			// first determine whether we are going to modify the argument and set up pre/post assignments
+			for (int i = 0; i < object.getTarget().getParameters().size(); i++) {
+				if (org.eclipse.edt.gen.CommonUtilities.isArgumentToBeAltered(object.getTarget().getParameters().get(i), object.getArguments().get(i), ctx)) {
+					has = true;
+					setReturnData(has);
+					break;
+				}
+			}
+			return;
 		}
 	}
 }
