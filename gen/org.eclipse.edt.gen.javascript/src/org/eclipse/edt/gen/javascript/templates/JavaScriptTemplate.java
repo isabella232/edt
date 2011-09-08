74fc187f06b8da894befa4ba1361a99a07195c89
sgreer <sgreer>
2011-09-08 19:52:15 +0000

    * https://bugs.eclipse.org/bugs/show_bug.cgi?id=356429
      This is the fix for "forward references":
    
      a) setEmpty now gens default initializers for all fields
      b) setInit now gens initializerStatements for those fields that have them
    
      At runtime, the flow will be
      1) ctor now calls setInit
      2) set Init calls setEmpty, and then executes the initializerStatements

--- gen/org.eclipse.edt.gen.javascript/src/org/eclipse/edt/gen/javascript/templates/JavaScriptTemplate.java
+++ gen/org.eclipse.edt.gen.javascript/src/org/eclipse/edt/gen/javascript/templates/JavaScriptTemplate.java
@@ -71,6 +71,7 @@ public abstract class JavaScriptTemplate extends AbstractTemplate {
 	public static final String genInitializeMethod = "genInitializeMethod";
 	public static final String genInitializeMethodBody = "genInitializeMethodBody";
 	public static final String genInitializeMethods = "genInitializeMethods";
+	public static final String genInitializeStatement = "genInitializeStatement";
 	public static final String genInstantiation = "genInstantiation";
 	public static final String genInvocation = "genInvocation";
 	public static final String genJsonAnnotations = "genJsonAnnotations";
