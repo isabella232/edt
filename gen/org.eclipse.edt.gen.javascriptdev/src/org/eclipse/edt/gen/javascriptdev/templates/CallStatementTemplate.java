6d23256a791945ff4a02ab4951457f327a9c6a0c
jspadea <jspadea>
2011-09-08 15:32:20 +0000

    do not invoke services from the VE design page

---
+++ gen/org.eclipse.edt.gen.javascriptdev/src/org/eclipse/edt/gen/javascriptdev/templates/CallStatementTemplate.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright © 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.edt.gen.javascriptdev.templates;
+
+import org.eclipse.edt.gen.javascript.Context;
+import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.CallStatement;
+
+public class CallStatementTemplate extends org.eclipse.edt.gen.javascript.templates.CallStatementTemplate {
+	@Override
+	public void genStatementBody(CallStatement stmt, Context ctx, TabbedWriter out) {
+		out.println("if (egl.enableEditing !== true) {");
+		super.genStatementBody(stmt, ctx, out);
+		out.println("}");
+	}
+}
