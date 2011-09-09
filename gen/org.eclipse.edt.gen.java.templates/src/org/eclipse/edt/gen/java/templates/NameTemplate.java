7a187dfafae49fda736c9941479c67ea1fdf0a8a
mheitz <mheitz>
2011-09-09 13:17:42 +0000

    Bug 353652 - Support @Property and @EGLProperty.

--- gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/NameTemplate.java
+++ gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/NameTemplate.java
@@ -11,42 +11,32 @@
  *******************************************************************************/
 package org.eclipse.edt.gen.java.templates;
 
-import org.eclipse.edt.gen.java.Constants;
+import org.eclipse.edt.gen.java.CommonUtilities;
 import org.eclipse.edt.gen.java.Context;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
-import org.eclipse.edt.mof.egl.Annotation;
-import org.eclipse.edt.mof.egl.Expression;
-import org.eclipse.edt.mof.egl.Name;
+import org.eclipse.edt.mof.egl.*;
 
-public class NameTemplate extends JavaTemplate {
-
-	public Annotation getPropertyAnnotation(Name expr) {
-		return expr.getNamedElement().getAnnotation(Constants.Annotation_EGLProperty);
-	}
-
-	public void genAssignment(Name expr, Context ctx, TabbedWriter out, Expression arg1, String arg2) {
-		Annotation property = getPropertyAnnotation(expr);
-		if (property != null) {
-			if (expr.getQualifier() != null) {
-				ctx.invoke(genExpression, expr.getQualifier(), ctx, out);
-				out.print(".");
-				if (property.getValue("setMethod") != null)
-					out.print((String) property.getValue("setMethod"));
-				else {
-					out.print("set");
-					out.print(expr.getNamedElement().getName().substring(0, 1).toUpperCase());
-					if (expr.getNamedElement().getName().length() > 1)
-						out.print(expr.getNamedElement().getName().substring(1));
-				}
-				out.print("(");
-				ctx.invoke(genExpression, arg1, ctx, out);
-				out.print(")");
-			} else {
-				ctx.invoke(genName, expr.getNamedElement(), ctx, out);
-				out.print(arg2);
-				ctx.invoke(genExpression, arg1, ctx, out);
+public class NameTemplate extends JavaTemplate
+{
+	public void genAssignment( Name expr, Context ctx, TabbedWriter out, Expression arg1, String arg2 )
+	{
+		String propertyFunction = CommonUtilities.getPropertyFunction(
+				expr.getNamedElement(), true, ctx );
+		if ( propertyFunction != null )
+		{
+			if ( expr.getQualifier() != null )
+			{
+				ctx.invoke( genExpression, expr.getQualifier(), ctx, out );
+				out.print( '.' );
 			}
-		} else
-			ctx.invoke(genAssignment, expr.getType(), ctx, out, expr, arg1, arg2);
+			out.print( propertyFunction );
+			out.print( '(' );
+			ctx.invoke( genExpression, arg1, ctx, out );
+			out.print( ')' );
+		}
+		else
+		{
+			ctx.invoke( genAssignment, expr.getType(), ctx, out, expr, arg1, arg2 );
+		}
 	}
 }
--- gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/NamedElementTemplate.java
+++ gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/NamedElementTemplate.java
@@ -12,48 +12,36 @@
 package org.eclipse.edt.gen.java.templates;
 
 import org.eclipse.edt.gen.java.CommonUtilities;
-import org.eclipse.edt.gen.java.Constants;
 import org.eclipse.edt.gen.java.Context;
 import org.eclipse.edt.gen.java.JavaAliaser;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
-import org.eclipse.edt.mof.egl.Annotation;
-import org.eclipse.edt.mof.egl.ExternalType;
-import org.eclipse.edt.mof.egl.Member;
-import org.eclipse.edt.mof.egl.NamedElement;
+import org.eclipse.edt.mof.egl.*;
 
-public class NamedElementTemplate extends JavaTemplate {
-
-	public Annotation getPropertyAnnotation(NamedElement expr) {
-		return expr.getAnnotation(Constants.Annotation_EGLProperty);
+public class NamedElementTemplate extends JavaTemplate 
+{
+	public void genAccessor( NamedElement element, Context ctx, TabbedWriter out )
+	{
+		String propertyFunction = CommonUtilities.getPropertyFunction( element, false, ctx );
+		if ( propertyFunction != null )
+		{
+			out.print( propertyFunction );
+			out.print( "()" );
+		}
+		else
+		{
+			genName( element, ctx, out );
+		}
 	}
 
-	public void genAccessor(NamedElement element, Context ctx, TabbedWriter out) {
-		Annotation property = getPropertyAnnotation(element);
-		if (property != null) {
-			// obtain the name of the function
-			String functionName;
-			if (property.getValue("getMethod") != null)
-				functionName = (String) property.getValue("getMethod");
-			else {
-				functionName = "get" + element.getName().substring(0, 1).toUpperCase();
-				if (element.getName().length() > 1)
-					functionName = functionName + element.getName().substring(1);
-			}
-			// if the function name matches the name of the current function, then this is the getter and we simply output
-			// the name of the variable, instead of creating an infinite loop of calls to the same function
-			if (functionName.equals(ctx.getCurrentFunction()))
-				genName(element, ctx, out);
-			else
-				out.print(functionName + "()");
-		} else
-			genName(element, ctx, out);
-	}
-
-	public void genName(NamedElement element, Context ctx, TabbedWriter out) {
-		if (element instanceof Member && ((Member) element).getContainer() instanceof ExternalType) {
-			Member member = (Member) element;
-			if (CommonUtilities.isJavaExternalType((ExternalType) member.getContainer()) && member.getAnnotation("eglx.lang.ExternalName") != null) {
-				out.print((String) member.getAnnotation("eglx.lang.ExternalName").getValue());
+	public void genName(NamedElement element, Context ctx, TabbedWriter out) 
+	{
+		if ( element instanceof Member && ((Member)element).getContainer() instanceof ExternalType )
+		{
+			Member member = (Member)element;
+			if ( CommonUtilities.isJavaExternalType( (ExternalType)member.getContainer() ) 
+					&& member.getAnnotation( "eglx.lang.ExternalName" ) != null )
+			{
+				out.print( (String)member.getAnnotation( "eglx.lang.ExternalName" ).getValue() );
 				return;
 			}
 		}
