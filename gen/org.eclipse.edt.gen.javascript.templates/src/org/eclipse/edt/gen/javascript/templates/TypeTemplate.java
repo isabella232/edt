7471eff06efc5bef03fcbbc7862bb622e2363439
jdouglas <jdouglas>
2011-09-09 19:26:23 +0000

    Bug 356365: compound assignments not allowed for decimal type

--- gen/org.eclipse.edt.gen.javascript.templates/src/org/eclipse/edt/gen/javascript/templates/TypeTemplate.java
+++ gen/org.eclipse.edt.gen.javascript.templates/src/org/eclipse/edt/gen/javascript/templates/TypeTemplate.java
@@ -37,6 +37,11 @@ public class TypeTemplate extends JavaScriptTemplate {
 		// types may override this validation for specific checking
 	}
 
+	public Boolean isAssignmentBreakupWanted(Type type, Context ctx) {
+		// types can override this to cause an compound assignment expression to be broken up 
+		return false;
+	}
+
 	public void genInstantiation(Type type, Context ctx, TabbedWriter out) {
 		out.print("new ");
 		ctx.invoke(genRuntimeTypeName, type, ctx, out, TypeNameKind.JavascriptImplementation);
