f18bcc4b297eab222f64d1244d7c3216faa37b8b
jdouglas <jdouglas>
2011-09-12 19:34:40 +0000

    Moved a common utility to edt.gen

--- gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/DeclarationExpressionTemplate.java
+++ gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/DeclarationExpressionTemplate.java
@@ -13,20 +13,13 @@ package org.eclipse.edt.gen.java.templates;
 
 import org.eclipse.edt.gen.java.CommonUtilities;
 import org.eclipse.edt.gen.java.Context;
-import org.eclipse.edt.mof.EObject;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
 import org.eclipse.edt.mof.egl.AssignmentStatement;
 import org.eclipse.edt.mof.egl.DeclarationExpression;
-import org.eclipse.edt.mof.egl.DelegateInvocation;
-import org.eclipse.edt.mof.egl.Expression;
 import org.eclipse.edt.mof.egl.Field;
-import org.eclipse.edt.mof.egl.FunctionInvocation;
-import org.eclipse.edt.mof.egl.InvocationExpression;
 import org.eclipse.edt.mof.egl.ParameterKind;
-import org.eclipse.edt.mof.egl.QualifiedFunctionInvocation;
 import org.eclipse.edt.mof.egl.StatementBlock;
 import org.eclipse.edt.mof.egl.utils.TypeUtils;
-import org.eclipse.edt.mof.impl.AbstractVisitor;
 
 public class DeclarationExpressionTemplate extends JavaTemplate {
 
@@ -44,8 +37,8 @@ public class DeclarationExpressionTemplate extends JavaTemplate {
 				&& field.getInitializerStatements() instanceof StatementBlock
 				&& ((StatementBlock) field.getInitializerStatements()).getStatements().size() > 0
 				&& ((StatementBlock) field.getInitializerStatements()).getStatements().get(0) instanceof AssignmentStatement
-				&& !hasSideEffects(((AssignmentStatement) ((StatementBlock) field.getInitializerStatements()).getStatements().get(0)).getAssignment().getRHS(),
-					ctx)) {
+				&& !org.eclipse.edt.gen.CommonUtilities.hasLocalVariableSideEffects(((AssignmentStatement) ((StatementBlock) field.getInitializerStatements())
+					.getStatements().get(0)).getAssignment().getRHS(), ctx)) {
 				// if this is a value type, we need to define the field and then process the combined initialize statements
 				if (TypeUtils.isValueType(field.getType())) {
 					ctx.invoke(genName, field, ctx, out);
@@ -78,61 +71,6 @@ public class DeclarationExpressionTemplate extends JavaTemplate {
 				if (field.getInitializerStatements() != null)
 					ctx.invoke(genStatementNoBraces, field.getInitializerStatements(), ctx, out);
 			}
-		}
-	}
-
-	public static boolean hasSideEffects(Expression expr, Context ctx) {
-		return (new CheckSideEffects()).checkSideEffect(expr, ctx);
-	}
-
-	public static class CheckSideEffects extends AbstractVisitor {
-		boolean has = false;
-		Context ctx;
-
-		public boolean checkSideEffect(Expression expr, Context ctx) {
-			this.ctx = ctx;
-			disallowRevisit();
-			setReturnData(false);
-			expr.accept(this);
-			return (Boolean) getReturnData();
-		}
-
-		public boolean visit(EObject obj) {
-			return false;
-		}
-
-		public boolean visit(Expression expr) {
-			if (has)
-				return false;
-			return true;
-		}
-
-		public boolean visit(FunctionInvocation expr) {
-			processInvocation(expr);
-			return false;
-		}
-
-		public boolean visit(DelegateInvocation expr) {
-			processInvocation(expr);
-			return false;
-		}
-
-		public boolean visit(QualifiedFunctionInvocation expr) {
-			processInvocation(expr);
-			return false;
-		}
-
-		private void processInvocation(InvocationExpression object) {
-			// we need to scan the function arguments for any conditions that require temporary variables to be set
-			// up. Things like IN args, INOUT args with java primitives, OUT arg initialization, etc.
-			for (int i = 0; i < object.getTarget().getParameters().size(); i++) {
-				if (org.eclipse.edt.gen.CommonUtilities.isArgumentToBeAltered(object.getTarget().getParameters().get(i), object.getArguments().get(i), ctx)) {
-					has = true;
-					setReturnData(has);
-					break;
-				}
-			}
-			return;
 		}
 	}
 }
