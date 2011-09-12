6b77d50a2d1fd89041bce1dff6f4ddb657b755a8
jdouglas <jdouglas>
2011-09-12 19:34:40 +0000

    Moved a common utility to edt.gen

--- gen/org.eclipse.edt.gen/src/org/eclipse/edt/gen/CommonUtilities.java
+++ gen/org.eclipse.edt.gen/src/org/eclipse/edt/gen/CommonUtilities.java
@@ -1,13 +1,74 @@
 package org.eclipse.edt.gen;
 
 import org.eclipse.edt.gen.EGLMessages.EGLMessage;
+import org.eclipse.edt.mof.EObject;
 import org.eclipse.edt.mof.egl.Annotation;
+import org.eclipse.edt.mof.egl.DelegateInvocation;
 import org.eclipse.edt.mof.egl.Expression;
+import org.eclipse.edt.mof.egl.FunctionInvocation;
 import org.eclipse.edt.mof.egl.FunctionParameter;
+import org.eclipse.edt.mof.egl.InvocationExpression;
 import org.eclipse.edt.mof.egl.ParameterKind;
+import org.eclipse.edt.mof.egl.QualifiedFunctionInvocation;
 import org.eclipse.edt.mof.egl.utils.TypeUtils;
+import org.eclipse.edt.mof.impl.AbstractVisitor;
 
 public class CommonUtilities {
+
+	public static boolean hasLocalVariableSideEffects(Expression expr, EglContext ctx) {
+		return (new CheckLocalVariableSideEffects()).checkLocalVariableSideEffect(expr, ctx);
+	}
+
+	public static class CheckLocalVariableSideEffects extends AbstractVisitor {
+		boolean has = false;
+		EglContext ctx;
+
+		public boolean checkLocalVariableSideEffect(Expression expr, EglContext ctx) {
+			this.ctx = ctx;
+			disallowRevisit();
+			setReturnData(false);
+			expr.accept(this);
+			return (Boolean) getReturnData();
+		}
+
+		public boolean visit(EObject obj) {
+			return false;
+		}
+
+		public boolean visit(Expression expr) {
+			if (has)
+				return false;
+			return true;
+		}
+
+		public boolean visit(FunctionInvocation expr) {
+			processInvocation(expr);
+			return false;
+		}
+
+		public boolean visit(DelegateInvocation expr) {
+			processInvocation(expr);
+			return false;
+		}
+
+		public boolean visit(QualifiedFunctionInvocation expr) {
+			processInvocation(expr);
+			return false;
+		}
+
+		private void processInvocation(InvocationExpression object) {
+			// we need to scan the function arguments for any conditions that require temporary variables to be set
+			// up. Things like IN args, INOUT args with java primitives, OUT arg initialization, etc.
+			for (int i = 0; i < object.getTarget().getParameters().size(); i++) {
+				if (isArgumentToBeAltered(object.getTarget().getParameters().get(i), object.getArguments().get(i), ctx)) {
+					has = true;
+					setReturnData(has);
+					break;
+				}
+			}
+			return;
+		}
+	}
 
 	public static boolean isArgumentToBeAltered(FunctionParameter parameter, Expression expression, EglContext ctx) {
 		if (parameter.getParameterKind() == ParameterKind.PARM_IN) {
@@ -18,11 +79,11 @@ public class CommonUtilities {
 			// temporary
 			if (!parameter.getType().equals(expression.getType()) || parameter.isNullable() || expression.isNullable()
 				|| !ctx.mapsToPrimitiveType(parameter.getType()))
-				//if the parameter is a const then we should not make a copy
+				// if the parameter is a const then we should not make a copy
 				return !parameter.isConst();
 			return false;
-		} else{
-			//if the parameter is a const we should not make a copy
+		} else {
+			// if the parameter is a const we should not make a copy
 			return isBoxedParameterType(parameter, ctx) && !parameter.isConst();
 		}
 	}
@@ -39,8 +100,8 @@ public class CommonUtilities {
 			return true;
 		return false;
 	}
-	
-	public static Annotation includeEndOffset(Annotation annotation, EglContext ctx){
+
+	public static Annotation includeEndOffset(Annotation annotation, EglContext ctx) {
 		annotation.addAnnotation(ctx.getFactory().createAnnotation(EGLMessage.IncludeEndOffset));
 		return annotation;
 	}
