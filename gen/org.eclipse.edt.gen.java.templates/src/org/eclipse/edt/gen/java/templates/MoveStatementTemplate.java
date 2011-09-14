b93191cb4286847ecb9410fbb29f2e532eafd2b0
mheitz <mheitz>
2011-09-14 17:38:54 +0000

    Bug 353670 - support the move statement.  Not quite done yet.

--- gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/MoveStatementTemplate.java
+++ gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/MoveStatementTemplate.java
@@ -14,30 +14,183 @@ package org.eclipse.edt.gen.java.templates;
 import org.eclipse.edt.gen.java.Context;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
 import org.eclipse.edt.mof.egl.*;
+import org.eclipse.edt.mof.egl.utils.TypeUtils;
 
 public class MoveStatementTemplate extends JavaTemplate 
 {
+	public void genStatementEnd( MoveStatement stmt, Context ctx, TabbedWriter out )
+	{
+		// The other methods will generate the final semicolon if necessary.
+	}
+
 	public void genStatementBody( MoveStatement stmt, Context ctx, TabbedWriter out )
 	{
-		Expression source = stmt.getSourceExpr();
-		LHSExpr target = stmt.getTargetExpr();
-		Expression modifier = stmt.getModifierExpr();
-		
-		if ( modifier == null )
+		if ( stmt.getModifier() == MoveStatement.MOVE_DEFAULT )
 		{
-			String tempName = ctx.nextTempName();
-			ctx.invoke( genRuntimeTypeName, source.getType(), ctx, out );
-			out.print( ' ' );
-			out.print( tempName );
-			out.print( " = (" );
-			ctx.invoke( genRuntimeTypeName, source.getType(), ctx, out );
-			out.print( ')' );
-			ctx.invoke( genExpression, source, ctx, out );
-			out.println( ".clone();" );
-			ctx.invoke( genExpression, target, ctx, out );
-			out.print( " = " );
-			out.print( tempName );
+			deepCopy( stmt, ctx, out );
 		}
-		//TODO else...moveByName, moveFor, moveForAll
+		else
+		{
+			Expression targetExpr = stmt.getTargetExpr();
+			Expression sourceExpr = stmt.getSourceExpr();
+			
+			boolean needTemp = !(targetExpr instanceof Name) || !(sourceExpr instanceof Name);
+			if ( needTemp )
+			{
+				out.println( '{' );
+				
+				if ( !(targetExpr instanceof Name) )
+				{
+					// Save the target in a temp variable.
+					String temp = ctx.nextTempName();
+					
+					ctx.invoke( genRuntimeTypeName, targetExpr.getType(), ctx, out );
+					out.print( ' ' );				
+					out.print( temp );
+					out.print( " = " );				
+					ctx.invoke( genExpression, targetExpr, ctx, out );
+					out.println( ';' );
+
+					// Create a Name to be the new target.
+					targetExpr = nameForTemp( temp, ctx, targetExpr.getType() );
+				}
+				
+				if ( !(sourceExpr instanceof Name) )
+				{
+					// Save the source in a temp variable.
+					String temp = ctx.nextTempName();
+					
+					ctx.invoke( genRuntimeTypeName, sourceExpr.getType(), ctx, out );
+					out.print( ' ' );				
+					out.print( temp );
+					out.print( " = " );				
+					ctx.invoke( genExpression, sourceExpr, ctx, out );
+					out.println( ';' );
+
+					// Create a Name to be the new source.
+					sourceExpr = nameForTemp( temp, ctx, sourceExpr.getType() );
+				}
+			}
+			
+			switch ( stmt.getModifier() )
+			{
+				case MoveStatement.MOVE_BYNAME:
+					LogicAndDataPart targetPart = (LogicAndDataPart)targetExpr.getType();
+					LogicAndDataPart sourcePart = (LogicAndDataPart)sourceExpr.getType();
+					for ( Field sourceField : sourcePart.getFields() )
+					{
+						Field targetField = targetPart.getField( sourceField.getName() );
+						if ( targetField != null )
+						{
+							Assignment assign = ctx.getFactory().createAssignment();
+							assign.setOperator( "=" );
+							
+							MemberName targetName = ctx.getFactory().createMemberName();
+							targetName.setId( targetField.getName() );
+							targetName.setMember( (Member)targetField );
+							MemberAccess access = targetName.addQualifier( targetExpr );
+							assign.setLHS( access );
+							
+							MemberName sourceName = ctx.getFactory().createMemberName();
+							sourceName.setId( sourceField.getName() );
+							sourceName.setMember( (Member)sourceField );
+							access = sourceName.addQualifier( sourceExpr );
+							assign.setRHS( access );
+							
+							ctx.invoke( genExpression, assign, ctx, out );
+							out.println( ';' );
+						}
+					}
+					break;
+
+				case MoveStatement.MOVE_FOR:
+				case MoveStatement.MOVE_FORALL:
+					String counter = ctx.nextTempName();
+					out.print( "for ( int " + counter + " = " );
+
+					Expression forCount = stmt.getModifierExpr();
+					if ( forCount != null )
+					{
+						out.print( "(int)Math.min(Math.min(" );
+						ctx.invoke( genExpression, sourceExpr, ctx, out );
+						out.print( ".getSize(), " );
+						ctx.invoke( genExpression, targetExpr, ctx, out );
+						out.print( ".getSize())," );
+						ctx.invoke( genExpression, forCount, ctx, out );
+						out.print( ')' );
+					}
+					else
+					{
+						out.print( "Math.min(" );
+						ctx.invoke( genExpression, sourceExpr, ctx, out );
+						out.print( ".getSize(), " );
+						ctx.invoke( genExpression, targetExpr, ctx, out );
+						out.print( ".getSize())" );
+					}					
+					out.println( "; " + counter + " > 0; " + counter + "-- )" );
+					out.println( '{' );
+					
+					Assignment assign = ctx.getFactory().createAssignment();
+					assign.setOperator( "=" );
+					
+					Expression index = nameForTemp( counter, ctx, TypeUtils.Type_INT );
+					
+					ArrayAccess targetAccess = ctx.getFactory().createArrayAccess();
+					targetAccess.setArray( targetExpr );
+					targetAccess.setIndex( index );
+					assign.setLHS( targetAccess );
+					
+					ArrayAccess sourceAccess = ctx.getFactory().createArrayAccess();
+					sourceAccess.setArray( sourceExpr );
+					sourceAccess.setIndex( index );
+					assign.setRHS( sourceAccess );
+
+					ctx.invoke( genExpression, assign, ctx, out );
+					out.println( ';' );
+					
+					out.println( '}' );
+					break;
+			}
+
+			if ( needTemp )
+			{
+				out.println( '}' );
+			}
+		}
+	}
+	
+	private void deepCopy( MoveStatement stmt, Context ctx, TabbedWriter out )
+	{
+		String tempName = ctx.nextTempName();
+
+		//TODO no, don't use clone...the source should be an EglAny so use EglAny.deepCopy
+		Expression source = stmt.getSourceExpr();
+		ctx.invoke( genRuntimeTypeName, source.getType(), ctx, out );
+		out.print( ' ' );
+		out.print( tempName );
+		out.print( " = (" );
+		ctx.invoke( genRuntimeTypeName, source.getType(), ctx, out );
+		out.print( ')' );
+		ctx.invoke( genExpression, source, ctx, out );
+		out.println( ".clone();" );
+		ctx.invoke( genExpression, stmt.getTargetExpr(), ctx, out );
+		out.print( " = " );
+		out.print( tempName );
+		out.println( ';' );
+	}
+	
+	private Name nameForTemp( String id, Context ctx, Type type )
+	{
+		Field field = ctx.getFactory().createField();
+		field.setIsImplicit( true );
+		field.setContainer( ctx.getCurrentFunction() );
+		field.setType( type );
+		field.setName( id );
+		
+		MemberName name = ctx.getFactory().createMemberName();
+		name.setId( id );
+		name.setMember( field );
+		
+		return name;
 	}
 }
