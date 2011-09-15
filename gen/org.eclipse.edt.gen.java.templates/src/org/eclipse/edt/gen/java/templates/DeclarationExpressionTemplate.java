0fbf19c45eba46e452a798f6c66741ea76b28bab
mheitz <mheitz>
2011-09-15 21:31:55 +0000

    Fix null pointers from some initialized variables.

--- gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/DeclarationExpressionTemplate.java
+++ gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/DeclarationExpressionTemplate.java
@@ -14,10 +14,7 @@ package org.eclipse.edt.gen.java.templates;
 import org.eclipse.edt.gen.java.CommonUtilities;
 import org.eclipse.edt.gen.java.Context;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
-import org.eclipse.edt.mof.egl.AssignmentStatement;
-import org.eclipse.edt.mof.egl.DeclarationExpression;
-import org.eclipse.edt.mof.egl.Field;
-import org.eclipse.edt.mof.egl.MemberName;
+import org.eclipse.edt.mof.egl.*;
 
 public class DeclarationExpressionTemplate extends JavaTemplate {
 
@@ -29,6 +26,12 @@ public class DeclarationExpressionTemplate extends JavaTemplate {
 			ctx.invoke(genRuntimeTypeName, field, ctx, out, TypeNameKind.JavaPrimitive);
 			out.print(" ");
 			ctx.invoke(genName, field, ctx, out);
+			out.print(" = ");
+			ctx.invoke(genInitialization, field, ctx, out);
+			out.println(";");
+			// as this is an expression that also creates a new line with the above println method, it throws off the
+			// smap ending line number by 1. We need to issue a call to correct this
+			ctx.setSmapLastJavaLineNumber(out.getLineNumber() - 1);
 			// if the initializer statements are not against the currect field, then we need to do the initialization in
 			// addition to the statements
 			if (field.getInitializerStatements() != null
@@ -37,20 +40,14 @@ public class DeclarationExpressionTemplate extends JavaTemplate {
 				&& ((AssignmentStatement) field.getInitializerStatements().getStatements().get(0)).getAssignment().getLHS() instanceof MemberName
 				&& ((MemberName) ((AssignmentStatement) field.getInitializerStatements().getStatements().get(0)).getAssignment().getLHS())
 					.getMember().equals(field)) {
-				out.println(";");
-				// as this is an expression that also creates a new line with the above println method, it throws off the
-				// smap ending line number by 1. We need to issue a call to correct this
-				ctx.setSmapLastJavaLineNumber(out.getLineNumber() - 1);
 				// we need to run the temporary variables separately, otherwise we might not get wraps
-				ctx.invoke(genInitializeStatement, field, ctx, out, true);
+				Expression rhs = ((AssignmentStatement) field.getInitializerStatements().getStatements().get(0)).getAssignment().getRHS();
+				// Skip initialization when the initializer is going to create a new value anyway.
+				if ( !( rhs instanceof NewExpression && rhs.getType().equals( field.getType() ) ) )
+				{
+					ctx.invoke(genInitializeStatement, field, ctx, out, true);
+				}
 			} else {
-				out.print(" = ");
-				// this logic will not combine, because it isn't safe to
-				ctx.invoke(genInitialization, field, ctx, out);
-				out.println(";");
-				// as this is an expression that also creates a new line with the above println method, it throws off the
-				// smap ending line number by 1. We need to issue a call to correct this
-				ctx.setSmapLastJavaLineNumber(out.getLineNumber() - 1);
 				// now check for any statements to be processed
 				if (field.getInitializerStatements() != null)
 					ctx.invoke(genStatementNoBraces, field.getInitializerStatements(), ctx, out);
