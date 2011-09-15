733e98947410a5ee5227050e35fae647969fba82
jdouglas <jdouglas>
2011-09-15 00:13:48 +0000

    Bug 357665: local variables with initializers needed to have the logic placed outside of the local variable declaration

--- gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/DeclarationExpressionTemplate.java
+++ gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/DeclarationExpressionTemplate.java
@@ -20,7 +20,6 @@ import org.eclipse.edt.mof.egl.Field;
 import org.eclipse.edt.mof.egl.MemberName;
 import org.eclipse.edt.mof.egl.ParameterKind;
 import org.eclipse.edt.mof.egl.StatementBlock;
-import org.eclipse.edt.mof.egl.utils.TypeUtils;
 
 public class DeclarationExpressionTemplate extends JavaTemplate {
 
@@ -31,34 +30,32 @@ public class DeclarationExpressionTemplate extends JavaTemplate {
 			// process the field
 			ctx.invoke(genRuntimeTypeName, field, ctx, out, TypeNameKind.JavaPrimitive);
 			out.print(" ");
+			ctx.invoke(genName, field, ctx, out);
+			out.print(" = ");
 			// if the initializer statements are not against the currect field, then we need to do the initialization in
 			// addition to the statements
 			if (field.getInitializerStatements() != null
 				&& field.getInitializerStatements() instanceof StatementBlock
-				&& ((StatementBlock) field.getInitializerStatements()).getStatements().size() > 0
+				&& ((StatementBlock) field.getInitializerStatements()).getStatements().size() == 1
 				&& ((StatementBlock) field.getInitializerStatements()).getStatements().get(0) instanceof AssignmentStatement
 				&& ((AssignmentStatement) ((StatementBlock) field.getInitializerStatements()).getStatements().get(0)).getAssignment().getLHS() instanceof MemberName
 				&& ((MemberName) ((AssignmentStatement) ((StatementBlock) field.getInitializerStatements()).getStatements().get(0)).getAssignment().getLHS())
 					.getMember().equals(field)) {
-				// if this is a value type, we need to define the field and then process the combined initialize statements
-				if (TypeUtils.isValueType(field.getType())) {
-					ctx.invoke(genName, field, ctx, out);
-					out.print(" = ");
-					if (ctx.getAttribute(field, org.eclipse.edt.gen.Constants.SubKey_functionArgumentTemporaryVariable) != null
-						&& ctx.getAttribute(field, org.eclipse.edt.gen.Constants.SubKey_functionArgumentTemporaryVariable) != ParameterKind.PARM_IN)
-						out.print("null");
-					else
-						ctx.invoke(genInitialization, field, ctx, out);
+				if (ctx.getAttribute(field, org.eclipse.edt.gen.Constants.SubKey_functionArgumentTemporaryVariable) != null
+					&& ctx.getAttribute(field, org.eclipse.edt.gen.Constants.SubKey_functionArgumentTemporaryVariable) != ParameterKind.PARM_IN) {
+					out.print("null");
 					out.println(";");
 					// as this is an expression that also creates a new line with the above println method, it throws off the
 					// smap ending line number by 1. We need to issue a call to correct this
 					ctx.setSmapLastJavaLineNumber(out.getLineNumber() - 1);
+					// we need to run the temporary variables separately, otherwise we might not get wraps
+					ctx.invoke(genInitializeStatement, field, ctx, out, true);
+				} else {
+					ctx.invoke(genExpression, ((AssignmentStatement) ((StatementBlock) field.getInitializerStatements()).getStatements().get(0))
+						.getAssignment().getRHS(), ctx, out);
+					out.println(";");
 				}
-				// this logic will combine
-				ctx.invoke(genInitializeStatement, field, ctx, out, true);
 			} else {
-				ctx.invoke(genName, field, ctx, out);
-				out.print(" = ");
 				// this logic will not combine, because it isn't safe to
 				ctx.invoke(genInitialization, field, ctx, out);
 				out.println(";");
