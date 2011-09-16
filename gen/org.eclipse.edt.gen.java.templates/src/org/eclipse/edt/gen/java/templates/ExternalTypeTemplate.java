724b7deb775828ed068a3e0e70ce3ad924abdc9b
jdouglas <jdouglas>
2011-09-16 18:53:29 +0000

    Bug 357967: fixed casting for external types

--- gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/ExternalTypeTemplate.java
+++ gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/ExternalTypeTemplate.java
@@ -18,10 +18,15 @@ import org.eclipse.edt.gen.java.CommonUtilities;
 import org.eclipse.edt.gen.java.Constants;
 import org.eclipse.edt.gen.java.Context;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
-import org.eclipse.edt.mof.egl.*;
+import org.eclipse.edt.mof.egl.Annotation;
+import org.eclipse.edt.mof.egl.Assignment;
+import org.eclipse.edt.mof.egl.Delegate;
+import org.eclipse.edt.mof.egl.ExternalType;
+import org.eclipse.edt.mof.egl.Field;
+import org.eclipse.edt.mof.egl.Part;
 
 public class ExternalTypeTemplate extends JavaTemplate {
-	
+
 	static final String DELEGATE_IN_INNER_CLASS = "org.eclipse.edt.gen.java.templates.ExternalTypeTemplate.DELEGATE_IN_INNER_CLASS";
 
 	public void preGenClassBody(ExternalType part, Context ctx) {}
@@ -60,9 +65,8 @@ public class ExternalTypeTemplate extends JavaTemplate {
 
 	public void genClassHeader(ExternalType part, Context ctx, TabbedWriter out) {}
 
-	public void genAccessor( ExternalType part, Context ctx, TabbedWriter out )
-	{
-		genRuntimeTypeName( part, ctx, out, TypeNameKind.JavaImplementation );
+	public void genAccessor(ExternalType part, Context ctx, TabbedWriter out) {
+		genRuntimeTypeName(part, ctx, out, TypeNameKind.JavaImplementation);
 	}
 
 	public void genRuntimeTypeName(ExternalType part, Context ctx, TabbedWriter out, TypeNameKind arg) {
@@ -91,34 +95,30 @@ public class ExternalTypeTemplate extends JavaTemplate {
 			ctx.invoke(genPartName, part, ctx, out);
 	}
 
-	public void genContainerBasedAssignment( ExternalType part, Context ctx,
-			TabbedWriter out, Assignment assignment, Field field )
-	{
-		Annotation annot = part.getAnnotation( "eglx.java.JavaObject" );
-		if ( annot != null )
-		{
-			Annotation eventListener = field.getAnnotation( "egl.lang.EventListener" );
-			if ( eventListener != null )
-			{
-				ctx.invoke( genExpression, assignment.getLHS().getQualifier(), ctx, out );
-				out.print( '.' );
-				out.print( eventListener.getValue( "addMethod" ).toString() );
-				out.print( "( new " );
-				out.print( eventListener.getValue( "listenerType" ).toString() );
-				out.print( "() { public void " );
-				out.print( eventListener.getValue( "method" ).toString() );
-				out.print( "( " );
-				ctx.invoke( genRuntimeTypeName, ((Delegate)field.getType()).getParameters().get( 0 ).getType(), ctx, out );
-				out.print( " ezeEvent ) { " );
-				ctx.put( DELEGATE_IN_INNER_CLASS, true );
-				ctx.invoke( genExpression, assignment.getRHS(), ctx, out );
-				ctx.remove( DELEGATE_IN_INNER_CLASS );
-				out.print( ".invoke( ezeEvent );" );
-				out.print( " } } )" );
+	public void genContainerBasedAssignment(ExternalType part, Context ctx, TabbedWriter out, Assignment assignment, Field field) {
+		Annotation annot = part.getAnnotation("eglx.java.JavaObject");
+		if (annot != null) {
+			Annotation eventListener = field.getAnnotation("egl.lang.EventListener");
+			if (eventListener != null) {
+				ctx.invoke(genExpression, assignment.getLHS().getQualifier(), ctx, out);
+				out.print('.');
+				out.print(eventListener.getValue("addMethod").toString());
+				out.print("( new ");
+				out.print(eventListener.getValue("listenerType").toString());
+				out.print("() { public void ");
+				out.print(eventListener.getValue("method").toString());
+				out.print("( ");
+				ctx.invoke(genRuntimeTypeName, ((Delegate) field.getType()).getParameters().get(0).getType(), ctx, out);
+				out.print(" ezeEvent ) { ");
+				ctx.put(DELEGATE_IN_INNER_CLASS, true);
+				ctx.invoke(genExpression, assignment.getRHS(), ctx, out);
+				ctx.remove(DELEGATE_IN_INNER_CLASS);
+				out.print(".invoke( ezeEvent );");
+				out.print(" } } )");
 				return;
 			}
 		}
-		
-		ctx.invokeSuper( this, genContainerBasedAssignment, part, ctx, out, assignment, field );
+
+		ctx.invokeSuper(this, genContainerBasedAssignment, part, ctx, out, assignment, field);
 	}
 }
