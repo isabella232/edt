be5f68ef9119a9e2477ca1fa742755ef472a215a
jihuang <jihuang>
2011-09-16 07:07:57 +0000

    Bug 356889 - can not open custom widget

---
+++ gen/org.eclipse.edt.gen.deployment.javascript.templates/src/org/eclipse/edt/gen/deployment/javascript/templates/RUIHandlerTemplate.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright © 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.edt.gen.deployment.javascript.templates;
+
+import java.util.HashMap;
+
+import org.eclipse.edt.gen.deployment.javascript.Context;
+import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.Handler;
+
+public class RUIHandlerTemplate extends RUITemplate {
+	
+	public void genDeploymentHTML(Handler handler, Context ctx, TabbedWriter out, String egldd, HashMap eglParameters, String userMsgLocale, String runtimeMsgLocale) {		
+		genHTML(handler, ctx, out, egldd, eglParameters, userMsgLocale, runtimeMsgLocale);
+	}
+	
+	private void genHTML(Handler handler, Context ctx, TabbedWriter out, String egldd, HashMap eglParameters, String userMsgLocale, String runtimeMsgLocale){
+		genHTML(false, handler, ctx, out, egldd, eglParameters, userMsgLocale, runtimeMsgLocale, false, false, false);
+	}
+	
+}
