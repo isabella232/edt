2197df86801911c1adbce1adef544c772f9fbf29
jqian <jqian>
2011-09-21 01:48:44 +0000

    regenerated system types and libraries because there has been some bug fixes in java gen

--- runtimes/org.eclipse.edt.runtime.java/src/org/eclipse/edt/eunit/runtime/ConstantsLib.java
+++ runtimes/org.eclipse.edt.runtime.java/src/org/eclipse/edt/eunit/runtime/ConstantsLib.java
@@ -1,25 +1,15 @@
-/*******************************************************************************
- * Copyright (c) 2011 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
 package org.eclipse.edt.eunit.runtime;
 import org.eclipse.edt.javart.resources.*;
 import org.eclipse.edt.javart.*;
-import org.eclipse.edt.runtime.java.egl.lang.EString;
-import java.lang.String;
 import org.eclipse.edt.runtime.java.egl.lang.EglAny;
 import org.eclipse.edt.runtime.java.egl.lang.EInt;
 import java.lang.Integer;
+import org.eclipse.edt.runtime.java.egl.lang.EString;
+import java.lang.String;
 @javax.xml.bind.annotation.XmlRootElement(name="ConstantsLib")
 public class ConstantsLib extends ExecutableBase {
 	private static final long serialVersionUID = 10L;
-	private static final String ezeConst_NEWLINE = "      ";
+	private static final String ezeConst_NEWLINE = "\r\n";
 	public final String NEWLINE = ezeConst_NEWLINE;
 	private static final String ezeConst_EXIT_PREFIX = "STATUS: ";
 	public final String EXIT_PREFIX = ezeConst_EXIT_PREFIX;
@@ -31,6 +21,8 @@ public class ConstantsLib extends ExecutableBase {
 	public final int SERROR = ezeConst_SERROR;
 	private static final int ezeConst_SNOT_RUN = (int)((short) 3);
 	public final int SNOT_RUN = ezeConst_SNOT_RUN;
+	private static final int ezeConst_SBAD = (int)((short) 4);
+	public final int SBAD = ezeConst_SBAD;
 	public ConstantsLib() {
 		super();
 		ezeInitialize();
@@ -60,5 +52,9 @@ public class ConstantsLib extends ExecutableBase {
 	@org.eclipse.edt.javart.json.Json(name="SNOT_RUN", clazz=EInt.class, asOptions={})
 	public int getSNOT_RUN() {
 		return (SNOT_RUN);
+	}
+	@org.eclipse.edt.javart.json.Json(name="SBAD", clazz=EInt.class, asOptions={})
+	public int getSBAD() {
+		return (SBAD);
 	}
 }
--- runtimes/org.eclipse.edt.runtime.java/src/org/eclipse/edt/eunit/runtime/LogResult.java
+++ runtimes/org.eclipse.edt.runtime.java/src/org/eclipse/edt/eunit/runtime/LogResult.java
@@ -1,38 +1,28 @@
-/*******************************************************************************
- * Copyright (c) 2011 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
 package org.eclipse.edt.eunit.runtime;
 import org.eclipse.edt.javart.resources.*;
 import org.eclipse.edt.javart.*;
 import org.eclipse.edt.runtime.java.egl.lang.EglAny;
-import org.eclipse.edt.runtime.java.egl.lang.EDate;
-import java.util.Calendar;
-import org.eclipse.edt.runtime.java.egl.lang.EString;
-import java.lang.String;
-import org.eclipse.edt.eunit.runtime.ConstantsLib;
-import org.eclipse.edt.runtime.java.egl.lang.EInt;
-import java.lang.Integer;
-import org.eclipse.edt.eunit.runtime.Status;
-import eglx.lang.MathLib;
-import org.eclipse.edt.eunit.runtime.Log;
 import org.eclipse.edt.runtime.java.egl.lang.EDecimal;
 import java.math.BigDecimal;
 import org.eclipse.edt.eunit.runtime.AssertionFailedException;
-import org.eclipse.edt.runtime.java.egl.lang.EglList;
-import org.eclipse.edt.runtime.java.egl.lang.EFloat;
-import java.lang.Double;
-import org.eclipse.edt.runtime.java.egl.lang.ETimestamp;
-import org.eclipse.edt.runtime.java.egl.lang.EBigint;
-import java.lang.Long;
 import org.eclipse.edt.runtime.java.egl.lang.EBoolean;
 import java.lang.Boolean;
+import org.eclipse.edt.runtime.java.egl.lang.EFloat;
+import java.lang.Double;
+import org.eclipse.edt.runtime.java.egl.lang.EString;
+import java.lang.String;
+import eglx.lang.MathLib;
+import org.eclipse.edt.runtime.java.egl.lang.ETimestamp;
+import java.util.Calendar;
+import org.eclipse.edt.eunit.runtime.ConstantsLib;
+import org.eclipse.edt.runtime.java.egl.lang.EglList;
+import org.eclipse.edt.runtime.java.egl.lang.EDate;
+import org.eclipse.edt.eunit.runtime.Log;
+import org.eclipse.edt.runtime.java.egl.lang.EBigint;
+import java.lang.Long;
+import org.eclipse.edt.eunit.runtime.Status;
+import org.eclipse.edt.runtime.java.egl.lang.EInt;
+import java.lang.Integer;
 @javax.xml.bind.annotation.XmlRootElement(name="LogResult")
 public class LogResult extends ExecutableBase {
 	private static final long serialVersionUID = 10L;
@@ -138,18 +128,18 @@ public class LogResult extends ExecutableBase {
 	}
 	public void assertTrue(String failedReason, boolean testCondition) {
 		if (testCondition) {
-			AnyBoxedObject<String> eze$Temp6 = null;
+			AnyBoxedObject<String> eze$Temp6;
 			eze$Temp6 = EglAny.ezeWrap("OK");
 			passed(eze$Temp6);
 		}
 		else {
-			AnyBoxedObject<String> eze$Temp7 = null;
+			AnyBoxedObject<String> eze$Temp7;
 			eze$Temp7 = EglAny.ezeWrap(failedReason);
 			failed(eze$Temp7);
 			failedReason = eze$Temp7.ezeUnbox();
-			AssertionFailedException eze$Temp8 = (AssertionFailedException) null;
+			AssertionFailedException eze$Temp8 = new AssertionFailedException();
 			{
-				AssertionFailedException eze$SettingTarget1 = (AssertionFailedException) null;
+				AssertionFailedException eze$SettingTarget1;
 				eze$SettingTarget1 = new AssertionFailedException();
 				eze$SettingTarget1.setMessage(s.reason);
 				eze$Temp8 = eze$SettingTarget1;
@@ -158,44 +148,44 @@ public class LogResult extends ExecutableBase {
 		}
 	}
 	public void assertTrue1(boolean testCondition) {
-		String eze$Temp9 = Constants.EMPTY_STRING;
+		String eze$Temp9;
 		eze$Temp9 = "";
 		assertTrue(eze$Temp9, testCondition);
 	}
 	public void assertBigIntEqual(String message, long expected, long actual) {
-		boolean isEqual = false;
+		boolean isEqual;
 		isEqual = (expected == actual);
 		expectAssertTrue(message, EBigint.ezeBox(expected), EBigint.ezeBox(actual), isEqual);
 	}
 	public void assertBigIntEqual1(long expected, long actual) {
-		String eze$Temp10 = Constants.EMPTY_STRING;
+		String eze$Temp10;
 		eze$Temp10 = "";
 		assertBigIntEqual(eze$Temp10, expected, actual);
 	}
 	public void assertStringEqual(String message, String expected, String actual) {
-		boolean isEqual = false;
+		boolean isEqual;
 		isEqual = ((expected).equals(actual));
 		expectAssertTrue(message, EString.ezeBox(expected), EString.ezeBox(actual), isEqual);
 	}
 	public void assertStringEqual1(String expected, String actual) {
-		String eze$Temp11 = Constants.EMPTY_STRING;
+		String eze$Temp11;
 		eze$Temp11 = "";
 		assertStringEqual(eze$Temp11, expected, actual);
 	}
 	public void assertStringArrayEqual(String message, egl.lang.EglList<String> expected, egl.lang.EglList<String> actual) {
-		boolean isArrayEqual = false;
+		boolean isArrayEqual;
 		isArrayEqual = true;
-		int expectedSize = 0;
+		int expectedSize;
 		expectedSize = expected.getSize();
-		int actualSize = 0;
+		int actualSize;
 		actualSize = actual.getSize();
 		String failedReason = Constants.EMPTY_STRING;
 		if ((expectedSize == actualSize)) {
-			String failedHeader = Constants.EMPTY_STRING;
+			String failedHeader;
 			failedHeader = "Array element No.[";
-			String expectedValues = Constants.EMPTY_STRING;
+			String expectedValues;
 			expectedValues = ((EXPECTEDHEADER) + "[");
-			String actualValues = Constants.EMPTY_STRING;
+			String actualValues;
 			actualValues = ((ACTUALHEADER) + "[");
 			{
 				int i = 0;
@@ -231,37 +221,37 @@ public class LogResult extends ExecutableBase {
 		assertTrue(failedReason, isArrayEqual);
 	}
 	public void assertStringArrayEqual1(egl.lang.EglList<String> expected, egl.lang.EglList<String> actual) {
-		String eze$Temp18 = Constants.EMPTY_STRING;
+		String eze$Temp18;
 		eze$Temp18 = "";
 		assertStringArrayEqual(eze$Temp18, expected, actual);
 	}
 	public void assertDateEqual(String message, Calendar expected, Calendar actual) {
-		boolean isEqual = false;
+		boolean isEqual;
 		isEqual = (EDate.equals(expected, actual));
 		expectAssertTrue(message, EDate.ezeBox(expected), EDate.ezeBox(actual), isEqual);
 	}
 	public void assertDateEqual1(Calendar expected, Calendar actual) {
-		String eze$Temp19 = Constants.EMPTY_STRING;
+		String eze$Temp19;
 		eze$Temp19 = "";
 		assertDateEqual(eze$Temp19, expected, actual);
 	}
 	public void assertTimestampEqual(String message, Calendar expected, Calendar actual) {
-		boolean isEqual = false;
+		boolean isEqual;
 		isEqual = (ETimestamp.equals(expected, actual));
 		expectAssertTrue(message, ETimestamp.ezeBox(expected, ETimestamp.YEAR_CODE, ETimestamp.SECOND_CODE), ETimestamp.ezeBox(actual, ETimestamp.YEAR_CODE, ETimestamp.SECOND_CODE), isEqual);
 	}
 	public void assertTimestampEqual1(Calendar expected, Calendar actual) {
-		String eze$Temp20 = Constants.EMPTY_STRING;
+		String eze$Temp20;
 		eze$Temp20 = "";
 		assertTimestampEqual(eze$Temp20, expected, actual);
 	}
 	public void assertDecimalEqual(String message, BigDecimal expected, BigDecimal actual) {
-		boolean isEqual = false;
+		boolean isEqual;
 		isEqual = (EDecimal.equals(expected, actual));
 		expectAssertTrue(message, EDecimal.ezeBox(expected), EDecimal.ezeBox(actual), isEqual);
 	}
 	public void assertDecimalEqual1(BigDecimal expected, BigDecimal actual) {
-		String eze$Temp21 = Constants.EMPTY_STRING;
+		String eze$Temp21;
 		eze$Temp21 = "";
 		assertDecimalEqual(eze$Temp21, expected, actual);
 	}
@@ -273,7 +263,7 @@ public class LogResult extends ExecutableBase {
 		int mantissaActual = 0;
 		String signExpected = Constants.EMPTY_STRING;
 		String signActual = Constants.EMPTY_STRING;
-		double deltaLimit = 0.0;
+		double deltaLimit;
 		deltaLimit = 1E-14;
 		AnyBoxedObject<Integer> eze$Temp22 = EglAny.ezeWrap(0);
 		AnyBoxedObject<String> eze$Temp23 = EglAny.ezeWrap(Constants.EMPTY_STRING);
@@ -287,17 +277,17 @@ public class LogResult extends ExecutableBase {
 		signActual = eze$Temp25.ezeUnbox();
 		delta = (normalExpected - normalActual);
 		delta = MathLib.abs(delta);
-		boolean isEqual = false;
+		boolean isEqual;
 		isEqual = ((((signExpected).equals(signActual)) && (mantissaExpected == mantissaActual)) && (delta < deltaLimit));
 		expectAssertTrue(message, EFloat.ezeBox(expected), EFloat.ezeBox(actual), isEqual);
 	}
 	public void assertFloatEqual1(double expected, double actual) {
-		String eze$Temp26 = Constants.EMPTY_STRING;
+		String eze$Temp26;
 		eze$Temp26 = "";
 		assertFloatEqual(eze$Temp26, expected, actual);
 	}
 	public void expectAssertTrue(String message, egl.lang.EglAny expected, egl.lang.EglAny actual, boolean isEqual) {
-		String failedReason = Constants.EMPTY_STRING;
+		String failedReason;
 		failedReason = expect(expected, actual);
 		if (((org.eclipse.edt.runtime.java.egl.lang.NullType.notEquals(EString.ezeBox(message), null)) && (!(message).equals("")))) {
 			failedReason = ((((message) + " - ")) + failedReason);
@@ -305,7 +295,7 @@ public class LogResult extends ExecutableBase {
 		assertTrue(failedReason, isEqual);
 	}
 	public String expect(egl.lang.EglAny expected, egl.lang.EglAny actual) {
-		String standardMsg = Constants.EMPTY_STRING;
+		String standardMsg;
 		standardMsg = (((((((((((((((("Failed: ") + EXPECTEDHEADER)) + "'")) + EString.ezeCast(expected))) + "' ")) + ACTUALHEADER)) + "'")) + EString.ezeCast(actual))) + "' ");
 		return standardMsg;
 	}
