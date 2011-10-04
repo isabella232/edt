c6ee81471f729ba7ae7a820a8b9eee33e8899863
jspadea <jspadea>
2011-10-04 12:50:02 +0000

    regenerate model with EMF 2.6 runtime compliance

--- ide/org.eclipse.edt.ide.deployment.core/src/org/eclipse/edt/ide/ui/internal/deployment/util/DeploymentSwitch.java
+++ ide/org.eclipse.edt.ide.deployment.core/src/org/eclipse/edt/ide/ui/internal/deployment/util/DeploymentSwitch.java
@@ -17,8 +17,6 @@ import org.eclipse.edt.ide.ui.internal.deployment.*;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.util.Switch;
 
 /**
  * <!-- begin-user-doc -->
@@ -33,7 +31,7 @@ import org.eclipse.emf.ecore.util.Switch;
  * @see org.eclipse.edt.ide.ui.internal.deployment.DeploymentPackage
  * @generated
  */
-public class DeploymentSwitch<T> extends Switch<T>
+public class DeploymentSwitch<T>
 {
 	/**
 	 * The cached model package
@@ -58,17 +56,15 @@ public class DeploymentSwitch<T> extends Switch<T>
 	}
 
 	/**
-	 * Checks whether this is a switch for the given package.
+	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @parameter ePackage the package in question.
-	 * @return whether this is a switch for the given package.
+	 * @return the first non-null result returned by a <code>caseXXX</code> call.
 	 * @generated
 	 */
-	@Override
-	protected boolean isSwitchFor(EPackage ePackage)
+	public T doSwitch(EObject theEObject)
 	{
-		return ePackage == modelPackage;
+		return doSwitch(theEObject.eClass(), theEObject);
 	}
 
 	/**
@@ -78,7 +74,29 @@ public class DeploymentSwitch<T> extends Switch<T>
 	 * @return the first non-null result returned by a <code>caseXXX</code> call.
 	 * @generated
 	 */
-	@Override
+	protected T doSwitch(EClass theEClass, EObject theEObject)
+	{
+		if (theEClass.eContainer() == modelPackage)
+		{
+			return doSwitch(theEClass.getClassifierID(), theEObject);
+		}
+		else
+		{
+			List<EClass> eSuperTypes = theEClass.getESuperTypes();
+			return
+				eSuperTypes.isEmpty() ?
+					defaultCase(theEObject) :
+					doSwitch(eSuperTypes.get(0), theEObject);
+		}
+	}
+
+	/**
+	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the first non-null result returned by a <code>caseXXX</code> call.
+	 * @generated
+	 */
 	protected T doSwitch(int classifierID, EObject theEObject)
 	{
 		switch (classifierID)
@@ -467,7 +485,6 @@ public class DeploymentSwitch<T> extends Switch<T>
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject)
 	 * @generated
 	 */
-	@Override
 	public T defaultCase(EObject object)
 	{
 		return null;
