195ece19d4dd6751aaf42c3b6f6148ff96aa69ee
jspadea <jspadea>
2011-10-05 18:27:20 +0000

    add icu to the build path to support debug workspaces with the java runtime checked out (version from cvs doesn't contain icu classes)

--- debug/org.eclipse.edt.debug.ui/src/org/eclipse/edt/debug/ui/launching/EGLJavaLaunchDelegate.java
+++ debug/org.eclipse.edt.debug.ui/src/org/eclipse/edt/debug/ui/launching/EGLJavaLaunchDelegate.java
@@ -169,6 +169,12 @@ public class EGLJavaLaunchDelegate extends JavaLaunchDelegate
 				// Finally, add any JDBC jars that might be required.
 				ClasspathUtil.addJDBCJars( project, new HashSet<IProject>(), new HashSet<IResource>(), classpath );
 				
+				String icu = ClasspathUtil.getClasspathEntry( "com.ibm.icu" ); //$NON-NLS-1$
+				if ( icu != null )
+				{
+					classpath.add( icu );
+				}
+				
 				copy.setAttribute( IJavaLaunchConfigurationConstants.ATTR_CLASSPATH, classpath );
 				copy.setAttribute( IJavaLaunchConfigurationConstants.ATTR_DEFAULT_CLASSPATH, false );
 				
