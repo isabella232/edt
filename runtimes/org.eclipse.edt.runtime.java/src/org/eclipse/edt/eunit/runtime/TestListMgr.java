e33c38e0b7afc5d164b5a887d01253e417545e96
jqian <jqian>
2011-10-07 02:48:47 +0000

    name refactoring for TestAsyncLib => TestListMgr

--- runtimes/org.eclipse.edt.runtime.java/src/org/eclipse/edt/eunit/runtime/TestListMgr.java
+++ runtimes/org.eclipse.edt.runtime.java/src/org/eclipse/edt/eunit/runtime/TestListMgr.java
@@ -1,18 +1,19 @@
 package org.eclipse.edt.eunit.runtime;
 import org.eclipse.edt.javart.resources.*;
 import org.eclipse.edt.javart.*;
-import org.eclipse.edt.runtime.java.eglx.lang.EAny;
-import org.eclipse.edt.runtime.java.eglx.lang.EList;
-import java.util.List;
-import eglx.lang.SysLib;
-import org.eclipse.edt.eunit.runtime.MultiStatus;
 import org.eclipse.edt.runtime.java.eglx.lang.EString;
 import java.lang.String;
 import org.eclipse.edt.runtime.java.eglx.lang.EInt;
 import java.lang.Integer;
-@javax.xml.bind.annotation.XmlRootElement(name="TestAsyncLib")
-public class TestAsyncLib extends ExecutableBase {
+import org.eclipse.edt.eunit.runtime.MultiStatus;
+import org.eclipse.edt.runtime.java.eglx.lang.EList;
+import java.util.List;
+import org.eclipse.edt.runtime.java.eglx.lang.EAny;
+@javax.xml.bind.annotation.XmlRootElement(name="TestListMgr")
+public class TestListMgr extends ExecutableBase {
 	private static final long serialVersionUID = 10L;
+	@javax.xml.bind.annotation.XmlTransient
+	public MultiStatus ms;
 	@javax.xml.bind.annotation.XmlTransient
 	public int nextTestIndex;
 	@javax.xml.bind.annotation.XmlTransient
@@ -21,15 +22,23 @@ public class TestAsyncLib extends ExecutableBase {
 	public List<org.eclipse.edt.javart.Delegate> LibraryStartTests;
 	@javax.xml.bind.annotation.XmlTransient
 	public int nextLibIndex;
-	public TestAsyncLib() {
+	public TestListMgr() {
 		super();
 		ezeInitialize();
 	}
 	public void ezeInitialize() {
+		ms = new MultiStatus();
 		runTestMtds = EList.ezeNew(org.eclipse.edt.javart.Delegate.class);
 		LibraryStartTests = EList.ezeNew(org.eclipse.edt.javart.Delegate.class);
 		nextTestIndex = (int)((short) 1);
 		nextLibIndex = (int)((short) 1);
+	}
+	@org.eclipse.edt.javart.json.Json(name="ms", clazz=MultiStatus.class, asOptions={})
+	public MultiStatus getMs() {
+		return (ms);
+	}
+	public void setMs( MultiStatus ezeValue ) {
+		this.ms = ezeValue;
 	}
 	@org.eclipse.edt.javart.json.Json(name="nextTestIndex", clazz=EInt.class, asOptions={})
 	public int getNextTestIndex() {
@@ -59,22 +68,21 @@ public class TestAsyncLib extends ExecutableBase {
 	public void setNextLibIndex( int ezeValue ) {
 		this.nextLibIndex = ezeValue;
 	}
-	public void nextTestLibrary() {
-		boolean eze$Temp1;
-		eze$Temp1 = (nextLibIndex < EList.getSize(LibraryStartTests));
-		if (eze$Temp1) {
-			nextLibIndex += (int)((short) 1);
-			LibraryStartTests.get(nextLibIndex - 1).invoke();
-		}
-	}
-	public void nextTest(MultiStatus ms, String testId) {
+	public void nextTest(String testId) {
 		ms.addStatus(testId);
-		SysLib.writeStdout((("TestAsyncLib.runTestMtds array size: ") + EString.asString(EList.getSize(this.runTestMtds))));
-		boolean eze$Temp3;
-		eze$Temp3 = (nextTestIndex < EList.getSize(runTestMtds));
-		if (eze$Temp3) {
+		boolean eze$Temp1;
+		eze$Temp1 = (nextTestIndex < EList.getSize(runTestMtds));
+		if (eze$Temp1) {
 			nextTestIndex += (int)((short) 1);
 			runTestMtds.get(nextTestIndex - 1).invoke();
+		}
+	}
+	public void nextTestLibrary() {
+		boolean eze$Temp3;
+		eze$Temp3 = (nextLibIndex < EList.getSize(LibraryStartTests));
+		if (eze$Temp3) {
+			nextLibIndex += (int)((short) 1);
+			LibraryStartTests.get(nextLibIndex - 1).invoke();
 		}
 	}
 }
