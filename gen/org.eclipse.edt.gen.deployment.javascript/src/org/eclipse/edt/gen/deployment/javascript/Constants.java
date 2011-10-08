e82c93a9c08af0a6948389efda96ea26df268c9e
jqian <jqian>
2011-10-08 14:46:31 +0000

    added more js runtime files

--- gen/org.eclipse.edt.gen.deployment.javascript/src/org/eclipse/edt/gen/deployment/javascript/Constants.java
+++ gen/org.eclipse.edt.gen.deployment.javascript/src/org/eclipse/edt/gen/deployment/javascript/Constants.java
@@ -96,6 +96,7 @@ public class Constants {
 		RUI_RUNTIME_JAVASCRIPT_FILES.add("org/eclipse/edt/eunit/runtime/LogResult.js"); //$NON-NLS-1$
 		RUI_RUNTIME_JAVASCRIPT_FILES.add("org/eclipse/edt/eunit/runtime/MultiStatus.js"); //$NON-NLS-1$
 		RUI_RUNTIME_JAVASCRIPT_FILES.add("org/eclipse/edt/eunit/runtime/Status.js"); //$NON-NLS-1$
+		RUI_RUNTIME_JAVASCRIPT_FILES.add("org/eclipse/edt/eunit/runtime/ServiceBindingType.js"); //$NON-NLS-1$
 		RUI_RUNTIME_JAVASCRIPT_FILES.add("org/eclipse/edt/eunit/runtime/TestListMgr.js"); //$NON-NLS-1$
 		
 	}
