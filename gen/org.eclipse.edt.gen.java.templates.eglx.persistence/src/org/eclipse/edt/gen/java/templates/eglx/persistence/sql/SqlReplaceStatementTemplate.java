9ccae7ad22d86264bc084c87508822b02f16322a
jvincens <jvincens>
2011-10-14 19:07:44 +0000

    bugzilla 360103

--- gen/org.eclipse.edt.gen.java.templates.eglx.persistence/src/org/eclipse/edt/gen/java/templates/eglx/persistence/sql/SqlReplaceStatementTemplate.java
+++ gen/org.eclipse.edt.gen.java.templates.eglx.persistence/src/org/eclipse/edt/gen/java/templates/eglx/persistence/sql/SqlReplaceStatementTemplate.java
@@ -2,6 +2,7 @@ package org.eclipse.edt.gen.java.templates.eglx.persistence.sql;
 
 import org.eclipse.edt.gen.java.Context;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.Classifier;
 import org.eclipse.edt.mof.egl.EGLClass;
 import org.eclipse.edt.mof.egl.Field;
 import org.eclipse.edt.mof.eglx.persistence.sql.SqlReplaceStatement;
@@ -34,19 +35,27 @@ public class SqlReplaceStatementTemplate extends SqlActionStatementTemplate {
 		else {
 			// We have an open result set already
 			if (stmt.getTarget() != null) {
-				for (Field f : ((EGLClass)stmt.getTarget()).getFields()) {
+				String var_resultSet = ctx.nextTempName();
+				out.println("try {");
+				// DataSource is a ResultSet already which is why there is no sql string
+				out.print(class_ResultSet + " " + var_resultSet);
+				out.print(" = ");
+				ctx.invoke(genExpression, stmt.getDataSource(), ctx, out);
+				out.println(".getResultSet();");
+				for (Field f : ((EGLClass)stmt.getTarget().getType()).getFields()) {
 					if (SQL.isPersistable(f) && SQL.isUpdateable(f)) {
 						EGLClass type = (EGLClass)f.getType().getClassifier();
-						ctx.invoke(genExpression, stmt.getDataSource(), ctx, out);
-						out.println(".getResultSet();");
-						out.print(".setObject(");
+						out.print(var_resultSet);
+						out.print(".");
+						genSqlUpdateValueMethodName(type, ctx, out);
+						out.print("(");
 						out.print(quoted(SQL.getColumnName(f)));
-						out.println(", ");
+						out.print(", ");
 						genConvertValueStart(type, ctx, out);
 						ctx.invoke(genExpression, stmt.getTarget(), ctx, out);
 						out.print('.');
 						ctx.invoke(genName, f, ctx, out);
-						genConvertValueStart(type, ctx, out);
+						genConvertValueEnd(type, ctx, out);
 						out.println(");");
 					}
 				}
@@ -56,4 +65,10 @@ public class SqlReplaceStatementTemplate extends SqlActionStatementTemplate {
 			genSqlStatementEnd(stmt, ctx, out);
 		}
 	}	
+	public void genSqlUpdateValueMethodName(Classifier type, Context ctx, TabbedWriter out) {
+		String name = "update";
+		name += SQL.getSqlSimpleTypeName(type);
+		out.print(name);
+	}
+
 }
