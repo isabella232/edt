3b7c37f2bd1abe0ef10c3c0cad293795494903ce
jqian <jqian>
2011-10-18 15:22:52 +0000

    print out more info on screen

--- runtimes/org.eclipse.edt.runtime.javascript/runtime/org/eclipse/edt/eunit/runtime/TestListMgr.js
+++ runtimes/org.eclipse.edt.runtime.javascript/runtime/org/eclipse/edt/eunit/runtime/TestListMgr.js
@@ -5,6 +5,11 @@ egl.defineRUILibrary('org.eclipse.edt.eunit.runtime', 'TestListMgr',
 		"constructor": function() {
 			if(egl.org.eclipse.edt.eunit.runtime.TestListMgr['$inst']) return egl.org.eclipse.edt.eunit.runtime.TestListMgr['$inst'];
 			egl.org.eclipse.edt.eunit.runtime.TestListMgr['$inst']=this;
+			egl.loadScript( "org.eclipse.edt.eunit.runtime","Status" );
+			egl.loadScript( "org.eclipse.edt.eunit.runtime","ConstantsLib" );
+			egl.loadScript( "org.eclipse.edt.eunit.runtime","AssertionFailedException" );
+			egl.loadScript( "org.eclipse.edt.eunit.runtime","MultiStatus" );
+			egl.loadScript( "org.eclipse.edt.eunit.runtime","LogResult" );
 			new egl.org.eclipse.edt.eunit.runtime.ConstantsLib();
 			new egl.org.eclipse.edt.eunit.runtime.LogResult();
 			this.eze$$setInitial();
@@ -22,10 +27,16 @@ egl.defineRUILibrary('org.eclipse.edt.eunit.runtime', 'TestListMgr',
 		}
 		,
 		"eze$$setInitial": function() {
-			this.eze$$setEmpty();
-			this.bindingType = egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ServiceBindingType).DEDICATED;
-			this.testIndex = 1;
-			this.libIndex = 1;
+			try { egl.enter("<init>",this,arguments);
+				this.eze$$setEmpty();
+				this.bindingType = egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ServiceBindingType).DEDICATED;
+				this.testIndex = 1;
+				this.libIndex = 1;
+				if (!egl.debugg) egl.leave();
+			} finally {
+				if (!egl.debugg){
+				} else { egl.leave(); }
+			}
 		}
 		,
 		"eze$$getAnnotations": function() {
@@ -77,104 +88,214 @@ egl.defineRUILibrary('org.eclipse.edt.eunit.runtime', 'TestListMgr',
 		}
 		,
 		"nextTest": function() {
-			var testId = "";
-			testId = this.getTestIdString();
-			this.ms.addStatus(testId);
-			var eze$Temp1 = false;
-			eze$Temp1 = ((this.testIndex < this.runTestMtds.getSize()));
-			if (eze$Temp1) {
-				this.testIndex = ((this.testIndex + 1));
-				egl.checkNull(this.runTestMtds)[this.runTestMtds.checkIndex(egl.eglx.lang.EInt32.ezeCast({eze$$value : this.testIndex, eze$$signature : "I;"},false) - 1)]();
+			try { egl.enter("nextTest",this,arguments);
+				var testId = "";
+				egl.addLocalFunctionVariable("testId", testId, "eglx.lang.EString", "testId");
+				testId = this.getTestIdString();
+				egl.setLocalFunctionVariable("testId", testId, "eglx.lang.EString");
+				egl.eglx.lang.SysLib.writeStdout((("Running test: ") + testId));
+				this.ms.addStatus(testId);
+				var eze$Temp1 = false;
+				eze$Temp1 = ((this.testIndex < this.runTestMtds.getSize()));
+				if (eze$Temp1) {
+					try{egl.enterBlock();
+						this.testIndex = ((this.testIndex + 1));
+						egl.checkNull(this.runTestMtds, "runTestMtds")[this.runTestMtds.checkIndex(egl.eglx.lang.EInt32.ezeCast({eze$$value : this.testIndex, eze$$signature : "I;"},false) - 1)]();
+					}finally{egl.exitBlock();}
+				}
+				if (!egl.debugg) egl.leave();
+			} finally {
+				if (!egl.debugg){
+				} else { egl.leave(); }
 			}
 		}
 		,
 		"getTestIdString": function() {
-			var testMethodNamesSize = 0;
-			testMethodNamesSize = this.testMethodNames.getSize();
-			var testId = "";
-			testId = ((this.testLibName) + "::");
-			if (((this.testIndex <= testMethodNamesSize))) {
-				testId = ((testId) + egl.checkNull(this.testMethodNames)[this.testMethodNames.checkIndex(egl.eglx.lang.EInt32.ezeCast({eze$$value : this.testIndex, eze$$signature : "I;"},false) - 1)]);
-			}
-			else {
-				if (((this.testIndex == ((testMethodNamesSize + 1))))) {
-					testId = ((testId) + "endTest");
+			try { egl.enter("getTestIdString",this,arguments);
+				var testMethodNamesSize = 0;
+				egl.addLocalFunctionVariable("testMethodNamesSize", testMethodNamesSize, "eglx.lang.EInt", "testMethodNamesSize");
+				testMethodNamesSize = this.testMethodNames.getSize();
+				egl.setLocalFunctionVariable("testMethodNamesSize", testMethodNamesSize, "eglx.lang.EInt");
+				var testId = "";
+				egl.addLocalFunctionVariable("testId", testId, "eglx.lang.EString", "testId");
+				testId = ((this.testLibName) + "::");
+				egl.setLocalFunctionVariable("testId", testId, "eglx.lang.EString");
+				if (((this.testIndex <= testMethodNamesSize))) {
+					try{egl.enterBlock();
+						testId = ((testId) + egl.checkNull(this.testMethodNames, "testMethodNames")[this.testMethodNames.checkIndex(egl.eglx.lang.EInt32.ezeCast({eze$$value : this.testIndex, eze$$signature : "I;"},false) - 1)]);
+						egl.setLocalFunctionVariable("testId", testId, "eglx.lang.EString");
+					}finally{egl.exitBlock();}
 				}
 				else {
-					testId = ((testId) + "INVALIDINDEXFOUND!!!");
+					try{egl.enterBlock();
+						if (((this.testIndex == ((testMethodNamesSize + 1))))) {
+							try{egl.enterBlock();
+								testId = ((testId) + "endTest");
+								egl.setLocalFunctionVariable("testId", testId, "eglx.lang.EString");
+							}finally{egl.exitBlock();}
+						}
+						else {
+							try{egl.enterBlock();
+								testId = ((testId) + "INVALIDINDEXFOUND!!!");
+								egl.setLocalFunctionVariable("testId", testId, "eglx.lang.EString");
+							}finally{egl.exitBlock();}
+						}
+					}finally{egl.exitBlock();}
 				}
+				if (!egl.debugg) egl.leave();
+				return testId;
+			} finally {
+				if (!egl.debugg){
+				} else { egl.leave(); }
 			}
-			return testId;
 		}
 		,
 		"nextTestLibrary": function() {
-			var eze$Temp5 = false;
-			eze$Temp5 = ((this.libIndex < this.LibraryStartTests.getSize()));
-			if (eze$Temp5) {
-				this.libIndex = ((this.libIndex + 1));
-				egl.checkNull(this.LibraryStartTests)[this.LibraryStartTests.checkIndex(egl.eglx.lang.EInt32.ezeCast({eze$$value : this.libIndex, eze$$signature : "I;"},false) - 1)]();
+			try { egl.enter("nextTestLibrary",this,arguments);
+				var eze$Temp5 = false;
+				eze$Temp5 = ((this.libIndex < this.LibraryStartTests.getSize()));
+				if (eze$Temp5) {
+					try{egl.enterBlock();
+						this.libIndex = ((this.libIndex + 1));
+						egl.checkNull(this.LibraryStartTests, "LibraryStartTests")[this.LibraryStartTests.checkIndex(egl.eglx.lang.EInt32.ezeCast({eze$$value : this.libIndex, eze$$signature : "I;"},false) - 1)]();
+					}finally{egl.exitBlock();}
+				}
+				if (!egl.debugg) egl.leave();
+			} finally {
+				if (!egl.debugg){
+				} else { egl.leave(); }
 			}
 		}
 		,
 		"handleCallBackException": function(exp) {
-			var str = "";
-			str = (((((("Caught service exception: ") + egl.checkNull(exp).messageID)) + ": ")) + egl.checkNull(exp).message);
-			if (egl.isa(exp, "Teglx/services/serviceinvocationexception;")) {
-				var sexp = null;
-				sexp = egl.eglx.lang.EAny.ezeCast({eze$$value : exp, eze$$signature : "Teglx/lang/anyexception;"}, egl.eglx.services.ServiceInvocationException);
-				var s1 = "";
-				s1 = (("detail1:") + egl.checkNull(sexp).detail1);
-				var s2 = "";
-				s2 = (("detail2:") + egl.checkNull(sexp).detail2);
-				var s3 = "";
-				s3 = (("detail3:") + egl.checkNull(sexp).detail3);
-				str = ((str) + egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ConstantsLib['$inst']).NEWLINE);
-				str = ((((str) + s1)) + egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ConstantsLib['$inst']).NEWLINE);
-				str = ((((str) + s2)) + egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ConstantsLib['$inst']).NEWLINE);
-				str = ((((str) + s3)) + egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ConstantsLib['$inst']).NEWLINE);
+			try { egl.enter("handleCallBackException",this,arguments);
+				egl.addLocalFunctionVariable("exp", exp, "eglx.lang.AnyException", "exp");
+				var str = "";
+				egl.addLocalFunctionVariable("str", str, "eglx.lang.EString", "str");
+				str = (((((("Caught service exception: ") + egl.checkNull(exp).messageID)) + ": ")) + egl.checkNull(exp).message);
+				egl.setLocalFunctionVariable("str", str, "eglx.lang.EString");
+				if (egl.isa(exp, "Teglx/services/serviceinvocationexception;")) {
+					try{egl.enterBlock();
+						var sexp = null;
+						egl.addLocalFunctionVariable("sexp", sexp, "eglx.services.ServiceInvocationException", "sexp");
+						sexp = egl.eglx.lang.EAny.ezeCast({eze$$value : exp, eze$$signature : "Teglx/lang/anyexception;"}, egl.eglx.services.ServiceInvocationException);
+						egl.setLocalFunctionVariable("sexp", sexp, "eglx.services.ServiceInvocationException");
+						var s1 = "";
+						egl.addLocalFunctionVariable("s1", s1, "eglx.lang.EString", "s1");
+						s1 = (("detail1:") + egl.checkNull(sexp).detail1);
+						egl.setLocalFunctionVariable("s1", s1, "eglx.lang.EString");
+						var s2 = "";
+						egl.addLocalFunctionVariable("s2", s2, "eglx.lang.EString", "s2");
+						s2 = (("detail2:") + egl.checkNull(sexp).detail2);
+						egl.setLocalFunctionVariable("s2", s2, "eglx.lang.EString");
+						var s3 = "";
+						egl.addLocalFunctionVariable("s3", s3, "eglx.lang.EString", "s3");
+						s3 = (("detail3:") + egl.checkNull(sexp).detail3);
+						egl.setLocalFunctionVariable("s3", s3, "eglx.lang.EString");
+						str = ((str) + egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ConstantsLib['$inst']).NEWLINE);
+						egl.setLocalFunctionVariable("str", str, "eglx.lang.EString");
+						str = ((((str) + s1)) + egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ConstantsLib['$inst']).NEWLINE);
+						egl.setLocalFunctionVariable("str", str, "eglx.lang.EString");
+						str = ((((str) + s2)) + egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ConstantsLib['$inst']).NEWLINE);
+						egl.setLocalFunctionVariable("str", str, "eglx.lang.EString");
+						str = ((((str) + s3)) + egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ConstantsLib['$inst']).NEWLINE);
+						egl.setLocalFunctionVariable("str", str, "eglx.lang.EString");
+					}finally{egl.exitBlock();}
+				}
+				var eze$Temp8 = null;
+				eze$Temp8 = egl.eglx.lang.EAny.ezeWrap(str);
+				egl.org.eclipse.edt.eunit.runtime.LogResult['$inst'].error(eze$Temp8);
+				str = eze$Temp8.ezeUnbox();
+				egl.setLocalFunctionVariable("str", str, "eglx.lang.EString");
+				var testId = "";
+				egl.addLocalFunctionVariable("testId", testId, "eglx.lang.EString", "testId");
+				testId = egl.checkNull(egl.checkNull(egl.org.eclipse.edt.eunit.runtime.TestListMgr['$inst']).testMethodNames, "testMethodNames")[egl.checkNull(egl.org.eclipse.edt.eunit.runtime.TestListMgr['$inst']).testMethodNames.checkIndex(egl.eglx.lang.EInt32.ezeCast({eze$$value : egl.checkNull(egl.org.eclipse.edt.eunit.runtime.TestListMgr['$inst']).testIndex, eze$$signature : "I;"},false) - 1)];
+				egl.setLocalFunctionVariable("testId", testId, "eglx.lang.EString");
+				this.nextTest();
+				if (!egl.debugg) egl.leave();
+			} finally {
+				if (!egl.debugg){
+				} else { egl.leave(); }
 			}
-			var eze$Temp8 = null;
-			eze$Temp8 = egl.eglx.lang.EAny.ezeWrap(str);
-			egl.org.eclipse.edt.eunit.runtime.LogResult['$inst'].error(eze$Temp8);
-			str = eze$Temp8.ezeUnbox();
-			var testId = "";
-			testId = egl.checkNull(egl.checkNull(egl.org.eclipse.edt.eunit.runtime.TestListMgr['$inst']).testMethodNames)[egl.checkNull(egl.org.eclipse.edt.eunit.runtime.TestListMgr['$inst']).testMethodNames.checkIndex(egl.eglx.lang.EInt32.ezeCast({eze$$value : egl.checkNull(egl.org.eclipse.edt.eunit.runtime.TestListMgr['$inst']).testIndex, eze$$signature : "I;"},false) - 1)];
-			this.nextTest();
 		}
 		,
 		"caughtFailedAssertion": function(exp) {
-			var assertMsg = "";
-			assertMsg = "Assertion failed for: ";
-			var s = new egl.org.eclipse.edt.eunit.runtime.Status();
-			s = egl.org.eclipse.edt.eunit.runtime.LogResult['$inst'].getStatus();
-			s.reason = ((assertMsg) + s.reason);
+			try { egl.enter("caughtFailedAssertion",this,arguments);
+				egl.addLocalFunctionVariable("exp", exp, "org.eclipse.edt.eunit.runtime.AssertionFailedException", "exp");
+				var assertMsg = "";
+				egl.addLocalFunctionVariable("assertMsg", assertMsg, "eglx.lang.EString", "assertMsg");
+				assertMsg = "Assertion failed for: ";
+				egl.setLocalFunctionVariable("assertMsg", assertMsg, "eglx.lang.EString");
+				var s = new egl.org.eclipse.edt.eunit.runtime.Status();
+				egl.addLocalFunctionVariable("s", s, "org.eclipse.edt.eunit.runtime.Status", "s");
+				s = egl.org.eclipse.edt.eunit.runtime.LogResult['$inst'].getStatus();
+				egl.setLocalFunctionVariable("s", s, "org.eclipse.edt.eunit.runtime.Status");
+				s.reason = ((assertMsg) + s.reason);
+				if (!egl.debugg) egl.leave();
+			} finally {
+				if (!egl.debugg){
+				} else { egl.leave(); }
+			}
 		}
 		,
 		"caughtAnyException": function(exp) {
-			var expMsg = "";
-			expMsg = (("uncaught exception for: ") + this.getTestIdString());
-			var eze$Temp9 = null;
-			eze$Temp9 = egl.eglx.lang.EAny.ezeWrap(expMsg);
-			egl.org.eclipse.edt.eunit.runtime.LogResult['$inst'].error(eze$Temp9);
-			expMsg = eze$Temp9.ezeUnbox();
+			try { egl.enter("caughtAnyException",this,arguments);
+				egl.addLocalFunctionVariable("exp", exp, "eglx.lang.AnyException", "exp");
+				var expMsg = "";
+				egl.addLocalFunctionVariable("expMsg", expMsg, "eglx.lang.EString", "expMsg");
+				expMsg = (("uncaught exception for: ") + this.getTestIdString());
+				egl.setLocalFunctionVariable("expMsg", expMsg, "eglx.lang.EString");
+				var eze$Temp9 = null;
+				eze$Temp9 = egl.eglx.lang.EAny.ezeWrap(expMsg);
+				egl.org.eclipse.edt.eunit.runtime.LogResult['$inst'].error(eze$Temp9);
+				expMsg = eze$Temp9.ezeUnbox();
+				egl.setLocalFunctionVariable("expMsg", expMsg, "eglx.lang.EString");
+				if (!egl.debugg) egl.leave();
+			} finally {
+				if (!egl.debugg){
+				} else { egl.leave(); }
+			}
 		}
 		,
 		"getBindingTypeString": function(bType) {
-			if ((bType == egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ServiceBindingType).DEDICATED)) {
-				return "DEDICATED_BINDING";
-			}
-			else {
-				if ((bType == egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ServiceBindingType).DEVELOP)) {
-					return "DEVELOP_BINDING";
+			try { egl.enter("getBindingTypeString",this,arguments);
+				egl.addLocalFunctionVariable("bType", bType, "org.eclipse.edt.eunit.runtime.ServiceBindingType", "bType");
+				if ((bType == egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ServiceBindingType).DEDICATED)) {
+					try{egl.enterBlock();
+						if (!egl.debugg) egl.leave();
+						return "DEDICATED_BINDING";
+					}finally{egl.exitBlock();}
 				}
 				else {
-					if ((bType == egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ServiceBindingType).DEPLOYED)) {
-						return "DEPLOYED_BINDING";
-					}
-					else {
-						return "UNKNOWN Binding Type - NOT supported";
-					}
+					try{egl.enterBlock();
+						if ((bType == egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ServiceBindingType).DEVELOP)) {
+							try{egl.enterBlock();
+								if (!egl.debugg) egl.leave();
+								return "DEVELOP_BINDING";
+							}finally{egl.exitBlock();}
+						}
+						else {
+							try{egl.enterBlock();
+								if ((bType == egl.checkNull(egl.org.eclipse.edt.eunit.runtime.ServiceBindingType).DEPLOYED)) {
+									try{egl.enterBlock();
+										if (!egl.debugg) egl.leave();
+										return "DEPLOYED_BINDING";
+									}finally{egl.exitBlock();}
+								}
+								else {
+									try{egl.enterBlock();
+										if (!egl.debugg) egl.leave();
+										return "UNKNOWN Binding Type - NOT supported";
+									}finally{egl.exitBlock();}
+								}
+							}finally{egl.exitBlock();}
+						}
+					}finally{egl.exitBlock();}
 				}
+				if (!egl.debugg) egl.leave();
+			} finally {
+				if (!egl.debugg){
+				} else { egl.leave(); }
 			}
 		}
 		,
@@ -258,10 +379,10 @@ egl.defineRUILibrary('org.eclipse.edt.eunit.runtime', 'TestListMgr',
 			{name: "bindingType", value : eze$$parent.bindingType, type : "org.eclipse.edt.eunit.runtime.ServiceBindingType", jsName : "bindingType"},
 			{name: "ms", value : eze$$parent.ms, type : "org.eclipse.edt.eunit.runtime.MultiStatus", jsName : "ms"},
 			{name: "testIndex", value : eze$$parent.testIndex, type : "eglx.lang.EInt", jsName : "testIndex"},
-			{name: "testMethodNames", value : eze$$parent.testMethodNames, type : "eglx.lang.EList<eglx.lang.EString>", jsName : "testMethodNames"},
-			{name: "runTestMtds", value : eze$$parent.runTestMtds, type : "eglx.lang.EList<org.eclipse.edt.eunit.runtime.runTestMethod>", jsName : "runTestMtds"},
+			{name: "testMethodNames", value : eze$$parent.testMethodNames, type : "eglx.lang.EList<eglx.lang.EString>", jsName : "!testMethodNames"},
+			{name: "runTestMtds", value : eze$$parent.runTestMtds, type : "eglx.lang.EList<org.eclipse.edt.eunit.runtime.runTestMethod>", jsName : "!runTestMtds"},
 			{name: "testLibName", value : eze$$parent.testLibName, type : "eglx.lang.EString", jsName : "testLibName"},
-			{name: "LibraryStartTests", value : eze$$parent.LibraryStartTests, type : "eglx.lang.EList<org.eclipse.edt.eunit.runtime.runTestMethod>", jsName : "LibraryStartTests"},
+			{name: "LibraryStartTests", value : eze$$parent.LibraryStartTests, type : "eglx.lang.EList<org.eclipse.edt.eunit.runtime.runTestMethod>", jsName : "!LibraryStartTests"},
 			{name: "libIndex", value : eze$$parent.libIndex, type : "eglx.lang.EInt", jsName : "libIndex"}
 			];
 		}
