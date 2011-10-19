e1a8c112653dcee5110e20d0d2e7141876bed022
jvincens <jvincens>
2011-10-19 11:07:19 +0000

    bugzilla 361312

--- gen/org.eclipse.edt.gen.java.templates.eglx.persistence/src/org/eclipse/edt/gen/java/templates/eglx/persistence/sql/SqlCloseStatementTemplate.java
+++ gen/org.eclipse.edt.gen.java.templates.eglx.persistence/src/org/eclipse/edt/gen/java/templates/eglx/persistence/sql/SqlCloseStatementTemplate.java
@@ -3,13 +3,19 @@ package org.eclipse.edt.gen.java.templates.eglx.persistence.sql;
 import org.eclipse.edt.gen.java.Context;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
 import org.eclipse.edt.mof.eglx.persistence.sql.SqlCloseStatement;
+import org.eclipse.edt.mof.eglx.persistence.sql.utils.SQL;
 
 public class SqlCloseStatementTemplate extends SqlActionStatementTemplate {
 
 	public void genStatementBody(SqlCloseStatement stmt, Context ctx, TabbedWriter out) {
 		out.println("try {");
 		ctx.invoke(genExpression, stmt.getTarget(), ctx, out);
-		out.println(".getResultSet().close();");
+		if(SQL.isSQLResultSet(stmt.getTarget().getType())){
+			out.println(".getResultSet().close();");
+		}
+		else{
+			out.println(".getConnection().close();");
+		}
 		genSqlStatementEnd(stmt, ctx, out);
 	}
 
