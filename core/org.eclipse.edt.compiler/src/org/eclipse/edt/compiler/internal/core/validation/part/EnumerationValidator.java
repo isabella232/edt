bef160733d8940b291fdf18660b5005e06f929ad
pharmon <pharmon>
2011-10-26 15:27:42 +0000

    bug 354861 - add validation for enumerations

--- core/org.eclipse.edt.compiler/src/org/eclipse/edt/compiler/internal/core/validation/part/EnumerationValidator.java
+++ core/org.eclipse.edt.compiler/src/org/eclipse/edt/compiler/internal/core/validation/part/EnumerationValidator.java
@@ -11,9 +11,17 @@
  *******************************************************************************/
 package org.eclipse.edt.compiler.internal.core.validation.part;
 
-import org.eclipse.edt.compiler.core.IEGLConstants;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.edt.compiler.binding.Binding;
+import org.eclipse.edt.compiler.binding.EnumerationDataBinding;
 import org.eclipse.edt.compiler.core.ast.AbstractASTVisitor;
 import org.eclipse.edt.compiler.core.ast.Enumeration;
+import org.eclipse.edt.compiler.core.ast.Expression;
+import org.eclipse.edt.compiler.core.ast.IntegerLiteral;
+import org.eclipse.edt.compiler.core.ast.Node;
+import org.eclipse.edt.compiler.core.ast.UnaryExpression;
 import org.eclipse.edt.compiler.internal.core.builder.IProblemRequestor;
 import org.eclipse.edt.compiler.internal.core.lookup.ICompilerOptions;
 
@@ -25,6 +33,7 @@ public class EnumerationValidator extends AbstractASTVisitor {
 	
 	protected IProblemRequestor problemRequestor;
 	private ICompilerOptions compilerOptions;
+	private List<Integer> alreadySeenValues;
 	
 	public EnumerationValidator(IProblemRequestor problemRequestor, ICompilerOptions compilerOptions) {
 		this.problemRequestor = problemRequestor;
@@ -32,16 +41,63 @@ public class EnumerationValidator extends AbstractASTVisitor {
 	}
 	
 	public boolean visit(Enumeration enumeration) {
-//		if(true) {
-//			problemRequestor.acceptProblem(
-//				enumeration.getName(),
-//				IProblemRequestor.PART_OR_STATEMENT_NOT_SUPPORTED,
-//				new String[] {
-//					IEGLConstants.KEYWORD_ENUMERATION.toUpperCase()
-//				});
-//			return false;
-//		}
-		
+		alreadySeenValues = new ArrayList<Integer>();
 		return true;
+	}
+	
+	public boolean visit(org.eclipse.edt.compiler.core.ast.EnumerationField enumerationField) {
+		if (!validateValue(enumerationField)) {
+			return false;
+		}
+		if (Binding.isValidBinding(enumerationField.getName().resolveDataBinding())) {
+			EnumerationDataBinding enumBinding = (EnumerationDataBinding) enumerationField.getName().resolveDataBinding();
+			Integer value = new Integer(enumBinding.geConstantValue());
+			if (alreadySeenValues.contains(new Integer(enumBinding.geConstantValue()))) {
+				Node node = enumerationField.getName();
+				if (enumerationField.hasConstantValue()) {
+					node = enumerationField.getConstantValue();
+				}				
+				problemRequestor.acceptProblem(
+						node,
+						IProblemRequestor.ENUMERATION_CONSTANT_DUPLICATE,
+						new String[] {
+							value.toString(),
+							enumBinding.getDeclaringPart().getCaseSensitiveName()
+						}
+				);
+			}
+			else {
+				alreadySeenValues.add(value);
+			}
+		}
+		
+		return false;
+	}
+	
+	private boolean validateValue(org.eclipse.edt.compiler.core.ast.EnumerationField enumerationField) {
+		if (enumerationField.hasConstantValue()) {
+			Expression val = enumerationField.getConstantValue();
+			while (val instanceof UnaryExpression) {
+				val = ((UnaryExpression)val).getExpression();
+			}
+			if (val instanceof IntegerLiteral) {
+				String value = ((IntegerLiteral)val).getValue();
+				try {
+					Integer.valueOf(value);
+					return true;
+				} catch (NumberFormatException e) {
+				}				
+			}
+			problemRequestor.acceptProblem(
+					enumerationField.getConstantValue(),
+					IProblemRequestor.ENUMERATION_CONSTANT_INVALID,
+					new String[] {
+						enumerationField.getConstantValue().getCanonicalString()
+					}
+			);
+			return false;
+		}
+		return true;
+		
 	}
 }
