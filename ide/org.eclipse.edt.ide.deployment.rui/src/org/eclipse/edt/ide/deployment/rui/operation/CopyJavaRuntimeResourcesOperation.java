a145e9b12592f65287ce2db154deac3177f33fbc
yfengma <yfengma>
2011-10-27 12:51:59 +0000

    358472 - Deployment not adding EDT Java runtime to target project for development workspace

--- ide/org.eclipse.edt.ide.deployment.rui/src/org/eclipse/edt/ide/deployment/rui/operation/CopyJavaRuntimeResourcesOperation.java
+++ ide/org.eclipse.edt.ide.deployment.rui/src/org/eclipse/edt/ide/deployment/rui/operation/CopyJavaRuntimeResourcesOperation.java
@@ -11,9 +11,20 @@
  *******************************************************************************/
 package org.eclipse.edt.ide.deployment.rui.operation;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.jar.Attributes;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+import java.util.jar.Manifest;
+import java.util.zip.CRC32;
+import java.util.zip.ZipOutputStream;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
@@ -35,38 +46,102 @@ public class CopyJavaRuntimeResourcesOperation extends AbstractDeploymentOperati
 
 	private static final String WEBLIB_FOLDER = "WEB-INF/lib/";
 	private static final String JAVARUNTIME_NAME = "org.eclipse.edt.runtime.java.jar";
+	private static final String ICU4J_NAME = "icu4j-4_4_2_2.jar";
 	
 	private String targetProjectName;
 	private DeploymentDesc model;
 	private DeploymentContext context;
+	private IFolder projectRootFolder;
 
 	public void execute(DeploymentContext context, IDeploymentResultsCollector resultsCollector, IProgressMonitor monitor)
 			throws CoreException {
 		this.context = context;
 		model = context.getDeploymentDesc();
-		IFolder projectRootFolder = Utils.getContextDirectory( context.getTargetProject() );
+		projectRootFolder = Utils.getContextDirectory( context.getTargetProject() );
 		
 		try {
 			File javaRuntimeLoc = FileLocator.getBundleFile(Platform.getBundle("org.eclipse.edt.runtime.java"));
+			InputStream fis = null;
 			if ( !javaRuntimeLoc.isFile() || !javaRuntimeLoc.exists() ) {
-				return;
-			}
-			IPath path = new Path( WEBLIB_FOLDER + JAVARUNTIME_NAME );
-			IPath targetFilePath = projectRootFolder.getFullPath().append( path );
-			CoreUtility.createFolder( ResourcesPlugin.getWorkspace().getRoot().getFolder( targetFilePath.removeLastSegments( 1 ) ), true, true, monitor );
+				InputStream icuis = new FileInputStream( new File( javaRuntimeLoc + "/" + ICU4J_NAME ) ); 
+				copyFile( icuis, ICU4J_NAME, monitor );
+				icuis.close();
+				
+				ByteArrayOutputStream bos = new ByteArrayOutputStream();
+				Manifest manifest = new Manifest();
+				manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1.0");
 
-			FileInputStream fis = new FileInputStream( javaRuntimeLoc ) ;
-			
-			IFile targetFile = ResourcesPlugin.getWorkspace().getRoot().getFile(targetFilePath);
-			if( targetFile.exists() ) {
-				targetFile.setContents(fis, true, true, monitor);
+		        ZipOutputStream zos = new JarOutputStream(bos, manifest);
+		        CRC32 crc = new CRC32();
+		        createRuntimeJar( zos, crc, javaRuntimeLoc, javaRuntimeLoc.getPath().length() + 4);
+		        zos.close();
+		        fis = new ByteArrayInputStream( bos.toByteArray() );
 			} else {
-				targetFile.create(fis, true, monitor);
-//					targetFile.setLocalTimeStamp(file.getLocalTimeStamp());
+				fis = new FileInputStream( javaRuntimeLoc ) ;
 			}
+			
+			copyFile( fis, JAVARUNTIME_NAME, monitor );
 			fis.close();
+			
 //				DeploymentUtilities.copyFile(resource.getInputStream(), path.toFile().getPath() );
 		} catch (IOException e) {
 		}
+	}
+	
+	private void copyFile( InputStream fis, String targetName, IProgressMonitor monitor ) throws CoreException{
+		IPath path = new Path( WEBLIB_FOLDER + targetName );
+		IPath targetFilePath = projectRootFolder.getFullPath().append( path );
+		CoreUtility.createFolder( ResourcesPlugin.getWorkspace().getRoot().getFolder( targetFilePath.removeLastSegments( 1 ) ), true, true, monitor );
+		IFile targetFile = ResourcesPlugin.getWorkspace().getRoot().getFile(targetFilePath);
+		if( targetFile.exists() ) {
+			targetFile.setContents(fis, true, true, monitor);
+		} else {
+			targetFile.create(fis, true, monitor);
+//				targetFile.setLocalTimeStamp(file.getLocalTimeStamp());
+		}
+	}
+	
+	private void createRuntimeJar( ZipOutputStream zos, CRC32 crc, File javaRuntimeLoc, int len ) {
+        int bytesRead;
+        for (File file : javaRuntimeLoc.listFiles( new FilenameFilter() {
+
+			@Override
+			public boolean accept(File dir, String name) {
+				if ( dir.getPath().indexOf( "bin" ) >= 0 || "bin".equals( name ) ) {
+					return true;
+				}
+				return false;
+			}}) ) {
+        	try {
+	            if (file.isFile()) {
+	                byte[] buffer = new byte[1024];
+		            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+		            String fileName = file.getPath().substring( len + 1 );
+		            fileName = fileName.replace("\\", "/");
+
+		            JarEntry entry = new JarEntry(fileName);
+		            entry.setSize(file.length());
+		            entry.setTime( file.lastModified() );
+		            zos.putNextEntry(entry);
+		            while ((bytesRead = bis.read(buffer)) != -1) {
+		                zos.write(buffer, 0, bytesRead);
+		            }
+		            bis.close();
+		            zos.closeEntry();
+	            } else {
+		            String fileName = file.getPath().substring( len );
+		            fileName = fileName.replace("\\", "/");
+		            if (!fileName.endsWith("/"))
+		            	fileName += "/";
+
+		            JarEntry entry = new JarEntry(fileName);
+		            entry.setTime( file.lastModified() );
+		            zos.putNextEntry(entry);
+	            	createRuntimeJar( zos, crc, file, len );
+	            }
+        	} catch ( Exception e ) {
+        		e.printStackTrace();
+        	}
+        }
 	}
 }
