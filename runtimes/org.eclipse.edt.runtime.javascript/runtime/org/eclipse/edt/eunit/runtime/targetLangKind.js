be6fbdacc5ad8fd1c61debb80598981fb2d890f9
jqian <jqian>
2011-10-28 19:28:59 +0000

    added enumeration for target language kind

---
+++ runtimes/org.eclipse.edt.runtime.javascript/runtime/org/eclipse/edt/eunit/runtime/targetLangKind.js
@@ -0,0 +1,9 @@
+egl.defineClass('org.eclipse.edt.eunit.runtime', "targetLangKind", "eglx.lang", "Enumeration",{
+		"constructor": function(valueIn) {
+			this.value = valueIn;
+		}
+	}
+);
+egl.org.eclipse.edt.eunit.runtime.targetLangKind['JAVA'] = new egl.org.eclipse.edt.eunit.runtime.targetLangKind(1);
+egl.org.eclipse.edt.eunit.runtime.targetLangKind['JAVASCRIPT'] = new egl.org.eclipse.edt.eunit.runtime.targetLangKind(2);
+;
