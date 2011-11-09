d8984e16398316b0db02982e55d25ac81fda0aea
jdouglas <jdouglas>
2011-11-09 22:30:13 +0000

    Replaced eunit runtime and fixed object comparisons

---
+++ gen/org.eclipse.edt.gen.java.templates.egl.lang/src/org/eclipse/edt/gen/java/templates/eglx/lang/AnyEnumerationTypeTemplate.java
@@ -0,0 +1,87 @@
+package org.eclipse.edt.gen.java.templates.eglx.lang;
+
+import org.eclipse.edt.gen.GenerationException;
+import org.eclipse.edt.gen.java.Constants;
+import org.eclipse.edt.gen.java.Context;
+import org.eclipse.edt.gen.java.templates.JavaTemplate;
+import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.BinaryExpression;
+import org.eclipse.edt.mof.egl.Type;
+
+public class AnyEnumerationTypeTemplate extends JavaTemplate {
+
+	public void genBinaryExpression(Type type, Context ctx, TabbedWriter out, BinaryExpression arg) throws GenerationException {
+		ctx.invoke(genExpression, arg.getLHS(), ctx, out);
+		out.print(getNativeEnumerationOperation(arg, ctx));
+		ctx.invoke(genExpression, arg.getRHS(), ctx, out);
+	}
+
+	@SuppressWarnings("static-access")
+	protected String getNativeEnumerationOperation(BinaryExpression expr, Context ctx) {
+		String op = expr.getOperator();
+		// if we are to use egl overflow checking, then don't pass back that we can do the mathematical operations in java
+		if (expr.isNullable() || (Boolean) ctx.getParameter(Constants.parameter_checkOverflow)) {
+			if (op.equals(expr.Op_EQ))
+				return " == ";
+			if (op.equals(expr.Op_NE))
+				return " != ";
+			if (op.equals(expr.Op_LT))
+				return " < ";
+			if (op.equals(expr.Op_GT))
+				return " > ";
+			if (op.equals(expr.Op_LE))
+				return " <= ";
+			if (op.equals(expr.Op_GE))
+				return " >= ";
+			if (op.equals(expr.Op_AND))
+				return " && ";
+			if (op.equals(expr.Op_OR))
+				return " || ";
+			if (op.equals(expr.Op_XOR))
+				return " ^ ";
+			if (op.equals(expr.Op_CONCAT))
+				return " + ";
+			if (op.equals(expr.Op_BITAND))
+				return " & ";
+			if (op.equals(expr.Op_BITOR))
+				return " | ";
+			return "";
+		}
+		// these are the defaults for all other types
+		// division is intentionally left off as all division must be done through the egl runtime
+		if (op.equals(expr.Op_PLUS))
+			return " + ";
+		if (op.equals(expr.Op_MINUS))
+			return " - ";
+		if (op.equals(expr.Op_MULTIPLY))
+			return " * ";
+		if (op.equals(expr.Op_MODULO))
+			return " % ";
+		if (op.equals(expr.Op_EQ))
+			return " == ";
+		if (op.equals(expr.Op_NE))
+			return " != ";
+		if (op.equals(expr.Op_LT))
+			return " < ";
+		if (op.equals(expr.Op_GT))
+			return " > ";
+		if (op.equals(expr.Op_LE))
+			return " <= ";
+		if (op.equals(expr.Op_GE))
+			return " >= ";
+		if (op.equals(expr.Op_AND))
+			return " && ";
+		if (op.equals(expr.Op_OR))
+			return " || ";
+		if (op.equals(expr.Op_XOR))
+			return " ^ ";
+		if (op.equals(expr.Op_CONCAT))
+			return " + ";
+		if (op.equals(expr.Op_BITAND))
+			return " & ";
+		if (op.equals(expr.Op_BITOR))
+			return " | ";
+		return "";
+	}
+
+}
