8ce3995b25ec29ddd0f7a63ea3e20ce060134916
jihuang <jihuang>
2011-11-13 13:16:51 +0000

    Bug 362856 - Runtime errors accessing values from an array

--- gen/org.eclipse.edt.gen.javascript.templates/src/org/eclipse/edt/gen/javascript/templates/ArrayLiteralTemplate.java
+++ gen/org.eclipse.edt.gen.javascript.templates/src/org/eclipse/edt/gen/javascript/templates/ArrayLiteralTemplate.java
@@ -11,16 +11,41 @@
  *******************************************************************************/
 package org.eclipse.edt.gen.javascript.templates;
 
+import java.util.List;
+
 import org.eclipse.edt.gen.javascript.Context;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
 import org.eclipse.edt.mof.egl.ArrayLiteral;
+import org.eclipse.edt.mof.egl.ArrayType;
+import org.eclipse.edt.mof.egl.BoxingExpression;
+import org.eclipse.edt.mof.egl.EGLClass;
 import org.eclipse.edt.mof.egl.Expression;
+import org.eclipse.edt.mof.egl.IrFactory;
+import org.eclipse.edt.mof.egl.MofConversion;
+import org.eclipse.edt.mof.egl.Type;
+import org.eclipse.edt.mof.egl.utils.TypeUtils;
 
 public class ArrayLiteralTemplate extends JavaScriptTemplate {
 
 	public void genExpression(ArrayLiteral expr, Context ctx, TabbedWriter out) {
 		out.print("[");
-		ctx.foreach(expr.getEntries(), ',', genExpression, ctx, out);
+		Type elementType = ((ArrayType)expr.getType()).getElementType();
+		if(elementType instanceof EGLClass && elementType.getTypeSignature().equalsIgnoreCase("eglx.lang.AnyValue")){
+			List<Expression> objs = expr.getEntries();
+			for(int i=0; i< objs.size(); i++ ){
+				Expression obj = objs.get(i);
+				if (obj.getType() != TypeUtils.Type_ANY) {
+					BoxingExpression boxingExpr = IrFactory.INSTANCE.createBoxingExpression();
+					boxingExpr.setExpr(obj);
+					ctx.invoke(genExpression, boxingExpr, ctx, out);					
+				}
+				if(i < objs.size() - 1){
+					out.print(", ");
+				}				
+			}			
+		}else{
+			ctx.foreach(expr.getEntries(), ',', genExpression, ctx, out);	
+		}			
 		out.print("]");
 		if (!expr.getEntries().isEmpty()) {
 			out.print(".setType(");
