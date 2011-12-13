62afc3d5ab6c20537fd263c6370d3f6576bfdcb1
xbinchen <xbinchen>
2011-12-13 08:21:40 +0000

    Refactor CA code for using statement

--- ide/org.eclipse.edt.ide.ui/src/org/eclipse/edt/ide/ui/internal/contentassist/referencecompletion/EGLSQLUsingStatementReferenceCompletion.java
+++ ide/org.eclipse.edt.ide.ui/src/org/eclipse/edt/ide/ui/internal/contentassist/referencecompletion/EGLSQLUsingStatementReferenceCompletion.java
@@ -24,36 +24,46 @@ public class EGLSQLUsingStatementReferenceCompletion extends
 
 	@Override
 	protected void precompileContexts() {
-		addContext("package a; function a() open a using "); //$NON-NLS-1$
-		addContext("package a; function a() open a using b, "); //$NON-NLS-1$
-		addContext("package a; function a() open a from b using "); //$NON-NLS-1$
-		addContext("package a; function a() open a from b using c, "); //$NON-NLS-1$
-		addContext("package a; function a() get a using "); //$NON-NLS-1$
-		addContext("package a; function a() get a using b, "); //$NON-NLS-1$
-		addContext("package a; function a() get a from b using "); //$NON-NLS-1$
-		addContext("package a; function a() get a from b using c, "); //$NON-NLS-1$
-		addContext("package a; function a() delete from b using "); //$NON-NLS-1$
-		addContext("package a; function a() delete from b using c, "); //$NON-NLS-1$
-		addContext("package a; function a() delete a from b using "); //$NON-NLS-1$
-		addContext("package a; function a() delete a from b using c, "); //$NON-NLS-1$
-		addContext("package a; function a() replace a to b using "); //$NON-NLS-1$
-		addContext("package a; function a() replace a to b using c, "); //$NON-NLS-1$
-		addContext("package a; function a() execute using "); //$NON-NLS-1$
-		addContext("package a; function a() execute using c, "); //$NON-NLS-1$
+		addContext("package a; function a() open a using( "); //$NON-NLS-1$
+		addContext("package a; function a() open a using(b, "); //$NON-NLS-1$
+		addContext("package a; function a() open a from b(using "); //$NON-NLS-1$
+		addContext("package a; function a() open a from b using(c, "); //$NON-NLS-1$
+		addContext("package a; function a() get a using( "); //$NON-NLS-1$
+		addContext("package a; function a() get a using(b, "); //$NON-NLS-1$
+		addContext("package a; function a() get a from b using( "); //$NON-NLS-1$
+		addContext("package a; function a() get a from b using(c, "); //$NON-NLS-1$
+		addContext("package a; function a() delete from b using( "); //$NON-NLS-1$
+		addContext("package a; function a() delete from b using(c, "); //$NON-NLS-1$
+		addContext("package a; function a() delete a from b using( "); //$NON-NLS-1$
+		addContext("package a; function a() delete a from b using(c, "); //$NON-NLS-1$
+		addContext("package a; function a() replace a to b using( "); //$NON-NLS-1$
+		addContext("package a; function a() replace a to b using(c, "); //$NON-NLS-1$
+		addContext("package a; function a() execute using("); //$NON-NLS-1$
+		addContext("package a; function a() execute using(c, "); //$NON-NLS-1$
 	}
 
 	@Override
 	protected List returnCompletionProposals(ParseStack parseStack,
 			final String prefix, final ITextViewer viewer, final int documentOffset) {
 		final List proposals = new ArrayList();
-
-		getBoundASTNodeForOffsetInStatement(viewer, documentOffset,
-				new IBoundNodeProcessor() {
-					public void processBoundNode(Node boundNode) {
-						proposals.addAll(new EGLDeclarationProposalHandler(
-								viewer, documentOffset, prefix, boundNode).getProposals(null, false, true));
-					}
-				});
+		if(isState(parseStack, ((Integer) validStates.get(0)).intValue()) 		||
+				isState(parseStack, ((Integer) validStates.get(2)).intValue())	||
+				isState(parseStack, ((Integer) validStates.get(4)).intValue())	||
+				isState(parseStack, ((Integer) validStates.get(6)).intValue()) 	||
+				isState(parseStack, ((Integer) validStates.get(8)).intValue()) 	||
+				isState(parseStack, ((Integer) validStates.get(10)).intValue())	||
+				isState(parseStack, ((Integer) validStates.get(12)).intValue())	||
+				isState(parseStack, ((Integer) validStates.get(14)).intValue())	||
+				isState(parseStack, ((Integer) validStates.get(16)).intValue())){
+					getBoundASTNodeForOffsetInStatement(viewer, documentOffset,
+					new IBoundNodeProcessor() {
+						public void processBoundNode(Node boundNode) {
+							proposals.addAll(new EGLDeclarationProposalHandler(
+									viewer, documentOffset, prefix, boundNode).getProposals(null, false, true));
+						}
+					});
+			
+		}
 
 		return proposals;
 	}
