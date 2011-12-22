2f89e37e7127ef1bebdb8f3c87f1042c1bd80cd5
jdouglas <jdouglas>
2011-12-22 15:58:03 +0000

    Fixed spelling mistake for constant

--- gen/org.eclipse.edt.gen/src/org/eclipse/edt/gen/Constants.java
+++ gen/org.eclipse.edt.gen/src/org/eclipse/edt/gen/Constants.java
@@ -34,7 +34,7 @@ public class Constants {
 	public static final String SubKey_functionArgumentNeedsWrapping = "functionArgumentNeedsWrapping";
 	public static final String SubKey_statementNeedsLabel = "statementNeedsLabel";
 	public static final String SubKey_statementHasBeenReorganized = "statementHasBeenReorganized";
-	public static final String SubKey_fieldsProccessed4Resource = "fieldsProccessed4Resource";
+	public static final String SubKey_fieldsProcessed4Resource = "fieldsProcessed4Resource";
 
 	// invoke names
 	public static final String isAssignmentBreakupWanted = "isAssignmentBreakupWanted";
