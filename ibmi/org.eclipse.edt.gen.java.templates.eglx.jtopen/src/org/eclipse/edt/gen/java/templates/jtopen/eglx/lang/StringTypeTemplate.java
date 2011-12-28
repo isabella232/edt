95dcc966b9a276d3212486d9a580a1e7eca20290
jvincens <jvincens>
2011-12-28 20:20:08 +0000

    fix limited string

--- ibmi/org.eclipse.edt.gen.java.templates.eglx.jtopen/src/org/eclipse/edt/gen/java/templates/jtopen/eglx/lang/StringTypeTemplate.java
+++ ibmi/org.eclipse.edt.gen.java.templates.eglx.jtopen/src/org/eclipse/edt/gen/java/templates/jtopen/eglx/lang/StringTypeTemplate.java
@@ -11,16 +11,41 @@
  *******************************************************************************/
 package org.eclipse.edt.gen.java.templates.jtopen.eglx.lang;
 
+import org.eclipse.edt.gen.java.CommonUtilities;
 import org.eclipse.edt.gen.java.Context;
 import org.eclipse.edt.gen.java.templates.eglx.jtopen.Constants;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
 import org.eclipse.edt.mof.egl.Annotation;
+import org.eclipse.edt.mof.egl.Member;
+import org.eclipse.edt.mof.egl.SequenceType;
 import org.eclipse.edt.mof.egl.Type;
 
 public class StringTypeTemplate extends org.eclipse.edt.gen.java.templates.eglx.lang.StringTypeTemplate  implements Constants{
+
+	public void preGenAS400Annotation(SequenceType type, Context ctx, Member member){
+		if (member.getAnnotation(Constants.signature_AS400Text) == null) {
+			try {
+				member.addAnnotation(getAS400FunctionParameterAnnotation(type, ctx));
+			}
+			catch (Exception e) {}
+		}	
+	}
 	public void genLength(Type type, Context ctx, TabbedWriter out, Annotation typeAnnot) {
 		if(typeAnnot != null){
 			out.print(((Integer)typeAnnot.getValue(subKey_length)).toString());
+		}
+	}
+	public void genLength(SequenceType type, Context ctx, TabbedWriter out, Annotation typeAnnot) {
+		out.print(type.getLength().toString());
+	}
+	public Annotation getAS400FunctionParameterAnnotation(SequenceType type, Context ctx){
+		try {
+			Annotation annotation = CommonUtilities.getAnnotation(ctx, Type.EGL_KeyScheme + Type.KeySchemeDelimiter + Constants.signature_AS400Text);
+			annotation.setValue(subKey_length, type.getLength());
+			return annotation;
+		}
+		catch (Exception e) {
+			return null;
 		}
 	}
 }
