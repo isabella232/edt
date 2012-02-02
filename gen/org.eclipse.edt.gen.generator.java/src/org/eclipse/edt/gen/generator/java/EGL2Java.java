d19c923cebba61b077c6efae2f2947d831a823dc
jdouglas <jdouglas>
2012-02-02 18:33:40 +0000

    Bug 369139 - create extensible framework for template plugins

--- gen/org.eclipse.edt.gen.generator.java/src/org/eclipse/edt/gen/generator/java/EGL2Java.java
+++ gen/org.eclipse.edt.gen.generator.java/src/org/eclipse/edt/gen/generator/java/EGL2Java.java
@@ -12,16 +12,12 @@
 package org.eclipse.edt.gen.generator.java;
 
 import org.eclipse.edt.gen.AbstractGeneratorCommand;
-import org.eclipse.edt.gen.java.Constants;
 import org.eclipse.edt.gen.java.JavaGenerator;
 
 public class EGL2Java extends AbstractGeneratorCommand {
 
 	public EGL2Java() {
 		super();
-		// define our local command parameters
-		this.installParameter(false, Constants.parameter_checkOverflow, new String[] { "checkOverflow", "overflow", "co" }, new Boolean[] { false, true },
-			"Overflow must be defined as true or false");
 	}
 
 	public static void main(String[] args) {
@@ -29,28 +25,4 @@ public class EGL2Java extends AbstractGeneratorCommand {
 		EGL2Java genPart = new EGL2Java();
 		genPart.generate(args, new JavaGenerator(genPart), null, null);
 	}
-
-	public String[] getNativeTypePath() {
-		// this defined the locations of the nativeTypes.properties files to be loaded and used
-		return new String[] { "org.eclipse.edt.gen.java.nativeTypes", "org.eclipse.edt.gen.java.templates.eglx.persistence.sql.nativeTypes" };
-	}
-
-	public String[] getPrimitiveTypePath() {
-		// this defined the locations of the primitiveTypes.properties files to be loaded and used
-		return new String[] { "org.eclipse.edt.gen.java.primitiveTypes" };
-	}
-
-	public String[] getEGLMessagePath() {
-		// this defined the locations of the EGLMessages.properties files to be loaded and used
-		return new String[] { "org.eclipse.edt.gen.java.EGLMessages" };
-	}
-
-	public String[] getTemplatePath() {
-		// this defined the locations of the templates.properties files to be loaded and used
-		return new String[] { "org.eclipse.edt.gen.java.templates.eglx.lang.templates",
-				"org.eclipse.edt.gen.java.templates.eglx.persistence.sql.templates",
-				"org.eclipse.edt.gen.java.templates.eglx.persistence.templates",
-				"org.eclipse.edt.gen.java.templates.templates" };
-	}
-
 }
