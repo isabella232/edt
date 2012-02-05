9ee94fc262d9d4cf68c6d1565ace5014856d043f
jdouglas <jdouglas>
2012-02-05 15:50:07 +0000

    Bug 369139 - create extensible framework for template plugins

--- gen/org.eclipse.edt.gen.deployment.javascript/src/org/eclipse/edt/gen/deployment/javascript/HTMLGenerationContribution.java
+++ gen/org.eclipse.edt.gen.deployment.javascript/src/org/eclipse/edt/gen/deployment/javascript/HTMLGenerationContribution.java
@@ -12,10 +12,10 @@
 package org.eclipse.edt.gen.deployment.javascript;
 
 import org.eclipse.edt.gen.CommandOption;
-import org.eclipse.edt.gen.Configurable;
-import org.eclipse.edt.gen.Configurator;
+import org.eclipse.edt.gen.GenerationContributor;
+import org.eclipse.edt.gen.GenerationContribution;
 
-public class HTMLGenConfig implements Configurator {
+public class HTMLGenerationContribution implements GenerationContribution {
 	static final CommandOption[] commandOptions;
 	static final String[] templatePath;
 	static final String[] nativeTypePath;
@@ -49,7 +49,7 @@ public class HTMLGenConfig implements Configurator {
 		};
 	}
 
-	public void configure(Configurable generator) {
+	public void configure(GenerationContributor generator) {
 		// register the array of command options for this configuration
 		// if you don't have any, then register the empty array
 		generator.registerCommandOptions(commandOptions);
