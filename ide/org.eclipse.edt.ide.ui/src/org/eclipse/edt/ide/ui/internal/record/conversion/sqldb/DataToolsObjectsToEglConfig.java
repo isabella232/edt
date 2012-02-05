72cc9058204b813ba64a7b3b09eaec25d7ebe361
jdouglas <jdouglas>
2012-02-05 15:50:10 +0000

    Bug 369139 - create extensible framework for template plugins

--- ide/org.eclipse.edt.ide.ui/src/org/eclipse/edt/ide/ui/internal/record/conversion/sqldb/DataToolsObjectsToEglConfig.java
+++ ide/org.eclipse.edt.ide.ui/src/org/eclipse/edt/ide/ui/internal/record/conversion/sqldb/DataToolsObjectsToEglConfig.java
@@ -12,10 +12,10 @@
 package org.eclipse.edt.ide.ui.internal.record.conversion.sqldb;
 
 import org.eclipse.edt.gen.CommandOption;
-import org.eclipse.edt.gen.Configurable;
-import org.eclipse.edt.gen.Configurator;
+import org.eclipse.edt.gen.GenerationContributor;
+import org.eclipse.edt.gen.GenerationContribution;
 
-public class DataToolsObjectsToEglConfig implements Configurator {
+public class DataToolsObjectsToEglConfig implements GenerationContribution {
 	static final CommandOption[] commandOptions;
 	static final String[] templatePath;
 	static final String[] nativeTypePath;
@@ -52,7 +52,7 @@ public class DataToolsObjectsToEglConfig implements Configurator {
 		};
 	}
 
-	public void configure(Configurable generator) {
+	public void configure(GenerationContributor generator) {
 		// register the array of command options for this configuration
 		// if you don't have any, then register the empty array
 		generator.registerCommandOptions(commandOptions);
