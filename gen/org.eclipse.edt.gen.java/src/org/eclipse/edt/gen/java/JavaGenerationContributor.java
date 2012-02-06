1497093c892aebf037d79f9ecab5acfc4d909db6
jdouglas <jdouglas>
2012-02-06 20:02:39 +0000

    Bug 369139 - create extensible framework for template plugins

--- gen/org.eclipse.edt.gen.java/src/org/eclipse/edt/gen/java/JavaGenerationContributor.java
+++ gen/org.eclipse.edt.gen.java/src/org/eclipse/edt/gen/java/JavaGenerationContributor.java
@@ -13,10 +13,10 @@ package org.eclipse.edt.gen.java;
 
 import org.eclipse.edt.gen.CommandOption;
 import org.eclipse.edt.gen.CommandParameter;
+import org.eclipse.edt.gen.GenerationRegistry;
 import org.eclipse.edt.gen.GenerationContributor;
-import org.eclipse.edt.gen.GenerationContribution;
 
-public class JavaGenerationContribution implements GenerationContribution {
+public class JavaGenerationContributor implements GenerationContributor {
 	static final CommandOption[] commandOptions;
 	static final String[] templatePath;
 	static final String[] nativeTypePath;
@@ -60,7 +60,7 @@ public class JavaGenerationContribution implements GenerationContribution {
 		};
 	}
 
-	public void configure(GenerationContributor generator) {
+	public void contribute(GenerationRegistry generator) {
 		// register the array of command options for this configuration
 		// if you don't have any, then register the empty array
 		generator.registerCommandOptions(commandOptions);
