f8395603335ba551e9256c5bedf1688d265f1246
jspadea <jspadea>
2012-02-09 14:55:36 +0000

    bugzilla 357095 - add filter for *.eglsmap files

---
+++ debug/org.eclipse.edt.debug.ui/src/org/eclipse/edt/debug/internal/ui/java/SMAPViewerFilter.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright © 2012 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.edt.debug.internal.ui.java;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.edt.debug.core.IEGLDebugCoreConstants;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
+/**
+ * Filters out *.eglsmap files from the viewer.
+ */
+public class SMAPViewerFilter extends ViewerFilter
+{
+	@Override
+	public boolean select( Viewer arg0, Object parent, Object element )
+	{
+		return !(element instanceof IFile) || !((IFile)element).getFileExtension().equals( IEGLDebugCoreConstants.SMAP_EXTENSION );
+	}
+}
