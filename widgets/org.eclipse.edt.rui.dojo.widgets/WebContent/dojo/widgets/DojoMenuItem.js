c7c9c798e425f24af10b08ec33e98d36e4be85de
jihuang <jihuang>
2012-02-17 09:39:57 +0000

    Bug 371700 - ID conflict in Dojo gallery for Firefox latest version

--- widgets/org.eclipse.edt.rui.dojo.widgets/WebContent/dojo/widgets/DojoMenuItem.js
+++ widgets/org.eclipse.edt.rui.dojo.widgets/WebContent/dojo/widgets/DojoMenuItem.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright © 2011 IBM Corporation and others.
+ * Copyright ï¿½ 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -60,6 +60,8 @@ egl.defineWidget(
 				this._parent = this.logicalParent;
 			}			
 			this.dojoWidget.destroy();
+		}else{
+			dijit.registry.remove(this.id);
 		}
 	},
 	"setChildren" : function(children){
