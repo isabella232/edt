bf295a355d05a33614e955d972955aeec226f989
jihuang <jihuang>
2012-02-29 01:30:16 +0000

    Bug 372275 - Make string a reference type - JSGen change made by Justin

--- gen/org.eclipse.edt.gen.javascript.templates/src/org/eclipse/edt/gen/javascript/templates/InvocationExpressionTemplate.java
+++ gen/org.eclipse.edt.gen.javascript.templates/src/org/eclipse/edt/gen/javascript/templates/InvocationExpressionTemplate.java
@@ -13,6 +13,7 @@ package org.eclipse.edt.gen.javascript.templates;
 
 import org.eclipse.edt.gen.javascript.Context;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.Expression;
 import org.eclipse.edt.mof.egl.FunctionParameter;
 import org.eclipse.edt.mof.egl.InvocationExpression;
 import org.eclipse.edt.mof.egl.MemberName;
@@ -36,8 +37,26 @@ public class InvocationExpressionTemplate extends JavaScriptTemplate {
 		}
 		ctx.invoke(genName, expr.getTarget(), ctx, out);
 		out.print("(");
-		ctx.foreach(expr.getArguments(), ',', genExpression, ctx, out);
+		ctx.invoke(genInvocationArguments, expr, ctx, out);
 		out.print(")");
+	}
+	
+	public void genInvocationArguments(InvocationExpression expr, Context ctx, TabbedWriter out) {
+		// check to see if we are trying to pass a nullable to a non-nullable
+		if (expr.getArguments() != null) {
+			for (int i = 0; i < expr.getArguments().size(); i++) {
+				Expression argExpr = expr.getArguments().get(i);
+				// if the parameter is non-nullable but the argument is nullable, we have a special case
+				if (!expr.getTarget().getParameters().get(i).isNullable() && argExpr.isNullable()) {
+					out.print("egl.checkNull(");
+					ctx.invoke(genExpression, argExpr, ctx, out);
+					out.print(")");
+				} else
+					ctx.invoke(genExpression, argExpr, ctx, out);
+				if (i < expr.getArguments().size() - 1)
+					out.print(", ");
+			}
+		}
 	}
 
 	protected String resetFunctionParmName(FunctionParameter parm) {
