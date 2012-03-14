f800937c3205e4cc44e8d3e8f0cc73cf6c3c7095
pharmon <pharmon>
2012-03-14 19:57:05 +0000

    Bug 373356  - add support for @Throws

--- core/org.eclipse.edt.compiler/src/org/eclipse/edt/compiler/internal/core/validation/annotation/ThrowsInvocationValidator.java
+++ core/org.eclipse.edt.compiler/src/org/eclipse/edt/compiler/internal/core/validation/annotation/ThrowsInvocationValidator.java
@@ -1,12 +1,15 @@
 package org.eclipse.edt.compiler.internal.core.validation.annotation;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.eclipse.edt.compiler.binding.Binding;
 import org.eclipse.edt.compiler.binding.ExternalTypeBinding;
 import org.eclipse.edt.compiler.binding.IBinding;
 import org.eclipse.edt.compiler.binding.IPartBinding;
 import org.eclipse.edt.compiler.binding.ITypeBinding;
+import org.eclipse.edt.compiler.binding.PartBinding;
 import org.eclipse.edt.compiler.core.ast.Node;
 import org.eclipse.edt.compiler.core.ast.OnExceptionBlock;
 import org.eclipse.edt.compiler.core.ast.TryStatement;
@@ -18,19 +21,23 @@ import org.eclipse.edt.mof.egl.utils.InternUtil;
 public class ThrowsInvocationValidator implements
 		IInvocationValidationRule {
 	
-	private final static String ANYEXCEPTION = InternUtil.intern("anyException");
-	private final static String[] EGLXLAN = InternUtil.intern(new String[] {"eglx", "lang"});
+	private final static String JAVAOBJECTECXPTION = InternUtil.intern("JavaObjectException");
+	private final static String[] EGLXJAVA = InternUtil.intern(new String[] {"eglx", "java"});
+	private List<IPartBinding> validExceptions = new ArrayList<IPartBinding>();
 
 	@Override
 	public void validate(Node node, final IBinding binding,
 			IPartBinding declaringPart, final IProblemRequestor problemRequestor,
 			ICompilerOptions compilerOptions) {
 		
-		if (!Binding.isValidBinding(binding)) {
+		if (!Binding.isValidBinding(binding) || !Binding.isValidBinding(declaringPart)) {
 			return;
 		}
 		
-		if (!isInTryCatchAnyException(node)) {
+		IPartBinding joe = declaringPart.getEnvironment().getPartBinding(EGLXJAVA, JAVAOBJECTECXPTION);
+		buildAllValidExceptions(validExceptions, joe);
+		
+		if (!isInTryCatchException(node)) {
 			problemRequestor.acceptProblem(
 					node, 
 					IProblemRequestor.INVOCATION_MUST_BE_IN_TRY, 
@@ -38,28 +45,25 @@ public class ThrowsInvocationValidator implements
 		}
 	}
 	
-	private boolean isInTryCatchAnyException(Node node) {
+	private boolean isInTryCatchException(Node node) {
 
 		TryStatement tryStmt = getTryStatement(node);
 		if (tryStmt == null) {
 			return false;
 		}
-		if (hasCatchAnyException(tryStmt)) {
+		if (hasCatchException(tryStmt)) {
 			return true;
 		}
-		return isInTryCatchAnyException(tryStmt.getParent());		
+		return isInTryCatchException(tryStmt.getParent());		
 	}
 	
-	private boolean hasCatchAnyException(TryStatement tryStmt) {
+	private boolean hasCatchException(TryStatement tryStmt) {
 		Iterator i = tryStmt.getOnExceptionBlocks().iterator();
 		while (i.hasNext()) {
 			OnExceptionBlock exBlock = (OnExceptionBlock)i.next();
 			Type type = exBlock.getExceptionType();
-			if (type != null && Binding.isValidBinding(type.resolveTypeBinding())) {
-				if (type.resolveTypeBinding().getKind() == ITypeBinding.EXTERNALTYPE_BINDING) {
-					ExternalTypeBinding et = (ExternalTypeBinding)type.resolveTypeBinding();
-					return et.getName() == ANYEXCEPTION && et.getPackageName() == EGLXLAN;
-				}
+			if (type != null && validExceptions.contains(type.resolveTypeBinding())) {
+				return true;
 			}
 		}
 		return false;
@@ -84,4 +88,22 @@ public class ThrowsInvocationValidator implements
 		return getTryStatement(node.getParent());
 		
 	}
+	
+	private void buildAllValidExceptions(List<IPartBinding> list, IPartBinding baseException) {
+		
+		if (!Binding.isValidBinding(baseException) || list.contains(baseException)) {
+			return;
+		}
+		
+		list.add(baseException);
+		
+		if (baseException.getKind() == ITypeBinding.EXTERNALTYPE_BINDING) {
+			ExternalTypeBinding et = (ExternalTypeBinding) baseException;
+			list.addAll(et.getExtendedTypes());
+		}
+		
+		buildAllValidExceptions(list, ((PartBinding)baseException).getDefaultSuperType());
+		
+	}
+
 }
