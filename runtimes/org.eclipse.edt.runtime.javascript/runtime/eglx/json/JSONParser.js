cf9220d50b2d52f0ad3e3828268e767f4378cab0
mheitz <mheitz>
2012-04-04 19:59:46 +0000

    Update copyrights.

--- runtimes/org.eclipse.edt.runtime.javascript/runtime/eglx/json/JSONParser.js
+++ runtimes/org.eclipse.edt.runtime.javascript/runtime/eglx/json/JSONParser.js
@@ -1,33 +1,14 @@
-/* 
- * This class implements a JSON parser to validate JSON input before eval-ing it. 
- * 
- * This code is based on the JSON Parser from the Yahoo UI JSON Utility (http://developer.yahoo.com/yui/json/).
- * That parser in turn is based on Douglas Crockford's JSON parsing and stringifying methods.
- * In the Yahoo JSON Utility, Douglas Crockford's JSON parsing and stringifying methods are adapted from 
- * work published at JSON.org. The adapted work is in the public domain.
- * 
- * Software License Agreement (BSD License)
- * Copyright (c) 2008, 2011, Yahoo! Inc.
- * All rights reserved.
- * 
- * Redistribution and use of this software in source and binary forms, with or without modification, 
- * are permitted provided that the following conditions are met:
- * 
- * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following 
- * disclaimer in the documentation and/or other materials provided with the distribution.
- * Neither the name of Yahoo! Inc. nor the names of its contributors may be used to endorse or promote 
- * products derived from this software without specific prior written permission of Yahoo! Inc.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
- * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
- * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- */
+/*******************************************************************************
+ * Copyright © 2008, 2012 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *
+ *******************************************************************************/
 
 egl.defineClass( 'eglx.json', 'JSONParser', {
 	"constructor" : function()
--- runtimes/org.eclipse.edt.runtime.javascript/runtime/eglx/lang/InvocationException.js
+++ runtimes/org.eclipse.edt.runtime.javascript/runtime/eglx/lang/InvocationException.js
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2011 IBM Corporation and others.
+ * Copyright © 2011, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ * IBM Corporation - initial API and implementation
+ *
  *******************************************************************************/
 egl.defineClass('eglx.lang', "InvocationException", "eglx.lang", "AnyException", {
 	"eze$$fileName" : "eglx/lang/Exceptions.egl",
