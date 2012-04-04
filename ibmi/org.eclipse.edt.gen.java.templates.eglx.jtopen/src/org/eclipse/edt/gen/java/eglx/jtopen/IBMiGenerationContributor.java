4fd819c55382f3b654e5a351802d301bc2c5dac3
mheitz <mheitz>
2012-04-04 19:43:31 +0000

    Update copyrights.

--- ibmi/org.eclipse.edt.gen.java.templates.eglx.jtopen/src/org/eclipse/edt/gen/java/eglx/jtopen/IBMiGenerationContributor.java
+++ ibmi/org.eclipse.edt.gen.java.templates.eglx.jtopen/src/org/eclipse/edt/gen/java/eglx/jtopen/IBMiGenerationContributor.java
@@ -1,6 +1,6 @@
 package org.eclipse.edt.gen.java.eglx.jtopen;
 /*******************************************************************************
- * Copyright Â© 2011 IBM Corporation and others.
+ * Copyright © 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
