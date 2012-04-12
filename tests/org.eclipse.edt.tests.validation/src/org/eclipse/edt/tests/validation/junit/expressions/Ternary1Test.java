7fe6cff47d2127db07096804bfebbebbc2ed8d02
jspadea <jspadea>
2012-04-12 18:34:39 +0000

    *** empty log message ***

---
+++ tests/org.eclipse.edt.tests.validation/src/org/eclipse/edt/tests/validation/junit/expressions/Ternary1Test.java
@@ -0,0 +1,195 @@
+package org.eclipse.edt.tests.validation.junit.expressions;
+
+import java.util.List;
+
+import org.eclipse.edt.tests.validation.junit.ValidationTestCase;
+
+/*
+ * A JUnit test case for the file EGLSource/expressions/ternary1.egl
+ */
+public class Ternary1Test extends ValidationTestCase {
+
+	public Ternary1Test() {
+		super( "EGLSource/expressions/ternary1.egl", false );
+	}
+
+	/*
+	 * x = true ? 10 - 1 : 20;
+	 * 0 validation messages are expected.
+	 */
+	public void testLine11() {
+		List messages = getMessagesAtLine( 11 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * x = x isa int?[] ? [] as int?[] : 20;
+	 * 0 validation messages are expected.
+	 */
+	public void testLine12() {
+		List messages = getMessagesAtLine( 12 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * x = a isa boolean[] ? 10 : 20;
+	 * 0 validation messages are expected.
+	 */
+	public void testLine13() {
+		List messages = getMessagesAtLine( 13 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * x = (5 == 4) ? 2 : 1;
+	 * 0 validation messages are expected.
+	 */
+	public void testLine14() {
+		List messages = getMessagesAtLine( 14 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * x = 5 == 4 ? 2 : 1;
+	 * 0 validation messages are expected.
+	 */
+	public void testLine15() {
+		List messages = getMessagesAtLine( 15 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * x = getValue() > 8 ? getValue() : 1 + getValue() * 22 - 2;
+	 * 0 validation messages are expected.
+	 */
+	public void testLine16() {
+		List messages = getMessagesAtLine( 16 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * a = false ? new int?[] : a2;
+	 * 0 validation messages are expected.
+	 */
+	public void testLine17() {
+		List messages = getMessagesAtLine( 17 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * a = true ? new int[] : a2;
+	 * 0 validation messages are expected.
+	 */
+	public void testLine18() {
+		List messages = getMessagesAtLine( 18 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * s = 56 > 22 ? "a" : "bc";
+	 * 0 validation messages are expected.
+	 */
+	public void testLine19() {
+		List messages = getMessagesAtLine( 19 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * a = doNotExist ? 1 : 3;
+	 * 1 validation message is expected.
+	 */
+	public void testLine21() {
+		List messages = getMessagesAtLine( 21 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * a = true ? doNotExist : 3;
+	 * 1 validation message is expected.
+	 */
+	public void testLine22() {
+		List messages = getMessagesAtLine( 22 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * a = true ? 10 : doNotExist;
+	 * 1 validation message is expected.
+	 */
+	public void testLine23() {
+		List messages = getMessagesAtLine( 23 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * x = 3 ? new date : 2;
+	 * 1 validation message is expected.
+	 */
+	public void testLine26() {
+		List messages = getMessagesAtLine( 26 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * x = "true" ? new date : 2;
+	 * 1 validation message is expected.
+	 */
+	public void testLine27() {
+		List messages = getMessagesAtLine( 27 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * if (true ? true : false) end
+	 * 1 validation message is expected.
+	 */
+	public void testLine30() {
+		List messages = getMessagesAtLine( 30 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * if ((true ? true : false) as boolean) end
+	 * 0 validation messages are expected.
+	 */
+	public void testLine31() {
+		List messages = getMessagesAtLine( 31 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * setValueIn(x > 10 ? x : a);
+	 * 0 validation messages are expected.
+	 */
+	public void testLine33() {
+		List messages = getMessagesAtLine( 33 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * setValueInout(x > 10 ? x : a);
+	 * 1 validation message is expected.
+	 */
+	public void testLine34() {
+		List messages = getMessagesAtLine( 34 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * x = getValue() > 9 ? x > 9 || x % 4 in x ? getValue() - 7 : x * 10 : getValue() + 99;
+	 * 0 validation messages are expected.
+	 */
+	public void testLine37() {
+		List messages = getMessagesAtLine( 37 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * x = getValue() ? x > 9 || x % 4 in x ? getValue() - 7 : x * 10 : getValue() + 99;
+	 * 1 validation message is expected.
+	 */
+	public void testLine38() {
+		List messages = getMessagesAtLine( 38 );
+		assertEquals( 1, messages.size() );
+	}
+}
