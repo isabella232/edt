65fdbf8ab90fac3bc32f25cd58a04dae7b08100c
jdouglas <jdouglas>
2012-04-24 18:37:33 +0000

    *** empty log message ***

--- gen/org.eclipse.edt.gen.javascript.templates/src/org/eclipse/edt/gen/javascript/annotation/templates/ResourceTemplate.java
+++ gen/org.eclipse.edt.gen.javascript.templates/src/org/eclipse/edt/gen/javascript/annotation/templates/ResourceTemplate.java
@@ -11,7 +11,6 @@
  *******************************************************************************/
 package org.eclipse.edt.gen.javascript.annotation.templates;
 
-
 import org.eclipse.edt.compiler.core.IEGLConstants;
 import org.eclipse.edt.gen.Constants;
 import org.eclipse.edt.gen.javascript.Context;
