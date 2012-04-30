04681add29d0400cd08c37666cfb6d27e50c07b5
jdouglas <jdouglas>
2012-04-30 17:56:43 +0000

    *** empty log message ***

--- gen/org.eclipse.edt.gen.generator.example/src/org/eclipse/edt/gen/generator/example/EGL2Example.java
+++ gen/org.eclipse.edt.gen.generator.example/src/org/eclipse/edt/gen/generator/example/EGL2Example.java
@@ -12,7 +12,7 @@
 package org.eclipse.edt.gen.generator.example;
 
 import org.eclipse.edt.gen.generator.java.EGL2Java;
-import org.eclipse.edt.gen.java.JavaGenerator;
+import org.eclipse.edt.gen.java.JavaCoreGenerator;
 
 public class EGL2Example extends EGL2Java {
 
@@ -26,9 +26,9 @@ public class EGL2Example extends EGL2Java {
 		// the constructor for this class to get executed, and it will invoke the super() method call to set up the EGL2Java
 		// defaults. The generate(args) method call will pass the command line arguments along, and set the user's
 		// values based on the parameters that have been defined. Once the parameters have been analyzed and accepted,
-		// generation can begin. The new JavaGenerator(genPart) defines the generator to use. You can use the default one, or
+		// generation can begin. The new JavaCoreGenerator(genPart) defines the generator to use. You can use the default one, or
 		// provide your own generator that extends the default.
 		EGL2Example genPart = new EGL2Example();
-		genPart.generate(args, new JavaGenerator(genPart), null, null);
+		genPart.generate(args, new JavaCoreGenerator(genPart), null, null);
 	}
 }
