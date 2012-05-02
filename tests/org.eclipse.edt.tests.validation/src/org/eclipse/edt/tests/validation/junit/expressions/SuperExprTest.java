8e49e4ad27e270b71257b05898251ebb63b108b2
jspadea <jspadea>
2012-05-02 17:40:36 +0000

    *** empty log message ***

---
+++ tests/org.eclipse.edt.tests.validation/src/org/eclipse/edt/tests/validation/junit/expressions/SuperExprTest.java
@@ -0,0 +1,249 @@
+package org.eclipse.edt.tests.validation.junit.expressions;
+
+import java.util.List;
+
+import org.eclipse.edt.tests.validation.junit.ValidationTestCase;
+
+/*
+ * A JUnit test case for the file EGLSource/expressions/superExpr.egl
+ */
+public class SuperExprTest extends ValidationTestCase {
+
+	public SuperExprTest() {
+		super( "EGLSource/expressions/superExpr.egl", false );
+	}
+
+	/*
+	 * super{x=5},
+	 * 1 validation message is expected.
+	 */
+	public void testLine14() {
+		List messages = getMessagesAtLine( 14 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * super{document = new document}
+	 * 0 validation messages are expected.
+	 */
+	public void testLine15() {
+		List messages = getMessagesAtLine( 15 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * ha h2{super{i = 3}};
+	 * 1 validation message is expected.
+	 */
+	public void testLine21() {
+		List messages = getMessagesAtLine( 21 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * hb h2{super{document = new document}};
+	 * 0 validation messages are expected.
+	 */
+	public void testLine22() {
+		List messages = getMessagesAtLine( 22 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * hc superExpr{super{x = 3}};
+	 * 1 validation message is expected.
+	 */
+	public void testLine24() {
+		List messages = getMessagesAtLine( 24 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * hd superExpr{super{document = new document}};
+	 * 0 validation messages are expected.
+	 */
+	public void testLine25() {
+		List messages = getMessagesAtLine( 25 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * super();
+	 * 0 validation messages are expected.
+	 */
+	public void testLine31() {
+		List messages = getMessagesAtLine( 31 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * super(10);
+	 * 2 validation messages are expected.
+	 */
+	public void testLine32() {
+		List messages = getMessagesAtLine( 32 );
+		assertEquals( 2, messages.size() );
+	}
+
+	/*
+	 * this();
+	 * 1 validation message is expected.
+	 */
+	public void testLine33() {
+		List messages = getMessagesAtLine( 33 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * this(10);
+	 * 1 validation message is expected.
+	 */
+	public void testLine34() {
+		List messages = getMessagesAtLine( 34 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * super.getProperty("");
+	 * 0 validation messages are expected.
+	 */
+	public void testLine35() {
+		List messages = getMessagesAtLine( 35 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * this.getProperty("");
+	 * 0 validation messages are expected.
+	 */
+	public void testLine36() {
+		List messages = getMessagesAtLine( 36 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * a = super.document;
+	 * 0 validation messages are expected.
+	 */
+	public void testLine37() {
+		List messages = getMessagesAtLine( 37 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * a = this.document;
+	 * 0 validation messages are expected.
+	 */
+	public void testLine38() {
+		List messages = getMessagesAtLine( 38 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * super(10);
+	 * 1 validation message is expected.
+	 */
+	public void testLine47() {
+		List messages = getMessagesAtLine( 47 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * super();
+	 * 1 validation message is expected.
+	 */
+	public void testLine48() {
+		List messages = getMessagesAtLine( 48 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * hy h2{super{i = 3}};
+	 * 1 validation message is expected.
+	 */
+	public void testLine52() {
+		List messages = getMessagesAtLine( 52 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * hz h2{super{document = new document}};
+	 * 0 validation messages are expected.
+	 */
+	public void testLine53() {
+		List messages = getMessagesAtLine( 53 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * super();
+	 * 1 validation message is expected.
+	 */
+	public void testLine55() {
+		List messages = getMessagesAtLine( 55 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * super(x);
+	 * 2 validation messages are expected.
+	 */
+	public void testLine56() {
+		List messages = getMessagesAtLine( 56 );
+		assertEquals( 2, messages.size() );
+	}
+
+	/*
+	 * super.getProperty("");
+	 * 0 validation messages are expected.
+	 */
+	public void testLine57() {
+		List messages = getMessagesAtLine( 57 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * m myrec{a = super.getProperty("")};
+	 * 0 validation messages are expected.
+	 */
+	public void testLine58() {
+		List messages = getMessagesAtLine( 58 );
+		assertEquals( 0, messages.size() );
+	}
+
+	/*
+	 * super{x = 10};
+	 * 1 validation message is expected.
+	 */
+	public void testLine59() {
+		List messages = getMessagesAtLine( 59 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * super();
+	 * 1 validation message is expected.
+	 */
+	public void testLine63() {
+		List messages = getMessagesAtLine( 63 );
+		assertEquals( 1, messages.size() );
+	}
+
+	/*
+	 * super(10);
+	 * 2 validation messages are expected.
+	 */
+	public void testLine64() {
+		List messages = getMessagesAtLine( 64 );
+		assertEquals( 2, messages.size() );
+	}
+
+	/*
+	 * super.go();
+	 * 1 validation message is expected.
+	 */
+	public void testLine65() {
+		List messages = getMessagesAtLine( 65 );
+		assertEquals( 1, messages.size() );
+	}
+}
