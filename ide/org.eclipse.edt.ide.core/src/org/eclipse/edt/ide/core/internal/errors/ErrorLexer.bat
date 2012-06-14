9831553e32cb30c1fa26c4558639d08b9416a6a4
xbinchen <xbinchen>
2012-06-14 05:30:30 +0000

    update copyright

--- ide/org.eclipse.edt.ide.core/src/org/eclipse/edt/ide/core/internal/errors/ErrorLexer.bat
+++ ide/org.eclipse.edt.ide.core/src/org/eclipse/edt/ide/core/internal/errors/ErrorLexer.bat
@@ -1,3 +1,13 @@
+@rem ***************************************************************************
+@rem Copyright (c) 2012 IBM Corporation and others.
+@rem All rights reserved. This program and the accompanying materials
+@rem are made available under the terms of the Eclipse Public License v1.0
+@rem which accompanies this distribution, and is available at
+@rem http://www.eclipse.org/legal/epl-v10.html
+@rem
+@rem Contributors:
+@rem     IBM Corporation - initial API and implementation
+@rem ***************************************************************************
 set path="C:\IBM\SDP-8011\jdk\bin";
 java -Xmx128m -jar C:\Work\JFlex\lib\jflex.jar error.flex
 del ErrorLexer.java~
--- ide/org.eclipse.edt.ide.core/src/org/eclipse/edt/ide/core/internal/errors/cup.bat
+++ ide/org.eclipse.edt.ide.core/src/org/eclipse/edt/ide/core/internal/errors/cup.bat
@@ -1,3 +1,13 @@
+@rem ***************************************************************************
+@rem Copyright (c) 2012 IBM Corporation and others.
+@rem All rights reserved. This program and the accompanying materials
+@rem are made available under the terms of the Eclipse Public License v1.0
+@rem which accompanies this distribution, and is available at
+@rem http://www.eclipse.org/legal/epl-v10.html
+@rem
+@rem Contributors:
+@rem     IBM Corporation - initial API and implementation
+@rem ***************************************************************************
 set path="C:\IBM\SDP\8013\jdk\bin";
 java -Xmx128m -classpath C:\Work\JavaCup java_cup.Main -parser ErrorBaseParser -nonterms -expect 134 -symbols ErrorNodeTypes -dump_grammar -dump_states error.cup 2> error.cup.dump
 java -classpath ..\..\..\..\..\..\..\..\..\org.eclipse.edt.compiler\bin2 org.eclipse.edt.compiler.core.ast.ActionMethodSplitter ErrorBaseParser > temp.file
