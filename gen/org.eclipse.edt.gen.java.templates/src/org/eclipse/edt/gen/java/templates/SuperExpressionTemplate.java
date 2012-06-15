fa14fcafd460afb3f9600855327f49ada0bef712
jdouglas <jdouglas>
2012-06-15 17:08:33 +0000

    Changes for constructor and super

---
+++ gen/org.eclipse.edt.gen.java.templates/src/org/eclipse/edt/gen/java/templates/SuperExpressionTemplate.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright © 2011, 2012 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM Corporation - initial API and implementation
+ *
+ *******************************************************************************/
+package org.eclipse.edt.gen.java.templates;
+
+import org.eclipse.edt.gen.java.Context;
+import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.SuperExpression;
+
+public class SuperExpressionTemplate extends JavaTemplate {
+
+	public void genExpression(SuperExpression expr, Context ctx, TabbedWriter out) {
+		out.print("super");
+	}
+}
