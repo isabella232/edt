3c3b62af4c3eededcb90c1aefa122accdcb95453
jspadea <jspadea>
2012-06-22 14:36:41 +0000

    bug 373261 - add an EGLDD entry for dedicated service

--- gen/org.eclipse.edt.gen.deployment.javascript/src/org/eclipse/edt/gen/deployment/javascript/DeploymentDescGenerator.java
+++ gen/org.eclipse.edt.gen.deployment.javascript/src/org/eclipse/edt/gen/deployment/javascript/DeploymentDescGenerator.java
@@ -16,8 +16,8 @@ import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import org.eclipse.edt.gen.deployment.util.CommonUtilities;
-import org.eclipse.edt.gen.javascript.JavaScriptAliaser;
 import org.eclipse.edt.ide.deployment.core.model.DeploymentDesc;
+import org.eclipse.edt.javart.resources.egldd.DedicatedBinding;
 import org.eclipse.edt.javart.resources.egldd.RestBinding;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
 
@@ -75,13 +75,24 @@ public class DeploymentDescGenerator{
 				writer.println(");");
 				writer.println("bindFile.bindings.push(binding);");
 			}
-		}	
+		}
+		List dedicatedBindings = deploymentDesc.getDedicatedBindings();
+		for (int i = 0; i < dedicatedBindings.size(); i++)
+		{
+			DedicatedBinding dedicatedBinding = (DedicatedBinding) dedicatedBindings.get(i);
+			writer.println("binding = new egl.eglx.services.DedicatedBinding(");
+			writer.pushIndent();
+			writer.println("/*name                   */ \"" + dedicatedBinding.getName().toLowerCase() + "\"");
+			writer.popIndent();
+			writer.println(");");
+			writer.println("bindFile.bindings.push(binding);");
+		}
 		for (String include : deploymentDesc.getIncludes())
 		{
 			writer.println("bindFile.includes.push(\"" + CommonUtilities.toIncludeDDName( include ) + "\");");
 		}	
 		writer.println("return bindFile;");
-		writer.println("}");
+		writer.println("};");
 	}
 
 }
