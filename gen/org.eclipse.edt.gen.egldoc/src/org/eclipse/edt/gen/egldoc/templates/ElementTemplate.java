0fba75581e967a6160a46b9564bf58d253a3d56e
bmargolis <bmargolis>
2012-07-12 01:09:56 +0000

    bug 353357:  draft metadata is being output for example XHTML files

--- gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/ElementTemplate.java
+++ gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/ElementTemplate.java
@@ -33,14 +33,22 @@ public class ElementTemplate extends EGLDocTemplate {
 			              .append(docType.substring(1, endPosition))
 			              .toString();
 		}
+				
+		/** TODO:  retrieve comments from the IR */
+		String mainComment = "CustomerType identifies the attributes of a customer. \n And what a customer! \n Yeah!";
+		
+		String mainComments[] = mainComment.split("\n", 2);
+		
 		
 		// public void putAttribute(Object key, String value, Object entry) {
 		ctx.putAttribute(this, "docType", docType);
 		ctx.putAttribute(this, "partName", partName);
 		ctx.putAttribute(this, "fullPartName", fullPartName);
+		ctx.putAttribute(this, "firstPara", mainComments[0]);
+		ctx.putAttribute(this, "postFirstPara", mainComments[1]);
 	}
 		
-	
+	 
 	public void genPart(Element part, Context ctx, TabbedWriter out) {
 		ctx.invoke(genTop, part, ctx, out);
 		ctx.invoke(genHead, part, ctx, out);
@@ -48,7 +56,7 @@ public class ElementTemplate extends EGLDocTemplate {
 		out.println("</html>");
 	}
 	
-	// Is for English.  Language should be made external.
+	/** TODO:  must make each language code an external value */
 	public void genTop(Element part, Context ctx, TabbedWriter out)
 		{
 			out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
@@ -63,14 +71,33 @@ public class ElementTemplate extends EGLDocTemplate {
 		// public Object getAttribute(Object key, String value)
 		String docType = (String) ctx.getAttribute(this, "docType");
 		String partName = (String) ctx.getAttribute(this, "partName");
+		String fullPartName = (String) ctx.getAttribute(this, "fullPartName");
+		String firstPara = (String) ctx.getAttribute(this, "firstPara");		
 		
 		out.println("<head>");
 		out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"></meta>");
 		out.println("<meta name=\"DC.Type\" content=\"topic\"></meta>");
 		out.println("<meta name=\"DC.Title\" content=\"" 
 		            + partName + " " + docType + "\"></meta>");
+		out.println("<meta name=\"abstract\" content=\"" + firstPara + "\"></meta>");
+		out.println("<meta name=\"description\" content=\"" + firstPara + "\"></meta>");
+		out.println("<meta name=\"DC.subject\" content=\"" + partName +"\"></meta>");
+		out.println("<meta name=\"keywords\" content=\"" + partName +"\"></meta>");
+		
+		/** TODO:  must do something about the copyright and owner */
+		out.println("<meta name=\"copyright\" content=\"" + "\"(C) Copyright 2011, 2012\" type=\"primary\"></meta>");
+		out.println("<meta name=\"DC.Rights.Owner\" content=\"" + "\"(C) Copyright 2011, 2012\" type=\"primary\"></meta>");		
+		
+		out.println("<meta name=\"DC.Format\" content=\"XHTML\"></meta>");
+		out.println("<meta name=\"DC.Identifier\" content=\"" + fullPartName + "\"></meta>");
+		
+		/** TODO:  must make language an external value */
+		out.println("<meta name=\"DC.Language\" content=\"en-us\"></meta>");
+		
+		out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"style/commonltr.css\"></link>");
+		out.println("<title>" + partName + " " + docType + "</title>");
+		out.println("</head>");
 	}
-	
 	
 	
 	
