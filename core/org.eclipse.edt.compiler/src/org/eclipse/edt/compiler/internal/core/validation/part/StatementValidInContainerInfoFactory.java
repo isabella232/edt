63b623ce03b9898a702d7feb91847eba644227bb
jspadea <jspadea>
2012-07-18 14:24:51 +0000

    remove code for things not supported in EDT, like JSFHandler

--- core/org.eclipse.edt.compiler/src/org/eclipse/edt/compiler/internal/core/validation/part/StatementValidInContainerInfoFactory.java
+++ core/org.eclipse.edt.compiler/src/org/eclipse/edt/compiler/internal/core/validation/part/StatementValidInContainerInfoFactory.java
@@ -12,16 +12,10 @@
 package org.eclipse.edt.compiler.internal.core.validation.part;
 
 import org.eclipse.edt.compiler.binding.IPartBinding;
-import org.eclipse.edt.compiler.core.IEGLConstants;
-import org.eclipse.edt.compiler.core.ast.ConverseStatement;
 import org.eclipse.edt.compiler.core.ast.DisplayStatement;
-import org.eclipse.edt.compiler.core.ast.PrintStatement;
-import org.eclipse.edt.compiler.core.ast.ShowStatement;
 import org.eclipse.edt.compiler.core.ast.Statement;
 import org.eclipse.edt.compiler.core.ast.TransferStatement;
 import org.eclipse.edt.compiler.internal.core.builder.IProblemRequestor;
-import org.eclipse.edt.compiler.internal.core.lookup.AbstractBinder;
-import org.eclipse.edt.compiler.internal.core.validation.statement.IOStatementValidatorConstants;
 
 
 public class StatementValidInContainerInfoFactory {
@@ -72,39 +66,17 @@ public class StatementValidInContainerInfoFactory {
 		TransferStatement.class
 	};
 	
-	private final Class[] INVALID_STATEMENTS_IN_VGWEBTRANSACTION = new Class[] {
-		PrintStatement.class,
-	};
-	
 	private final Class[] INVALID_STATEMENTS_IN_LIBRARY = new Class[] {
 		DisplayStatement.class,
 		TransferStatement.class
 	};
 	
-	private final Class[] INVALID_STATEMENTS_IN_JSFHANDLER = new Class[] {
-		ConverseStatement.class,
-		DisplayStatement.class,
-		PrintStatement.class,
-		ShowStatement.class,
-		TransferStatement.class
-	};
-
 	public IStatementValidInContainerInfo create(IPartBinding partBinding) {
 		switch(partBinding.getKind()) {
 			case IPartBinding.SERVICE_BINDING:
 				return new StatementValidInContainerInfo(INVALID_STATEMENTS_IN_SERVICE, IProblemRequestor.STATEMENT_CANNOT_BE_IN_SERVICE);			
-			case IPartBinding.PROGRAM_BINDING:
-				if (AbstractBinder.annotationIs(partBinding.getSubType(), IOStatementValidatorConstants.EGLUIWEBTRANSACTION, IEGLConstants.PROGRAM_SUBTYPE_VG_WEB_TRANSACTION)) {
-					return new StatementValidInContainerInfo(INVALID_STATEMENTS_IN_VGWEBTRANSACTION, IProblemRequestor.STATEMENT_CANNOT_BE_IN_ACTION_PROGRAM);
-				}
-				break;
 			case IPartBinding.LIBRARY_BINDING:
 				return new StatementValidInContainerInfo(INVALID_STATEMENTS_IN_LIBRARY , IProblemRequestor.STATEMENT_CANNOT_BE_IN_LIBRARY);
-			case IPartBinding.HANDLER_BINDING:
-				if(AbstractBinder.annotationIs(partBinding.getSubType(), IOStatementValidatorConstants.EGLUIJSF, IEGLConstants.PROPERTY_JSFHANDLER)) {
-					return new StatementValidInContainerInfo(INVALID_STATEMENTS_IN_JSFHANDLER , IProblemRequestor.STATEMENT_CANNOT_BE_IN_PAGE_HANDLER);
-				}
-				break;
 		}
 		return DefaultStatementValidInContainerInfo.INSTANCE;
 	}
