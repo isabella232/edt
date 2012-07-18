a67973e4b5d114cc93ef353b72803261794e92fe
bmargolis <bmargolis>
2012-07-18 16:50:04 +0000

    bug 353357; ongoing eglDoc work

--- gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/FieldTemplate.java
+++ gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/FieldTemplate.java
@@ -8,9 +8,20 @@ public class FieldTemplate extends EGLDocTemplate {
 	
 	public void genDeclaration(Field field, Context ctx, TabbedWriter out) {
 
-		// name and type 
+
+		String lastTypeQualifier = field.getType().getTypeSignature();
+		int lastPeriod = lastTypeQualifier.lastIndexOf('.');
+		
+		if (lastPeriod != 0) {
+           lastTypeQualifier = lastTypeQualifier.substring(lastPeriod + 1);
+		}
+		
+		/** TODO.  Identify the url for the doc that's specified for the field type */
+		String typeDocLocation = "unknown";
+		
+		
 		out.println("<dt class=\"dt pt dlterm\"><span class=\"ph synph\"><span class=\"keyword kwd\">" + field.getName() + "</span>");
-		out.println("<a href=eglx.lang.EString>" + field.getType().getTypeSignature() + "</a></span></dt>");
+		out.println("<a href=" + typeDocLocation + ">" +  lastTypeQualifier + "</a></span></dt>");
         out.println("<dd class=\"dd pd\">" + "This is the purpose!" + "</dd>");
 		
 		// out.print(field.getName());
