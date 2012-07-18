19a6febc69c7ef35fdb093be6de41e048deee11e
bmargolis <bmargolis>
2012-07-18 22:08:47 +0000

    bug 353357; ongoing eglDoc work

--- gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/EGLClassTemplate.java
+++ gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/EGLClassTemplate.java
@@ -18,131 +18,133 @@ public class EGLClassTemplate extends EGLDocTemplate {
 		ctx.invoke(genFields, part, ctx, out);
 		ctx.invoke(genFunctions, part, ctx, out);
 	}
-	
-	public void genConstants(EGLClass part, Context ctx, TabbedWriter out){
+
+	public void genConstants(EGLClass part, Context ctx, TabbedWriter out) {
 		List<ConstantField> constantFields = new ArrayList<ConstantField>();
 		for (Field field : part.getFields()) {
-			if(field instanceof ConstantField){
-				constantFields.add((ConstantField)field);
+			if (field instanceof ConstantField) {
+				constantFields.add((ConstantField) field);
 			}
 		}
-		if(constantFields.size() > 0){
+		if (constantFields.size() > 0) {
 			out.println("Constants");
 		}
 		for (ConstantField constantField : constantFields) {
 			ctx.invoke(genConstant, part, ctx, out, constantField);
 		}
 	}
-	
-	public void genConstant(EGLClass part, Context ctx, TabbedWriter out, ConstantField arg){
+
+	public void genConstant(EGLClass part, Context ctx, TabbedWriter out,
+			ConstantField arg) {
 		ctx.invoke(genDeclaration, arg, ctx, out);
 	}
 
 	public void genFields(EGLClass part, Context ctx, TabbedWriter out) {
-		
+
 		String fieldContainerType = (String) ctx.get("fieldContainerType");
-		
-		
+
 		List<Field> nonConstantFields = new ArrayList<Field>();
 		for (Field field : part.getFields()) {
-			if(!(field instanceof ConstantField)){
-				nonConstantFields.add((Field)field);
+			if (!(field instanceof ConstantField)) {
+				nonConstantFields.add((Field) field);
 			}
 		}
 
-		/* SBM.  show section in any case 
-		if(nonConstantFields.size() > 0){
-			out.println(part.getEClass().getName() + " fields");
-		} 
-		*/
+		/*
+		 * SBM. show section in any case if(nonConstantFields.size() > 0){
+		 * out.println(part.getEClass().getName() + " fields"); }
+		 */
 		// part.getEClass().getName()
-		out.println ("<dt class=\"dt dlterm\"><a name=\"recordfields\"></a>" + fieldContainerType + " fields</dt>");
-		if(nonConstantFields.size() == 0){
-			
-			out.println ("<dd> <p class=\"p\">None.</p><p class=\"p\"></p></dd></dt>");
+		out.println("<dt class=\"dt dlterm\"><a name=\"recordfields\"></a>"
+				+ fieldContainerType + " fields</dt>");
+		if (nonConstantFields.size() == 0) {
+
+			out.println("<dd> <p class=\"p\">None.</p><p class=\"p\"></p></dd></dt>");
 			/*
-			out.println("<dd class=\"dd\">");
-			out.println("None.");
-			out.println("<p class=\"p\"></p>");
-			out.println("</dd>");
-			*/
-		}
-		else {			
-			out.println ("<dd class=\"dd\"><dl class=\"dl parml\"><p class=\"p\"></p>");
-			
+			 * out.println("<dd class=\"dd\">"); out.println("None.");
+			 * out.println("<p class=\"p\"></p>"); out.println("</dd>");
+			 */
+		} else {
+			out.println("<dd class=\"dd\"><dl class=\"dl parml\"><p class=\"p\"></p>");
+
 			for (Field field : nonConstantFields) {
 				ctx.invoke(genField, part, ctx, out, field);
 				out.println("<p class=\"p\"></p>");
 			}
-			out.println("<p class=\"p\"></p></dd>");			
+			out.println("<p class=\"p\"></p></dd>");
 		}
-		
-		
-		
-		
-		
-		
+
 	}
 
 	public void genField(EGLClass part, Context ctx, TabbedWriter out, Field arg) {
 		ctx.invoke(genDeclaration, arg, ctx, out);
-		
-		
 
-		
-		
 	}
 
 	public void genFunctions(EGLClass part, Context ctx, TabbedWriter out) {
-		if(part.getFunctions().size() > 0){
-			out.println("Functions");
+
+		out.println("<dt class=\"dt dlterm\"><a name=\"functions\"></a>Functions</dt>");
+		out.println("<dd class=\"dd\"><p class=\"p\"></p>");
+
+		if (part.getFunctions().size() == 0) {
+			out.println ("<dd> <p class=\"p\">None.</p><p class=\"p\"></p></dd></dt>");			
+		} 
+		else {
+			out.println ("<dd class=\"dd\"><dl class=\"dl parml\"><p class=\"p\"></p>");			
+			out.println ("<div class=\"tablenoborder\">"); 		
+			out.println ("<table cellpadding=\"4\" cellspacing=\"0\" id=\"function_table\"");            
+			out.println ("class=\"table\" frame=\"border\" border=\"1\" rules=\"all\">"); 			
+			out.println ("<thead class=\"thead\" align=\"left\">"); 			
+			out.println ("<tr class=\"row\" valign=\"bottom\">");			
+			out.println ("<th class=\"entry\" valign=\"bottom\" width=\"30%\">Name</th>");			
+			out.println ("<th class=\"entry\" valign=\"bottom\" width=\"70%\">Description</th></tr></thead>");			
+			out.println ("<tbody class=\"tbody\">");
+         	
+			for (Function function : part.getFunctions()) {
+				ctx.invoke(genFunction, part, ctx, out, function);
+			}
+			
+			out.println ("</tbody></table></div></dd></dl></div>");
 		}
-		for (Function function : part.getFunctions()) {
-			ctx.invoke(genFunction, part, ctx, out, function);
-		}
+		
 	}
-	
 
 	public void genFunction(EGLClass part, Context ctx, TabbedWriter out, Function arg) {
 		ctx.invoke(genDeclaration, arg, ctx, out);
 	}
 
 	public void genDeclaration(EGLClass part, Context ctx, TabbedWriter out) {
-		
-		out.println(part.getFullyQualifiedName());
-		
-				
-/*		
-		
-		<dt class="dt dlterm"><a name="recordfields"></a>Record fields</dt>
 
-		<!-- is the destination for EGLDoc comments that precede Record fields. 
-		Include the last <p class="p"></p>, whether or not the content is supplied, but include "None" if none. -->
-		<dd class="dd"><dl class="dl parml"><p class="p"></p>
-		
-		<dt class="dt pt dlterm"><span class="ph synph"><span class="keyword kwd">name</span>
+		// out.println(part.getFullyQualifiedName());
+		out.println("in EGLClassTemplate.genDeclaration");
 
-		<!-- uses the Estring definition, calls it String. do an equivalent type renaming when refering to any of the EGL simple-type definitions --> 
-		<a href=eglx.lang.EString>String</a></span></dt>
-		<dd class="dd pd">The customer name.
-		<!-- automatically handle field annotations, each separated from the next by a paragraph -->
-		<p class="p"></p>
-		Field annotations:
-		<dl dlterm><dt class="dt"></dt>
-		<dd class="dd">@<a href="eglx.lang.ExternalName">ExternalName</a> { value = "the_name" }</dd><p class="p"></p></dl></dd>
-
-		<dt class="dt pt dlterm"><span class="ph synph"><span class="keyword kwd">number</span>
-		<a href="eglx.lang.EInt">Int</a>
-		</span></dt>
-		<dd class="dd pd"><class="p">The customer number.
-		<p class="p">Is unique across all our divisions.</p>
-		<p class="p"></p></dd>
-*/		
-
-		
-		
-		
-		
+		/*
+		 * 
+		 * <dt class="dt dlterm"><a name="recordfields"></a>Record fields</dt>
+		 * 
+		 * <!-- is the destination for EGLDoc comments that precede Record
+		 * fields. Include the last <p class="p"></p>, whether or not the
+		 * content is supplied, but include "None" if none. --> <dd
+		 * class="dd"><dl class="dl parml"><p class="p"></p>
+		 * 
+		 * <dt class="dt pt dlterm"><span class="ph synph"><span
+		 * class="keyword kwd">name</span>
+		 * 
+		 * <!-- uses the Estring definition, calls it String. do an equivalent
+		 * type renaming when refering to any of the EGL simple-type definitions
+		 * --> <a href=eglx.lang.EString>String</a></span></dt> <dd
+		 * class="dd pd">The customer name. <!-- automatically handle field
+		 * annotations, each separated from the next by a paragraph --> <p
+		 * class="p"></p> Field annotations: <dl dlterm><dt class="dt"></dt> <dd
+		 * class="dd">@<a href="eglx.lang.ExternalName">ExternalName</a> { value
+		 * = "the_name" }</dd><p class="p"></p></dl></dd>
+		 * 
+		 * <dt class="dt pt dlterm"><span class="ph synph"><span
+		 * class="keyword kwd">number</span> <a href="eglx.lang.EInt">Int</a>
+		 * </span></dt> <dd class="dd pd"><class="p">The customer number. <p
+		 * class="p">Is unique across all our divisions.</p> <p
+		 * class="p"></p></dd>
+		 */
 
 	}
 
--- gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/FunctionTemplate.java
+++ gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/FunctionTemplate.java
@@ -11,16 +11,78 @@ public class FunctionTemplate extends EGLDocTemplate {
 
 	public void genSyntax(Function function, Context ctx, TabbedWriter out) {
 		// process the function
-		out.println(function.getId());
-		out.println("Syntax");
+		/*
+		<dt class="dt dlterm"><a name="compat"></a>Compatibility</dt>
+		<dd class="dd"><p class="p"></p>
+		<div class="tablenoborder"><table cellpadding="4" cellspacing="0" summary=""
+		id="compat_table" class="table" frame="border" border="1"
+		rules="all">
+		<thead class="thead" align="left">
+		<tr class="row" valign="bottom">
+		<th class="entry" valign="bottom" width="20%">Target</th>
+		<th class="entry" valign="bottom" width="80%">Issue</th></tr>
+		</thead>
+		<tbody class="tbody">
+		<tr id="compat_java" class="row">
+		<td class="entry" valign="top" width="20%">Java</td>
+		<td class="entry" valign="top" width="80%">No issues.</td>
+		</tr>
+		<tr id="compat_javascript" class="row">
+		<td class="entry" valign="top" width="20%">JavaScript</td>
+		<td class="entry" valign="top" width="80%">No issues.</td>
+		</tr>
+		</tbody>
+		</table>
+		</div>
+		</dd>
+		</dl>
+		</div>
+		*/
+		
+		
+
+			
+		
+		// ???  out.println(function.getId());
+		// out.println("Syntax");
 		List<FunctionParameter> parameters = function.getParameters();
+		
+		int numberOfParmsLeft = parameters.size() - 1;
+		
 		for (FunctionParameter functionParameter : parameters) {
-			out.println(functionParameter.getName());
+			String parmName = functionParameter.getName().trim();
+			String parmType = functionParameter.getType().getTypeSignature().toString();
+			
+			
+			/** TODO.  Identify the url for the field-type doc*/
+			String typeDocLocation = "unknownParmType";
+					
+			int lastPeriod = parmType.lastIndexOf('.');
+			
+			if (lastPeriod != 0) {
+	           parmType = parmType.substring(lastPeriod + 1);
+			}
+			
+			out.println(parmName + " <a href=\"" + typeDocLocation + "\">" +  parmType + "</a>");			
+						
+			if (numberOfParmsLeft > 0) {
+				
+				out.println(", ");
+				numberOfParmsLeft--;
+			}
 		}
 	}
 
 	public void genDeclaration(Function function, org.eclipse.edt.gen.egldoc.Context ctx, TabbedWriter out) {
+		String functionName = function.getName();
+		String functionFirstPara = "What a function!";
+				
+		out.println("<tr id=\"" + functionName + "\" class=\"row\">");
+		out.println("<td class=\"entry\" valign=\"top\" width=\"20%\">" + functionName + " (");
 		ctx.invoke(genSyntax, function, ctx, out);
+		out.println(")</td>");
+		out.println("<td class=\"entry\" valign=\"top\" width=\"80%\">" + functionFirstPara + "</td></tr>");
+		
 		//ctx.invoke(genFunctionExampleUse, function, ctx, out);
 		//ctx.invoke(genComments()
 		//ctx.invoke(genExample()
