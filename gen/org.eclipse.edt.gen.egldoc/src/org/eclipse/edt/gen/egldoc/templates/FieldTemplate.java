a7cdb7d1958b927d1c33f3cd851f435cfc126326
bmargolis <bmargolis>
2012-07-19 19:45:46 +0000

    Turn off comment processing for now.

--- gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/FieldTemplate.java
+++ gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/FieldTemplate.java
@@ -28,16 +28,16 @@ public class FieldTemplate extends EGLDocTemplate {
 		out.println("<a href=" + typeDocLocation + ">" +  lastTypeQualifier + "</a></span></dt>");
         out.println("<dd class=\"dd pd\">" + "<p></p>" + "This is the purpose!" + "</dd>");
 		
-		String blockComment = Util.findBlockComment(ctx, Util.getLine(field));
-		
-		if(blockComment != null){
-			Map<String, String> blockCommentMap = Util.parseCommentBlock(blockComment);
-			for (Iterator<String> iterator = blockCommentMap.keySet().iterator(); iterator.hasNext();) {
-				String key = (String) iterator.next();
-				out.print(key);
-				out.print(" ");
-				out.println((String)blockCommentMap.get(key));
-			}
-		}
+//		String blockComment = Util.findBlockComment(ctx, Util.getLine(field));
+//		
+//		if(blockComment != null){
+//			Map<String, String> blockCommentMap = Util.parseCommentBlock(blockComment);
+//			for (Iterator<String> iterator = blockCommentMap.keySet().iterator(); iterator.hasNext();) {
+//				String key = (String) iterator.next();
+//				out.print(key);
+//				out.print(" ");
+//				out.println((String)blockCommentMap.get(key));
+//			}
+//		}
 	}
 }
