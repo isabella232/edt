8177de7d1f23545c1cdf69e7ce162d631cd9b8ff
bmargolis <bmargolis>
2012-07-24 00:38:54 +0000

    bug 353357; ongoing eglDoc work

--- gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/EFieldTemplate.java
+++ gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/EFieldTemplate.java
@@ -1,10 +1,13 @@
 package org.eclipse.edt.gen.egldoc.templates;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.eclipse.edt.gen.egldoc.Context;
 import org.eclipse.edt.gen.egldoc.Util;
 import org.eclipse.edt.mof.EField;
+import org.eclipse.edt.mof.EMetadataObject;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
 
 public class EFieldTemplate extends EGLDocTemplate {
@@ -24,6 +27,26 @@ public class EFieldTemplate extends EGLDocTemplate {
              out.println("<a href=\"" + stringDetails.get(2) + "\">");
              out.println(" [ ]</a>");
 		}     
+		
+		
+		/* shows nothing
+		List<EMetadataObject> theList= field.getMetadataList();
+		
+		Iterator<EMetadataObject> iterator = theList.iterator();
+		while (iterator.hasNext()){
+			out.println(iterator.next().toString());
+		}
+		*/
+		
+		/* fails
+		java.lang.annotation.Annotation[] theAnnoList = field.getClass().getAnnotations();
+		int numberOfAnnos = theAnnoList.length; 
+		for (int i = 0; i < numberOfAnnos; i++) {
+			out.println(theAnnoList[i].toString());
+		}
+		*/
+		
+		
 		
 		out.println("</span></dt>");
 		out.println("<dd class=\"dd pd\">" + "<p></p>" + "This is the purpose!"
