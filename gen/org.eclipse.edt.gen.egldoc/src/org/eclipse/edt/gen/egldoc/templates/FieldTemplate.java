b26fc0835d37d77723daf59c3f45cc07274c4243
bmargolis <bmargolis>
2012-07-24 00:40:34 +0000

    bug 353357; ongoing eglDoc work

--- gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/FieldTemplate.java
+++ gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/FieldTemplate.java
@@ -2,11 +2,12 @@ package org.eclipse.edt.gen.egldoc.templates;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Map;
+import java.util.List;
 
 import org.eclipse.edt.gen.egldoc.Context;
 import org.eclipse.edt.gen.egldoc.Util;
 import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.Annotation;
 import org.eclipse.edt.mof.egl.Field;
 
 public class FieldTemplate extends EGLDocTemplate {
@@ -27,9 +28,84 @@ public class FieldTemplate extends EGLDocTemplate {
              out.println(" [ ]</a>");
 		}     
 		
+		
+	
+		
+	
+		    
+				
+				
+		
+		
+		
 		out.println("</span></dt>");
-		out.println("<dd class=\"dd pd\">" + "<p></p>" + "This is the purpose!"
-				+ "</dd>");
+		out.println("<dd class=\"dd pd\">" + "<p></p>" + "This is the purpose!");
+				
+		
+		// get field annotations such as ExternalName
+		List<Annotation> theList= field.getAnnotations();
+		if (!theList.isEmpty()) {
+			out.println("<dl class=\"dl\">");
+			out.println("<dt class=\"dt pt dlterm\">");
+			out.println("<dl class=\"dl\">");
+			out.println("<dt class=\"dt pt dlterm\">");
+			
+			
+			out.println("<dd class=\"dd pd\">" + "<p></p>");
+			out.println("Field annotations");
+			
+			Iterator<Annotation> iterator = theList.iterator();
+			String annotationName;
+			String annotationValue;
+			
+			while (iterator.hasNext()){
+				
+				// out.println(iterator.next().toString());  gives this:
+				/*
+				 * Instance of: eglx.lang.ExternalName eClass -> 
+				 * Instance of: org.eclipse.edt.mof.egl.AnnotationType 
+				 * - eglx.lang.ExternalName annotations 
+				 * -> [] metadata -> [] value -> hello 
+				 * Instance of: EGL_Location eClass 
+				 * -> Instance of: org.eclipse.edt.mof.EClass 
+				 * - EGL_Location annotations 
+				 * -> [] metadata -> [] len -> 41 off -> 182 line -> 18 
+				 */
+				
+				/*
+				 * List fields  = annotation.getEClass().getEFields();
+				 * 
+                 * for(Iterator fieldsIterator : fields.iterator() ){
+                 * annotation.eGet(filedsIterator.next()); }
+				 * 
+				 */
+				
+				
+				
+				
+				
+				// "eglx.lang.ExternalName EGL_Location":  out.println(iterator.next().getEClass().getETypeSignature());
+				annotationName = iterator.next().getEClass().getETypeSignature();
+				
+				// display, element, and list (if appropriate); but assume no list
+				stringDetails = (ArrayList<String>) Util.getEGLSimpleType(annotationName);
+				out.println("<a href=\"" + stringDetails.get(1) + "\">");
+				out.println(stringDetails.get(0) + "</a>");
+				
+				// "hello 41": out.println(iterator.next().getValue().toString());			
+				
+				
+			}
+				
+			out.println("</dd></dl>");
+			out.println("</dd></dl>");
+			
+		}
+		
+		out.println("</dd>");
+		
+		
+		
 		/*
 		String typeDocLocation = field.getType().getTypeSignature(); 		
 	    String displayedTypeName = Util.getEGLSimpleType(typeDocLocation);			
