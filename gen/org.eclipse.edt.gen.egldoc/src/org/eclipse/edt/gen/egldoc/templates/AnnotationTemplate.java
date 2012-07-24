8a864705e5063cba89b65438b0fb192fdaa0b29a
bmargolis <bmargolis>
2012-07-24 15:22:50 +0000

    bug 353357; ongoing eglDoc work

---
+++ gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/AnnotationTemplate.java
@@ -0,0 +1,18 @@
+package org.eclipse.edt.gen.egldoc.templates;
+
+import org.eclipse.edt.gen.egldoc.Context;
+import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.Annotation;
+
+public class AnnotationTemplate extends EGLDocTemplate {
+
+	public void genDeclaration(Annotation annotation, Context ctx, TabbedWriter out) {
+
+		out.println("display the annotations");
+		
+		
+		
+		
+		
+	}
+}
