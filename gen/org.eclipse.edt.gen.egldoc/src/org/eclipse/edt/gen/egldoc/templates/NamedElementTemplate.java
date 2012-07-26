73592b05ac2467d30bfaebb68828d745748f0db6
bmargolis <bmargolis>
2012-07-26 20:04:16 +0000

    bug 353357; ongoing eglDoc work

---
+++ gen/org.eclipse.edt.gen.egldoc/src/org/eclipse/edt/gen/egldoc/templates/NamedElementTemplate.java
@@ -0,0 +1,16 @@
+package org.eclipse.edt.gen.egldoc.templates;
+
+import org.eclipse.edt.gen.egldoc.Context;
+import org.eclipse.edt.mof.codegen.api.TabbedWriter;
+import org.eclipse.edt.mof.egl.NamedElement;
+
+public class NamedElementTemplate extends EGLDocTemplate {
+	
+	public void genDeclaration(NamedElement field, Context ctx, TabbedWriter out) {
+		out.println(field.getName());
+	}
+	public void genDeclaration(String field, Context ctx, TabbedWriter out) {
+		out.println(field);
+	}
+	
+}
