52759862a1d142e496ab9c48b04d6fce9558d554
Matt Heitz <mheitz@us.ibm.com>
2012-10-23 16:25:09 -0400

    Making changes so EUnits will be valid according to our latest rules.

--- gen/org.eclipse.edt.gen.eunit/src/org/eclipse/edt/gen/eunit/templates/javascript/LibraryTemplate.java
+++ gen/org.eclipse.edt.gen.eunit/src/org/eclipse/edt/gen/eunit/templates/javascript/LibraryTemplate.java
@@ -43,7 +43,7 @@ public class LibraryTemplate extends EUnitTemplate {
 	
 	public void genLibDriverClassBody(Library part, Context ctx, TabbedWriter out, String driverPartNameAppend, TestCounter counter){
 		String genedHandlerName = part.getCaseSensitiveName() + driverPartNameAppend;
-		out.println("Handler " + genedHandlerName + " type RUIhandler {initialUI = [], includefile = \"rununit.html\", onConstructionFunction = start, title=\"" +genedHandlerName + "\"} ");
+		out.println("Handler " + genedHandlerName + " type RUIHandler {initialUI = [], includefile = \"rununit.html\", onConstructionFunction = start, title=\"" +genedHandlerName + "\"} ");
 		
 		out.pushIndent();
 		//out.println("use " + part.getFullyQualifiedName() + ";");		
